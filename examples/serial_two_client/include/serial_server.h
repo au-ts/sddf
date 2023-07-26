e7be067014f362dd612e80b8ddc4eef5c05cff0e
Krishnan Winter <krishnanwinter1@gmail.com>
2023-07-26 18:28:07 +1000

    Changed naming from serial to uart. Changed empty debug puts to asn nop.

--- examples/serial_two_client/include/serial_server.h
+++ examples/serial_two_client/include/serial_server.h
@@ -9,3 +9,7 @@ struct serial_server {
 
 #define SERVER_PRINT_CHANNEL 9
 #define SERVER_GETCHAR_CHANNEL 11
+
+// These are also defined in the uart.h header file
+#define NUM_BUFFERS 512
+#define BUFFER_SIZE 2048
