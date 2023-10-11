880c985e682ba54bffb4858559ebeb04b117ccb7
Ivan Velickovic <i.velickovic@unsw.edu.au>
2023-10-11 16:24:48 +1100

    serial: add PL011 driver for QEMU ARM virt

---
+++ serial/drivers/qemu/include/uart.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "shared_ringbuffer.h"
+
+/*
+ * This UART driver is based on the following specificion:
+ * ARM PrimeCell UART (PL011)
+ * Revision: r1p5
+ */
+
+/* Register layout is based on section 3.2 of the specification. */
+struct pl011_uart_regs {
+    uint32_t dr;        /* 0x000 Data Register */
+    uint32_t rsr_ecr;   /* 0x004 Receive Status Register/Error Clear Register */
+    uint32_t res1;      /* 0x008 Reserved */
+    uint32_t res2;      /* 0x00c Reserved */
+    uint32_t res3;      /* 0x010 Reserved */
+    uint32_t res4;      /* 0x014 Reserved */
+    uint32_t fr;        /* 0x018 Flag Register */
+    uint32_t res5;      /* 0x01c Reserved */
+    uint32_t ilpr;      /* 0x020 IrDA Low-Power Counter Register */
+    uint32_t ibrd;      /* 0x024 Integer Baud Rate Register */
+    uint32_t fbrd;      /* 0x028 Fractional Baud Rate Register */
+    uint32_t lcr_h;     /* 0x02c Line Control Register */
+    uint32_t tcr;       /* 0x030 Control Register */
+    uint32_t ifls;      /* 0x034 Interrupt FIFO Level Select Register */
+    uint32_t imsc;      /* 0x038 Interrupt Mask Set/Clear Register */
+    uint32_t ris;       /* 0x03C Raw Interrupt Status Register */
+    uint32_t mis;       /* 0x040 Masked Interrupt Status Register */
+    uint32_t icr;       /* 0x044 Interrupt Clear Register */
+    uint32_t dmacr;     /* 0x048 DMA Control Register */
+    /* The rest of the registers are either reserved or not used */
+};
+
+#define PL011_UARTFR_TXFF       (1 << 5)
+#define PL011_UARTFR_RXFE       (1 << 4)
+
+/*
+Serial driver global state
+*/
+struct serial_driver {
+    int echo;
+    int mode;
+
+    /* Values for handling line mode */
+    uintptr_t line_buffer;
+    int line_buffer_size;
+};
+
+/* LINE CONFIG */
+#define RAW_MODE 0
+#define LINE_MODE 1
+#define ECHO_DIS 0
+#define ECHO_EN 1
+
+/* LINE CONTROL */
+#define ETX 3   /* ctrl+c */
+#define EOT 4   /* ctrl+d */
+#define BS 8    /* backspace */
+#define LF 10   /* Line feed/new line */
+#define CR 13   /* Carriage return */
+#define NEG 21  /* ctrl+u */
+#define SB 26   /* ctrl+z*/
+#define SP 32   /* Space*/
+#define DL 127  /* Delete */
+
+#define NUM_BUFFERS 512
+#define BUFFER_SIZE 2048
+
+enum serial_parity {
+    PARITY_NONE,
+    PARITY_EVEN,
+    PARITY_ODD
+};
+
---
+++ serial/drivers/qemu/uart.c
@@ -0,0 +1,237 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <microkit.h>
+#include "uart.h"
+#include "uart_config.h"
+#include "shared_ringbuffer.h"
+
+/*
+ * The PL011 is supposedly universal, which means that this driver should be
+ * usable for any PL011 compatible device.
+ * Right now the driver is making assumptions that are true on QEMU, but not
+ * necessarily on hardware. Once this is fixed then this is a proper PL011
+ * driver.
+ */
+
+#define LOG_DRIVER(...) do{ microkit_dbg_puts(microkit_name); microkit_dbg_puts("|INFO: "); microkit_dbg_puts(__VA_ARGS__); }while(0)
+#define LOG_DRIVER_ERR(...) do{ microkit_dbg_puts(microkit_name); microkit_dbg_puts("|ERROR: "); microkit_dbg_puts(__VA_ARGS__); }while(0)
+
+/* Defines to manage interrupts and notifications */
+#define IRQ_CH 1
+#define TX_CH  8
+#define RX_CH  10
+
+/* Shared ring buffers */
+uintptr_t rx_free;
+uintptr_t rx_used;
+uintptr_t tx_free;
+uintptr_t tx_used;
+/* UART device registers */
+uintptr_t uart_base;
+
+/* Handlers to be given to the shared ringbuffer API */
+ring_handle_t rx_ring;
+ring_handle_t tx_ring;
+
+struct serial_driver global_serial_driver = {0};
+
+int getchar() {
+    volatile struct pl011_uart_regs *regs = (volatile struct pl011_uart_regs *) uart_base;
+    // @ivanv: don't like having an infinite loop here
+    while (regs->fr & PL011_UARTFR_RXFE) {};
+
+    return regs->dr;
+}
+
+int putchar(int ch) {
+    volatile struct pl011_uart_regs *regs = (volatile struct pl011_uart_regs *) uart_base;
+
+    while ((regs->fr & PL011_UARTFR_TXFF) != 0) {};
+
+    regs->dr = ch;
+    if (ch == '\r') {
+        putchar('\n');
+    }
+
+    return 0;
+}
+
+// Called from handle tx, write each character stored in the buffer to the serial port
+static void raw_tx(char *phys, unsigned int len, void *cookie) {
+    // This is byte by byte for now, switch to DMA use later
+    for (int i = 0; i < len || phys[i] != '\0'; i++) {
+        // Loop until the fifo queue is ready to transmit
+        // @ivanv: putchar always returns zero? what's the point of the return value
+        while (putchar(phys[i]) != 0);
+    }
+}
+
+void handle_tx() {
+    uintptr_t buffer = 0;
+    unsigned int len = 0;
+    void *cookie = 0;
+    // Dequeue something from the Tx ring -> the server will have placed something in here, if its empty then nothing to do
+    while (!driver_dequeue(tx_ring.used_ring, &buffer, &len, &cookie)) {
+        // Buffer cointaining the bytes to write to serial
+        char *phys = (char * )buffer;
+        // Handle the tx
+        raw_tx(phys, len, cookie);
+        // Then enqueue this buffer back into the free queue, so that it can be collected and reused by the server
+        enqueue_free(&tx_ring, buffer, len, &cookie);
+    }
+}
+
+void handle_irq() {
+    /* Here we have interrupted because a character has been inputted. We first want to get the
+    character from the hardware FIFO queue.
+
+    Then we want to dequeue from the rx free ring, and populate it, then add to the rx used queue
+    ready to be processed by the client server
+    */
+    int input = getchar();
+    char input_char = (char) input;
+    microkit_irq_ack(IRQ_CH);
+
+    // Not sure if we should be printing this here or elsewhere? What is the expected behaviour?
+    // putchar(input);
+
+    if (input == -1) {
+        LOG_DRIVER_ERR("invalid input when attempting to getchar\n");
+        return;
+    }
+
+    int ret = 0;
+
+    if (global_serial_driver.echo == ECHO_EN) {
+        // // Special case for backspace
+        if (input_char == '\b' || input_char == 0x7f) {
+            // Backspace will move the cursor back, and space will erase the character
+            putchar('\b');
+            putchar(' ');
+            putchar('\b');
+        } else if (input_char == '\r') {
+            // Convert the carriage return to a new line
+            putchar('\n');
+        }
+        else {
+            putchar(input);
+        }
+    }
+
+    if (global_serial_driver.mode == RAW_MODE) {
+        // Place characters straight into the buffer
+
+        // Address that we will pass to dequeue to store the buffer address
+        uintptr_t buffer = 0;
+        // Integer to store the length of the buffer
+        unsigned int buffer_len = 0;
+
+        void *cookie = 0;
+
+        ret = dequeue_free(&rx_ring, &buffer, &buffer_len, &cookie);
+
+        if (ret != 0) {
+            LOG_DRIVER_ERR("unable to dequeue from RX free ring\n");
+            return;
+        }
+
+        ((char *) buffer)[0] = (char) input;
+
+        // Now place in the rx used ring
+        ret = enqueue_used(&rx_ring, buffer, 1, &cookie);
+        microkit_notify(RX_CH);
+
+    } else if (global_serial_driver.mode == LINE_MODE) {
+        // Place in a buffer, until we reach a new line, ctrl+d/ctrl+c/enter (check what else can stop)
+        if (global_serial_driver.line_buffer == 0) {
+            // We need to dequeue a buffer to use
+            // Address that we will pass to dequeue to store the buffer address
+            uintptr_t buffer = 0;
+            // Integer to store the length of the buffer
+            unsigned int buffer_len = 0;
+
+            void *cookie = 0;
+
+            ret = dequeue_free(&rx_ring, &buffer, &buffer_len, &cookie);
+            if (ret != 0) {
+                LOG_DRIVER_ERR("unable to dequeue from the RX free ring\n");
+                return;
+            }
+
+            global_serial_driver.line_buffer = buffer;
+            global_serial_driver.line_buffer_size = 0;
+        }
+
+        // Check that the buffer is not full, and other exit conditions here
+        if (global_serial_driver.line_buffer_size > BUFFER_SIZE ||
+            input_char == EOT ||
+            input_char == ETX ||
+            input_char == LF ||
+            input_char == SB ||
+            input_char == CR) {
+                char *char_arr = (char * ) global_serial_driver.line_buffer;
+                void *cookie = 0;
+                // Place the line end character into buffer
+                char_arr[global_serial_driver.line_buffer_size] = input_char;
+                global_serial_driver.line_buffer_size += 1;
+                // Enqueue buffer back
+                ret = enqueue_used(&rx_ring, global_serial_driver.line_buffer, global_serial_driver.line_buffer_size, &cookie);
+                // Zero out the driver states
+                global_serial_driver.line_buffer = 0;
+                global_serial_driver.line_buffer_size = 0;
+                microkit_notify(RX_CH);
+
+        } else {
+            // Otherwise, add to the character array
+            char *char_arr = (char * ) global_serial_driver.line_buffer;
+
+            // Conduct any line editing as long as we have stuff in the buffer
+            if (input_char == 0x7f && global_serial_driver.line_buffer_size > 0) {
+                // Remove last character
+                global_serial_driver.line_buffer_size -= 1;
+                char_arr[global_serial_driver.line_buffer_size] = 0;
+            } else {
+                char_arr[global_serial_driver.line_buffer_size] = input;
+                global_serial_driver.line_buffer_size += 1;
+            }
+        }
+    }
+
+    if (ret != 0) {
+        LOG_DRIVER_ERR("unable to enqueue to the TX free ring\n");
+        return;
+    }
+}
+
+void init(void) {
+    LOG_DRIVER("initialising\n");
+
+    // Init the shared ring buffers
+    ring_init(&rx_ring, (ring_buffer_t *)rx_free, (ring_buffer_t *)rx_used, 0, SIZE, SIZE);
+    ring_init(&tx_ring, (ring_buffer_t *)tx_free, (ring_buffer_t *)tx_used, 0, SIZE, SIZE);
+
+    // @ivanv: need to do proper initialisation
+
+    /* Line configuration. Set LINE or RAW mode here, and disable or enable ECHO */
+    // int ret = serial_configure(115200, 8, PARITY_NONE, 1, UART_MODE, ECHO_MODE);
+
+    // if (ret != 0) {
+    //     LOG_DRIVER("error occured during line configuration\n");
+    // }
+}
+
+void notified(microkit_channel ch) {
+    switch(ch) {
+        case IRQ_CH:
+            handle_irq();
+            return;
+        case TX_CH:
+            handle_tx();
+            break;
+        case RX_CH:
+            break;
+        default:
+            LOG_DRIVER("received notification on unexpected channel\n");
+            break;
+    }
+}
