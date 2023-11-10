2bee017ab55ff2a288764b94340ce8f975a95a6a
James Archer <j.archer@unsw.edu.au>
2023-11-10 15:49:53 +1100

    Add timer client library
    This commit adds a library to the sDDF which provides a client interface for interacting with timer drivers.

--- examples/timer/client.c
+++ examples/timer/client.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 #include <microkit.h>
-#include "timer.h"
+#include <sddf/timer/client.h>
 
 static void
 put64(uint64_t val) {
@@ -25,24 +25,24 @@ notified(microkit_channel ch)
      */
     microkit_dbg_puts("CLIENT|INFO: Got a timeout!\n");
     /* Get the current time */
-    uint64_t time = time_now();
+    uint64_t time = sddf_timer_time_now();
     microkit_dbg_puts("CLIENT|INFO: Now the time (in nanoseconds) is: ");
     put64(time);
     microkit_dbg_puts("\n");
     /* Set another timeout */
-    set_timeout(NS_IN_S);
+    sddf_timer_set_timeout(NS_IN_S);
 }
 
 void
 init(void)
 {
     // lets get the time!
-    uint64_t time = time_now();
+    uint64_t time = sddf_timer_time_now();
     microkit_dbg_puts("CLIENT|INFO: The time now is: ");
     put64(time);
     microkit_dbg_puts("\n");
 
     // lets set a timeout
     microkit_dbg_puts("CLIENT|INFO: Setting a time out for 1 second\n");
-    set_timeout(NS_IN_S);
+    sddf_timer_set_timeout(NS_IN_S);
 }
---
+++ timer/client/client.c
@@ -0,0 +1,21 @@
+#include <stdint.h>
+#include <microkit.h>
+#include <sddf/timer/client.h>
+
+#define GET_TIME 0
+#define SET_TIMEOUT 1
+
+void
+sddf_timer_set_timeout(uint64_t timeout)
+{
+    microkit_mr_set(0, timeout);
+    microkit_ppcall(TIMER_CHANNEL, microkit_msginfo_new(SET_TIMEOUT, 1));
+}
+
+uint64_t
+sddf_timer_time_now(void)
+{
+    microkit_ppcall(TIMER_CHANNEL, microkit_msginfo_new(GET_TIME, 0));
+    uint64_t time_now = seL4_GetMR(0);
+    return time_now;
+}
