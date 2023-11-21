b4a9a795e61b48cdd6f56637aef89564c53db23b
Ivan Velickovic <i.velickovic@unsw.edu.au>
2023-11-21 15:14:24 +1100

    serial: clean up TX multiplexor and make colours configurable
    See the comments added in the patch for details, but essentially
    this patch allows the addition of colour to each client buffer to
    be configurable by the system designer.

--- serial/components/mux_tx.c
+++ serial/components/mux_tx.c
@@ -8,8 +8,6 @@
 #include "util/include/util.h"
 #include "uart.h"
 
-/* TODO: ADD IN DIFFERENT COLOURS TO DIFFERENTIATE DIFFERENT CLIENT STREAMS */
-
 #define DRIVER_CH 9
 
 #ifndef NUM_CLIENTS
@@ -19,7 +17,15 @@
 #define COLOUR_START_LEN 5
 #define COLOUR_END_LEN 4
 
-char *client_colours[] = { "\x1b[31m", "\x1b[32m" };
+#define MAX_NUM_CLIENTS 2
+
+/* This is a simple sanity check that we have defined as many colours
+   as the user as specified clients */
+#if NUM_CLIENTS > MAX_NUM_CLIENTS && defined(TRANSFER_WITH_COLOUR)
+#error "There are more clients then there are colours to differentiate them"
+#endif
+
+char *client_colours[MAX_NUM_CLIENTS] = { "\x1b[31m", "\x1b[32m" };
 char *client_colour_end = "\x1b[0m";
 
 /* Memory regions as defined in the system file */
@@ -42,10 +48,34 @@ uintptr_t tx_data_client2;
 ring_handle_t tx_ring[NUM_CLIENTS];
 ring_handle_t drv_tx_ring;
 
+size_t copy_with_colour(size_t client, size_t buffer_len, char *driver_buf, char *client_buf) {
+    size_t len_copied = 0;
+    assert(buffer_len + COLOUR_START_LEN + COLOUR_START_LEN <= BUFFER_SIZE);
+    /* First copy the colour start bytes */
+    memcpy((char *) driver_buf, client_colours[client], COLOUR_START_LEN);
+    len_copied += COLOUR_START_LEN;
+    /* Copy the actual TX data */
+    char *dest_buffer = driver_buf + COLOUR_START_LEN;
+    memcpy(dest_buffer, client_buf, buffer_len);
+    len_copied += buffer_len;
+    /* And then finally the colour end bytes */
+    memcpy((char *)(dest_buffer + buffer_len), client_colour_end, COLOUR_END_LEN);
+    len_copied += COLOUR_END_LEN;
+
+    return len_copied;
+}
+
+size_t copy_normal(size_t buffer_len, char *driver_buf, char *client_buf) {
+    /* All we need to do is copy the client buffer into the driver buffer. */
+    memcpy(driver_buf, client_buf, buffer_len);
+
+    return buffer_len;
+}
+
 int handle_tx(int curr_client) {
     // Copy data from the client ring to the driver rings.
-    uintptr_t buffer = 0;
-    unsigned int buffer_len = 0;
+    uintptr_t client_buf = 0;
+    unsigned int client_buf_len = 0;
     void *cookie = 0;
 
     bool was_empty = ring_empty(drv_tx_ring.used_ring);
@@ -56,43 +86,49 @@ int handle_tx(int curr_client) {
         if (ring_plugged(tx_ring[client].used_ring)) {
             continue;
         }
-        while (!dequeue_used(&tx_ring[client], &buffer, &buffer_len, &cookie)) {
+        while (!dequeue_used(&tx_ring[client], &client_buf, &client_buf_len, &cookie)) {
             // We want to enqueue into the drivers used ring
-            uintptr_t drv_buffer = 0;
-            unsigned int drv_len = 0;
+            uintptr_t driver_buf = 0;
+            unsigned int driver_buf_len = 0;
             void *drv_cookie = 0;
 
-            int ret = driver_dequeue(drv_tx_ring.free_ring, &drv_buffer, &drv_len, &drv_cookie);
+            int ret = driver_dequeue(drv_tx_ring.free_ring, &driver_buf, &driver_buf_len, &drv_cookie);
             if (ret != 0) {
                 microkit_dbg_puts("Failed to dequeue buffer from drv tx avail ring\n");
                 return 1;
             }
 
-            assert(buffer_len + COLOUR_START_LEN + COLOUR_START_LEN <= BUFFER_SIZE);
-            /* First copy the colour start bytes */
-            size_t len_copied = 0;
-            memcpy((char *) drv_buffer, client_colours[client], COLOUR_START_LEN);
-            len_copied += COLOUR_START_LEN;
-            /* Then the actual colour */
-            uintptr_t dest_buffer = drv_buffer + COLOUR_START_LEN;
-            char *string = (char *) buffer;
-            memcpy((char *) dest_buffer, string, buffer_len);
-            len_copied += buffer_len;
-            /* And then finally the colour end bytes */
-            memcpy((char *)(dest_buffer + buffer_len), client_colour_end, COLOUR_END_LEN);
-            len_copied += COLOUR_END_LEN;
+            /*
+             * Depending on the overall system, we may want to add colour to
+             * the transmit data in order to easily identify which client the
+             * data belongs to when looking at the serial output. This makes
+             * the most sense when you have, for example, multiple client
+             * consoles being multiplexed. However, not all systems will act
+             * like that even if they have multiple clients, which is why this
+             * is configurable behaviour.
+             */
+#ifdef TRANSFER_WITH_COLOUR
+            size_t len_copied = copy_with_colour(client, client_buf_len, (char *)driver_buf, (char *)client_buf);
+#else
+            size_t len_copied = copy_normal(client_buf_len, (char *)driver_buf, (char *)client_buf);
+#endif
 
             drv_cookie = cookie;
 
-            ret = enqueue_used(&drv_tx_ring, drv_buffer, len_copied, drv_cookie);
+            ret = enqueue_used(&drv_tx_ring, driver_buf, len_copied, drv_cookie);
             if (ret != 0) {
                 microkit_dbg_puts("Failed to enqueue buffer to drv tx used ring\n");
                 // Don't know if I should return here, because we need to enqueue a
                 // serpeate buffer
             }
 
-            // enqueue back to the client free ring
-            enqueue_free(&tx_ring[client], buffer, BUFFER_SIZE, cookie);
+            /*
+             * Now that we've finished processing the client's used buffer, we
+             * can put it back in the free ring. Note that the *whole* buffer is
+             * free now, which is why the length we enqueue is the maximum
+             * buffer length.
+             */
+            enqueue_free(&tx_ring[client], client_buf, BUFFER_SIZE, cookie);
         }
     }
 
