df4534aa344ef2daf0600ae2ed15372c223fd01a
Eric Chan <eric.chan2@unsw.edu.au>
2023-12-01 14:56:29 +1100

    arm timer driver multiplex with minheap

--- drivers/clock/arm/timer.c
+++ drivers/clock/arm/timer.c
@@ -7,15 +7,15 @@
 
 /* All time units are in nano seconds */
 
+#define IRQ_CH 0
 #define MAX_TIMEOUTS 4096
 
 #define GET_TIME 0
 #define SET_TIMEOUT 1
 
 static uint32_t freq;
-// static min_heap_t timeout_heap;
-// static heap_element_t timeout_heap_data[MAX_TIMEOUTS];
-
+static min_heap_t timeout_heap;
+static heap_element_t timeout_heap_data[MAX_TIMEOUTS];
 
 static uint64_t get_time()
 {
@@ -31,49 +31,43 @@ static void set_timeout(uint64_t timeout)
 void
 init(void)
 {
-    generic_timer_enable();
-    generic_timer_unmask_irq();
     freq = generic_timer_get_freq();
-    
-    uint64_t time = get_time();
-    time += 5000000000;
-    generic_timer_set_compare(freq_ns_and_hz_to_cycles(time, freq));
-
-    uint64_t ticks;
-    uint64_t time_print;
-    // printf("freq: %d\n", freq);
-    for(int i = 0; true; i++) {
-        ticks = generic_timer_get_ticks();
-        time_print = freq_cycles_and_hz_to_ns(ticks, freq);
-        
-        if (i % 1000000 == 0) {
-            printf("time: %d\n", time_print / 1000000000);
-            printf("status: %d\n", generic_timer_status());
-        }
-    }
-    
-    // heap_init(&timeout_heap, timeout_heap_data, MAX_TIMEOUTS);
-
+    generic_timer_enable();
+    generic_timer_set_compare(UINT64_MAX);
+    heap_init(&timeout_heap, timeout_heap_data, MAX_TIMEOUTS);    
 }
 
 static void
 handle_irq(microkit_channel ch)
-{
-    // printf("ARM_TIMER_DRIVER|INFO: IRQ received!\n");
-    // heap_element_t min;
-    // // heap_extract_min(&timeout_heap, min);
+{   
+    if (heap_empty(&timeout_heap)) {
+        // Could receive an IRQ on PD init when heap is empty, do nothing just ack it
+        // After init this should never happen, and would be considered an error
+        printf("ARM_TIMER_DRIVER|INFO: received IRQ when heap is empty\n");
+        generic_timer_set_compare(UINT64_MAX);
+        return;
+    }
+
+    heap_element_t min;
+    heap_extract_min(&timeout_heap, &min);
     
-    // microkit_notify(min.ch);
-    
-    // set_timeout(min.key);
+    microkit_notify(min.ch);
+
+    if (heap_empty(&timeout_heap)) {
+        generic_timer_set_compare(UINT64_MAX);
+        return;
+    } else {
+        heap_get_min(&timeout_heap, &min);
+        set_timeout(min.key);
+    }
 }
 
 void
 notified(microkit_channel ch)
 {
-    if (ch == GENERIC_TIMER_PCNT_IRQ) {
+    if (ch == IRQ_CH) {
         handle_irq(ch);
-        // now ACK irq
+        microkit_irq_ack_delayed(ch);
     } else {
         printf("ARM_TIMER_DRIVER|ERROR: unexpected notification\n");
     }
@@ -89,45 +83,23 @@ protected(microkit_channel ch, microkit_msginfo msginfo)
             return microkit_msginfo_new(0, 1);
         }
         case SET_TIMEOUT: {
-            // uint64_t timeout_duration = (uint64_t)(seL4_GetMR(0));
-            // uint64_t cur_time = get_time();
-            // uint64_t timeout = cur_time + timeout_duration;
-            // heap_insert(&timeout_heap, (heap_element_t){.key = timeout, .ch = ch});
-            // uint64_t set_timeout;
-            // heap_get_min(&timeout_heap, &timeout);
+            if (heap_full(&timeout_heap)) {
+                // @ericc: Drop the request if heap is full
+                printf("ARM_TIMER_DRIVER|ERROR: timeout heap full\n");
+                break;
+            }
+            uint64_t timeout_duration = (uint64_t)(seL4_GetMR(0));
+            uint64_t cur_time = get_time();
+            uint64_t timeout = cur_time + timeout_duration;
+            heap_insert(&timeout_heap, (heap_element_t){.key = timeout, .ch = ch});
+            
+            heap_element_t min;
+            heap_get_min(&timeout_heap, &min);
+            set_timeout(min.key);
             break;
         }
         default:
             break;
     }
-
     return microkit_msginfo_new(0, 0);
 }
-    
-
-
-    // heap_insert(&timeout_heap, (heap_element_t){.key = 300, .ch = 3});
-    // heap_insert(&timeout_heap, (heap_element_t){.key = 100, .ch = 1});
-    // heap_insert(&timeout_heap, (heap_element_t){.key = 200, .ch = 2});
-    // heap_insert(&timeout_heap, (heap_element_t){.key = 400, .ch = 4});
-    // heap_insert(&timeout_heap, (heap_element_t){.key = 600, .ch = 6});
-    // heap_insert(&timeout_heap, (heap_element_t){.key = 800, .ch = 8});
-    // heap_insert(&timeout_heap, (heap_element_t){.key = 700, .ch = 7});
-    // heap_insert(&timeout_heap, (heap_element_t){.key = 500, .ch = 5});
-    // heap_element_t min;
-    // heap_extract_min(&timeout_heap, &min);
-    // printf("min: %llu\n", min.key);
-    // heap_extract_min(&timeout_heap, &min);
-    // printf("min: %llu\n", min.key);
-    // heap_extract_min(&timeout_heap, &min);
-    // printf("min: %llu\n", min.key);
-    // heap_extract_min(&timeout_heap, &min);
-    // printf("min: %llu\n", min.key);
-    // heap_extract_min(&timeout_heap, &min);
-    // printf("min: %llu\n", min.key);
-    // heap_extract_min(&timeout_heap, &min);
-    // printf("min: %llu\n", min.key);
-    // heap_extract_min(&timeout_heap, &min);
-    // printf("min: %llu\n", min.key);
-    // heap_extract_min(&timeout_heap, &min);
-    // printf("min: %llu\n", min.key);
--- examples/arm_timer/client.c
+++ examples/arm_timer/client.c
@@ -1,19 +1,27 @@
 #include <microkit.h>
+#include <sddf/timer/client.h>
+#include <printf.h>
+
+#define TIMER_CH 1
 
 void
 init(void)
 {
-    // generic_timer_enable();
-    // microkit_dbg_puts("");
+    // get time
+    uint64_t time = sddf_timer_time_now();
+    printf("ARM_TIMER_CLIENT|INFO: The time now is: %llu\n", time);
+
+    // set timeouts
+    printf("ARM_TIMER_CLIENT|INFO: Setting a time out for 2 seconds\n");
+    sddf_timer_set_timeout(NS_IN_S * 2);
+    printf("ARM_TIMER_CLIENT|INFO: Setting a time out for 6 seconds\n");
+    sddf_timer_set_timeout(NS_IN_S * 6);
 }
 
 void
 notified(microkit_channel ch)
 {
-}
-
-seL4_MessageInfo_t
-protected(microkit_channel ch, microkit_msginfo msginfo)
-{
-    return microkit_msginfo_new(0, 0);
+    if (ch == TIMER_CH) {
+        printf("ARM_TIMER_CLIENT|INFO: Timeout received!\n");
+    }
 }
