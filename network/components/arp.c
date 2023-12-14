12bb49447952420e3d65ba1d71c057a8fc002d44
James Archer <j.archer@unsw.edu.au>
2023-12-14 14:01:09 +1100

    Duplicate cache functions in ARP component to fix Kitty build

--- network/components/arp.c
+++ network/components/arp.c
@@ -1,7 +1,6 @@
 #include <microkit.h>
 #include <sddf/network/shared_ringbuffer.h>
 #include "util.h"
-#include "cache.h"
 #include <string.h>
 #include "lwip/ip_addr.h"
 #include "netif/etharp.h"
@@ -25,6 +24,60 @@
 #define LINE_START(a) ROUND_DOWN(a, CONFIG_L1_CACHE_LINE_SIZE_BITS)
 #define LINE_INDEX(a) (LINE_START(a)>>CONFIG_L1_CACHE_LINE_SIZE_BITS)
 
+static inline void
+dsb(void)
+{
+    asm volatile("dsb sy" ::: "memory");
+}
+
+static inline void 
+dmb(void)
+{
+    asm volatile("dmb sy" ::: "memory");
+}
+
+static inline void
+cleanInvalByVA(unsigned long vaddr)
+{
+    asm volatile("dc civac, %0" : : "r"(vaddr));
+    dsb();
+}
+
+static inline void
+cleanByVA(unsigned long vaddr)
+{
+    asm volatile("dc cvac, %0" : : "r"(vaddr));
+    dmb();
+}
+
+static void
+cleanInvalidateCache(unsigned long start, unsigned long end)
+{
+    unsigned long line;
+    unsigned long index;
+    /* Clean the L1 range */
+
+    /* Finally clean and invalidate the L1 range. The extra clean is only strictly neccessary
+     * in a multiprocessor environment to prevent a write being lost if another core is
+     * attempting a store at the same time. As the range should already be clean asking
+     * it to clean again should not affect performance */
+    for (index = LINE_INDEX(start); index < LINE_INDEX(end) + 1; index++) {
+        line = index << CONFIG_L1_CACHE_LINE_SIZE_BITS;
+        cleanInvalByVA(line);
+    }
+}
+
+static void
+cleanCache(unsigned long start, unsigned long end)
+{
+    unsigned long line;
+    unsigned long index;
+
+    for (index = LINE_INDEX(start); index < LINE_INDEX(end) + 1; index++) {
+        line = index << CONFIG_L1_CACHE_LINE_SIZE_BITS;
+        cleanByVA(line);
+    }
+}
 
 uintptr_t rx_free;
 uintptr_t rx_used;
