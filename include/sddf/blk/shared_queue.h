05afc67da088d82bc2e2b56cc58a27051073726d
Eric Chan <eric.chan2@unsw.edu.au>
2024-01-15 16:04:29 +1100

    rename queue_handle -> h, remove macros, move serial number length to #define

--- include/sddf/blk/shared_queue.h
+++ include/sddf/blk/shared_queue.h
@@ -15,12 +15,11 @@
 #define BLK_REQ_QUEUE_SIZE 1024
 /* Maximum number of slots in the response queue. Can be configured. */
 #define BLK_RESP_QUEUE_SIZE 1024
-
-#define REQ_QUEUE(queue_handle) ((queue_handle)->req_queue)
-#define RESP_QUEUE(queue_handle) ((queue_handle)->resp_queue)
+/* Device serial number max string length */
+#define BLK_MAX_SERIAL_NUMBER 63
 
 typedef struct blk_storage_info {
-    char serial_number[64]; /* device serial numbers should fit within 64 characters */
+    char serial_number[BLK_MAX_SERIAL_NUMBER + 1]; 
     bool read_only;
     bool ready;
     uint16_t blocksize;
@@ -87,7 +86,7 @@ typedef struct blk_queue_handle {
 /**
  * Initialise the shared queues.
  *
- * @param queue_handle queue handle to use.
+ * @param h queue handle to use.
  * @param request pointer to request queue in shared memory.
  * @param response pointer to response queue in shared memory.
  * @param buffer_init true indicates the read and write indices in shared memory need to be initialised.
@@ -95,7 +94,7 @@ typedef struct blk_queue_handle {
  * @param request_size number of slots in request queue.
  * @param response_size number of slots in response queue.
  */
-void blk_queue_init(blk_queue_handle_t *queue_handle,
+void blk_queue_init(blk_queue_handle_t *h,
                         blk_req_queue_t *request,
                         blk_resp_queue_t *response,
                         bool buffer_init,
@@ -105,79 +104,79 @@ void blk_queue_init(blk_queue_handle_t *queue_handle,
 /**
  * Check if the request queue is empty.
  *
- * @param queue_handle queue handle containing request queue.
+ * @param h queue handle containing request queue.
  *
  * @return true indicates the request queue is empty, false otherwise.
  */
-static inline bool blk_req_queue_empty(blk_queue_handle_t *queue_handle)
+static inline bool blk_req_queue_empty(blk_queue_handle_t *h)
 {
-    return !((REQ_QUEUE(queue_handle)->write_idx - REQ_QUEUE(queue_handle)->read_idx) % REQ_QUEUE(queue_handle)->size);
+    return !((h->req_queue->write_idx - h->req_queue->read_idx) % h->req_queue->size);
 }
 
 /**
  * Check if the response queue is empty.
  *
- * @param queue_handle queue handle containing response queue.
+ * @param h queue handle containing response queue.
  *
  * @return true indicates the response queue is empty, false otherwise.
  */
-static inline bool blk_resp_queue_empty(blk_queue_handle_t *queue_handle)
+static inline bool blk_resp_queue_empty(blk_queue_handle_t *h)
 {
-    return !((RESP_QUEUE(queue_handle)->write_idx - RESP_QUEUE(queue_handle)->read_idx) % RESP_QUEUE(queue_handle)->size);
+    return !((h->resp_queue->write_idx - h->resp_queue->read_idx) % h->resp_queue->size);
 }
 
 /**
  * Check if the request queue is full.
  *
- * @param queue_handle queue handle containing request queue.
+ * @param h queue handle containing request queue.
  *
  * @return true indicates the request queue is full, false otherwise.
  */
-static inline bool blk_req_queue_full(blk_queue_handle_t *queue_handle)
+static inline bool blk_req_queue_full(blk_queue_handle_t *h)
 {
-    return !((REQ_QUEUE(queue_handle)->write_idx - REQ_QUEUE(queue_handle)->read_idx + 1) % REQ_QUEUE(queue_handle)->size);
+    return !((h->req_queue->write_idx - h->req_queue->read_idx + 1) % h->req_queue->size);
 }
 
 /**
  * Check if the response queue is full.
  *
- * @param queue_handle queue handle containing response queue.
+ * @param h queue handle containing response queue.
  *
  * @return true indicates the response queue is full, false otherwise.
  */
-static inline bool blk_resp_queue_full(blk_queue_handle_t *queue_handle)
+static inline bool blk_resp_queue_full(blk_queue_handle_t *h)
 {
-    return !((RESP_QUEUE(queue_handle)->write_idx - RESP_QUEUE(queue_handle)->read_idx + 1) % RESP_QUEUE(queue_handle)->size);
+    return !((h->resp_queue->write_idx - h->resp_queue->read_idx + 1) % h->resp_queue->size);
 }
 
 /**
  * Get the number of elements in a request queue.
  *
- * @param queue_handle queue handle containing request and response queues.
+ * @param h queue handle containing request and response queues.
  *
  * @return number of elements in the queue.
  */
-static inline int blk_req_queue_size(blk_queue_handle_t *queue_handle)
+static inline int blk_req_queue_size(blk_queue_handle_t *h)
 {
-    return (REQ_QUEUE(queue_handle)->write_idx - REQ_QUEUE(queue_handle)->read_idx);
+    return (h->req_queue->write_idx - h->req_queue->read_idx);
 }
 
 /**
  * Get the number of elements in a response queue.
  *
- * @param queue_handle queue handle containing request and response queues.
+ * @param h queue handle containing request and response queues.
  *
  * @return number of elements in the queue.
  */
-static inline int blk_resp_queue_size(blk_queue_handle_t *queue_handle)
+static inline int blk_resp_queue_size(blk_queue_handle_t *h)
 {
-    return (RESP_QUEUE(queue_handle)->write_idx - RESP_QUEUE(queue_handle)->read_idx);
+    return (h->resp_queue->write_idx - h->resp_queue->read_idx);
 }
 
 /**
  * Enqueue an element into the request queue.
  *
- * @param queue_handle queue handle containing request queue to enqueue to.
+ * @param h queue handle containing request queue to enqueue to.
  * @param code request code.
  * @param addr encoded dma address of data to read/write.
  * @param block_number block number to read/write to.
@@ -186,25 +185,25 @@ static inline int blk_resp_queue_size(blk_queue_handle_t *queue_handle)
  *
  * @return -1 when request queue is full, 0 on success.
  */
-static inline int blk_enqueue_req(blk_queue_handle_t *queue_handle,
+static inline int blk_enqueue_req(blk_queue_handle_t *h,
                                         blk_request_code_t code,
                                         uintptr_t addr,
                                         uint32_t block_number,
                                         uint16_t count,
                                         uint32_t id)
 {
-    if (blk_req_queue_full(queue_handle)) {
+    if (blk_req_queue_full(h)) {
         return -1;
     }
 
-    REQ_QUEUE(queue_handle)->buffers[REQ_QUEUE(queue_handle)->write_idx % REQ_QUEUE(queue_handle)->size].code = code;
-    REQ_QUEUE(queue_handle)->buffers[REQ_QUEUE(queue_handle)->write_idx % REQ_QUEUE(queue_handle)->size].addr = addr;
-    REQ_QUEUE(queue_handle)->buffers[REQ_QUEUE(queue_handle)->write_idx % REQ_QUEUE(queue_handle)->size].block_number = block_number;
-    REQ_QUEUE(queue_handle)->buffers[REQ_QUEUE(queue_handle)->write_idx % REQ_QUEUE(queue_handle)->size].count = count;
-    REQ_QUEUE(queue_handle)->buffers[REQ_QUEUE(queue_handle)->write_idx % REQ_QUEUE(queue_handle)->size].id = id;
+    h->req_queue->buffers[h->req_queue->write_idx % h->req_queue->size].code = code;
+    h->req_queue->buffers[h->req_queue->write_idx % h->req_queue->size].addr = addr;
+    h->req_queue->buffers[h->req_queue->write_idx % h->req_queue->size].block_number = block_number;
+    h->req_queue->buffers[h->req_queue->write_idx % h->req_queue->size].count = count;
+    h->req_queue->buffers[h->req_queue->write_idx % h->req_queue->size].id = id;
 
     THREAD_MEMORY_RELEASE();
-    REQ_QUEUE(queue_handle)->write_idx++;
+    h->req_queue->write_idx++;
 
     return 0;
 }
@@ -213,7 +212,7 @@ static inline int blk_enqueue_req(blk_queue_handle_t *queue_handle,
  * Enqueue an element into the response queue.
  * This indicates the request has been processed and a response is ready.
  *
- * @param queue_handle queue handle containing response queue to enqueue to.
+ * @param h queue handle containing response queue to enqueue to.
  * @param status response status.
  * @param addr pointer to encoded dma address of data.
  * @param count number of blocks allocated for corresponding request
@@ -222,25 +221,25 @@ static inline int blk_enqueue_req(blk_queue_handle_t *queue_handle,
  *
  * @return -1 when response queue is full, 0 on success.
  */
-static inline int blk_enqueue_resp(blk_queue_handle_t *queue_handle,
+static inline int blk_enqueue_resp(blk_queue_handle_t *h,
                                         blk_response_status_t status,
                                         uintptr_t addr,
                                         uint16_t count,
                                         uint16_t success_count,
                                         uint32_t id)
 {
-    if (blk_resp_queue_full(queue_handle)) {
+    if (blk_resp_queue_full(h)) {
         return -1;
     }
 
-    RESP_QUEUE(queue_handle)->buffers[RESP_QUEUE(queue_handle)->write_idx % RESP_QUEUE(queue_handle)->size].status = status;
-    RESP_QUEUE(queue_handle)->buffers[RESP_QUEUE(queue_handle)->write_idx % RESP_QUEUE(queue_handle)->size].addr = addr;
-    RESP_QUEUE(queue_handle)->buffers[RESP_QUEUE(queue_handle)->write_idx % RESP_QUEUE(queue_handle)->size].count = count;
-    RESP_QUEUE(queue_handle)->buffers[RESP_QUEUE(queue_handle)->write_idx % RESP_QUEUE(queue_handle)->size].success_count = success_count;
-    RESP_QUEUE(queue_handle)->buffers[RESP_QUEUE(queue_handle)->write_idx % RESP_QUEUE(queue_handle)->size].id = id;
+    h->resp_queue->buffers[h->resp_queue->write_idx % h->resp_queue->size].status = status;
+    h->resp_queue->buffers[h->resp_queue->write_idx % h->resp_queue->size].addr = addr;
+    h->resp_queue->buffers[h->resp_queue->write_idx % h->resp_queue->size].count = count;
+    h->resp_queue->buffers[h->resp_queue->write_idx % h->resp_queue->size].success_count = success_count;
+    h->resp_queue->buffers[h->resp_queue->write_idx % h->resp_queue->size].id = id;
 
     THREAD_MEMORY_RELEASE();
-    RESP_QUEUE(queue_handle)->write_idx++;
+    h->resp_queue->write_idx++;
 
     return 0;
 }
@@ -248,7 +247,7 @@ static inline int blk_enqueue_resp(blk_queue_handle_t *queue_handle,
 /**
  * Dequeue an element from the request queue.
  *
- * @param queue_handle queue handle containing request queue to dequeue from.
+ * @param h queue handle containing request queue to dequeue from.
  * @param code pointer to request code.
  * @param addr pointer to encoded dma address of data.
  * @param block_number pointer to  block number to read/write to.
@@ -257,25 +256,25 @@ static inline int blk_enqueue_resp(blk_queue_handle_t *queue_handle,
  *
  * @return -1 when request queue is empty, 0 on success.
  */
-static inline int blk_dequeue_req(blk_queue_handle_t *queue_handle,
+static inline int blk_dequeue_req(blk_queue_handle_t *h,
                                         blk_request_code_t *code,
                                         uintptr_t *addr,
                                         uint32_t *block_number,
                                         uint16_t *count,
                                         uint32_t *id)
 {
-    if (blk_req_queue_empty(queue_handle)) {
+    if (blk_req_queue_empty(h)) {
         return -1;
     }
 
-    *code = REQ_QUEUE(queue_handle)->buffers[REQ_QUEUE(queue_handle)->read_idx % REQ_QUEUE(queue_handle)->size].code;
-    *addr = REQ_QUEUE(queue_handle)->buffers[REQ_QUEUE(queue_handle)->read_idx % REQ_QUEUE(queue_handle)->size].addr;
-    *block_number = REQ_QUEUE(queue_handle)->buffers[REQ_QUEUE(queue_handle)->read_idx % REQ_QUEUE(queue_handle)->size].block_number;
-    *count = REQ_QUEUE(queue_handle)->buffers[REQ_QUEUE(queue_handle)->read_idx % REQ_QUEUE(queue_handle)->size].count;
-    *id = REQ_QUEUE(queue_handle)->buffers[REQ_QUEUE(queue_handle)->read_idx % REQ_QUEUE(queue_handle)->size].id;
+    *code = h->req_queue->buffers[h->req_queue->read_idx % h->req_queue->size].code;
+    *addr = h->req_queue->buffers[h->req_queue->read_idx % h->req_queue->size].addr;
+    *block_number = h->req_queue->buffers[h->req_queue->read_idx % h->req_queue->size].block_number;
+    *count = h->req_queue->buffers[h->req_queue->read_idx % h->req_queue->size].count;
+    *id = h->req_queue->buffers[h->req_queue->read_idx % h->req_queue->size].id;
 
     THREAD_MEMORY_RELEASE();
-    REQ_QUEUE(queue_handle)->read_idx++;
+    h->req_queue->read_idx++;
 
     return 0;
 }
@@ -283,7 +282,7 @@ static inline int blk_dequeue_req(blk_queue_handle_t *queue_handle,
 /**
  * Dequeue an element from a response queue.
  *
- * @param queue_handle queue handle containing response queue to dequeue from.
+ * @param h queue handle containing response queue to dequeue from.
  * @param status pointer to response status.
  * @param addr pointer to encoded dma address of data.
  * @param count pointer to number of blocks allocated for corresponding request
@@ -291,25 +290,25 @@ static inline int blk_dequeue_req(blk_queue_handle_t *queue_handle,
  * @param id pointer to stoqueue request ID to idenfity which request this response is for.
  * @return -1 when response queue is empty, 0 on success.
  */
-static inline int blk_dequeue_resp(blk_queue_handle_t *queue_handle,
+static inline int blk_dequeue_resp(blk_queue_handle_t *h,
                                         blk_response_status_t *status,
                                         uintptr_t *addr,
                                         uint16_t *count,
                                         uint16_t *success_count,
                                         uint32_t *id)
 {
-    if (blk_resp_queue_empty(queue_handle)) {
+    if (blk_resp_queue_empty(h)) {
         return -1;
     }
 
-    *status = RESP_QUEUE(queue_handle)->buffers[RESP_QUEUE(queue_handle)->read_idx % RESP_QUEUE(queue_handle)->size].status;
-    *addr = RESP_QUEUE(queue_handle)->buffers[RESP_QUEUE(queue_handle)->read_idx % RESP_QUEUE(queue_handle)->size].addr;
-    *count = RESP_QUEUE(queue_handle)->buffers[RESP_QUEUE(queue_handle)->read_idx % RESP_QUEUE(queue_handle)->size].count;
-    *success_count = RESP_QUEUE(queue_handle)->buffers[RESP_QUEUE(queue_handle)->read_idx % RESP_QUEUE(queue_handle)->size].success_count;
-    *id = RESP_QUEUE(queue_handle)->buffers[RESP_QUEUE(queue_handle)->read_idx % RESP_QUEUE(queue_handle)->size].id;
+    *status = h->resp_queue->buffers[h->resp_queue->read_idx % h->resp_queue->size].status;
+    *addr = h->resp_queue->buffers[h->resp_queue->read_idx % h->resp_queue->size].addr;
+    *count = h->resp_queue->buffers[h->resp_queue->read_idx % h->resp_queue->size].count;
+    *success_count = h->resp_queue->buffers[h->resp_queue->read_idx % h->resp_queue->size].success_count;
+    *id = h->resp_queue->buffers[h->resp_queue->read_idx % h->resp_queue->size].id;
 
     THREAD_MEMORY_RELEASE();
-    RESP_QUEUE(queue_handle)->read_idx++;
+    h->resp_queue->read_idx++;
 
     return 0;
 }
@@ -317,29 +316,29 @@ static inline int blk_dequeue_resp(blk_queue_handle_t *queue_handle,
 /**
  * Set the plug of the request queue to true.
  *
- * @param queue_handle queue handle containing request queue to check for plug.
+ * @param h queue handle containing request queue to check for plug.
 */
-static inline void blk_req_queue_plug(blk_queue_handle_t *queue_handle) {
-    REQ_QUEUE(queue_handle)->plugged = true;
+static inline void blk_req_queue_plug(blk_queue_handle_t *h) {
+    h->req_queue->plugged = true;
 }
 
 /**
  * Set the plug of the request queue to false.
  *
- * @param queue_handle queue handle containing request queue to check for plug.
+ * @param h queue handle containing request queue to check for plug.
 */
-static inline void blk_req_queue_unplug(blk_queue_handle_t *queue_handle) {
-    REQ_QUEUE(queue_handle)->plugged = false;
+static inline void blk_req_queue_unplug(blk_queue_handle_t *h) {
+    h->req_queue->plugged = false;
 }
 
 /**
  * Check the current value of the request queue plug.
  *
- * @param queue_handle queue handle containing request queue to check for plug.
+ * @param h queue handle containing request queue to check for plug.
  *
  * @return true when request queue is plugged, false when unplugged.
 */
-static inline bool blk_req_queue_plugged(blk_queue_handle_t *queue_handle) {
-    return REQ_QUEUE(queue_handle)->plugged;
+static inline bool blk_req_queue_plugged(blk_queue_handle_t *h) {
+    return h->req_queue->plugged;
 }
 
