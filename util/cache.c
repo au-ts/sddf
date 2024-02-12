b488850bbb2fd9b911a57eff5c47658fc3328b13
James Archer <j.archer@unsw.edu.au>
2024-02-12 12:27:31 +1100

    Add description to util/cache.c
    Co-authored-by: Ivan Velickovic <10481259+Ivan-Velickovic@users.noreply.github.com>
    Signed-off-by: James Archer <j.archer@unsw.edu.au>

--- util/cache.c
+++ util/cache.c
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include <sddf/util/cache.h>
 #include <sddf/util/util.h>
-
+/* This is a small utility library for performing manual cache operations on AArch64 from user-level. The primary use-case is for managing regions of memory that are mapped as cached but are accessible by DMA capable devices. */
 #ifndef CONFIG_AARCH64_USER_CACHE_ENABLE
 #error "CONFIG_AARCH64_USER_CACHE_ENABLE must be enabled"
 #endif
