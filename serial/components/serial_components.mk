f3c950c040c6db8e515f32fb17229d13463d716c
Peter Chubb <Peter.Chubb@unsw.edu.au>
2024-03-18 06:20:04 +1100

    Add build snippet for serial virtualiser
    Use by
      include ${SDDF}/serial_components/serial_components.mk
    
    to create serial_RX_virtualiser.elf and serial_TX_virtualiser.elf
    
    Signed-off-by: Peter Chubb <Peter.Chubb@unsw.edu.au>

---
+++ serial/components/serial_components.mk
@@ -0,0 +1,39 @@
+#
+# Copyright 2023, UNSW
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# This Makefile snippet builds the serial RX and TX virtualisers
+# it should be included into your project Makefile
+#
+# It relies on the variable SERIAL_NUM_CLIENTS to configure the virtualisers
+#
+
+ifeq ($(strip $(SERIAL_NUM_CLIENTS)),)
+$(error Specify the number of clients for the serial virtualisers.  Expect -DSERIAL_NUM_CLIENTS=3 or similar)
+endif
+ifeq ($(strip $(UART_DRIVER)),)
+$(error The serial virtualisers need headers from the UART source. PLease specify UART_DRIVER)
+endif
+
+CFLAGS_serial := -I ${SDDF}/include -I${UART_DRIVER}/include -I${SDDF}/util/include ${SERIAL_NUM_CLIENTS} 
+serial_RX_virtualiser.elf: virt_rx.o
+	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
+serial_TX_virtualiser.elf: virt_tx.o
+	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
+
+virt_rx.o: ${SDDF}/serial/components/virt_rx.c
+	${CC} ${CFLAGS} ${CFLAGS_serial} -o $@ -c ${SERIAL_NUM_CLIENTS} $<
+
+virt_tx.o: ${SDDF}/serial/components/virt_tx.c
+	${CC} ${CFLAGS} ${CFLAGS_serial} -o $@ -c ${SERIAL_NUM_CLIENTS} $<
+
+clean::
+	rm -f mux_[rt]x.[od]
+
+clobber::
+	rm -f serial_RX_virtualiser.elf serial_TX_virtualiser.elf
+
+
+-include mux_rx.d
+-include mux_tx.d
