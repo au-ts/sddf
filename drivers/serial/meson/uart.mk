63ed58742bb49624c565f6e4b860781c25b7f998
Peter Chubb <Peter.Chubb@unsw.edu.au>
2024-03-18 06:21:29 +1100

    Build snippet for UART

---
+++ drivers/serial/meson/uart.mk
@@ -0,0 +1,20 @@
+#
+# Copyright 2024, UNSW
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Include this snippet in your project Makefile to build
+# the Meson UART driver
+
+UART_DRIVER:= ${SDDF}/drivers/serial/meson
+
+uart_driver.elf: uart_driver.o
+	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
+
+uart_driver.o: ${SDDF}/drivers/serial/meson/uart.c
+	$(CC) -c $(CFLAGS) -I${UART_DRIVER}/include -o $@ $< 
+
+-include uart_driver.d
+
+clean::
+	rm -f uart_driver.[do]
