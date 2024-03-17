50eae067034a35052bfecc1b7e6c9a7e3af9d7a3
Ivan Velickovic <10481259+Ivan-Velickovic@users.noreply.github.com>
2024-03-18 09:30:51 +1100

    net: introduce helper for registering IP with ARP (#61)
    Rather than open-code the call to the ARP component, use a helper.

---
+++ include/sddf/network/arp.h
@@ -0,0 +1,10 @@
+#include <microkit.h>
+
+bool arp_register_ipv4(microkit_channel arp_ch, uint32_t ipv4_addr, uint8_t mac[6]) {
+    microkit_mr_set(0, ipv4_addr);
+    microkit_mr_set(1, (mac[0] << 8) | mac[1]);
+    microkit_mr_set(2, (mac[2] << 24) | (mac[3] << 16) | (mac[4] << 8) | mac[5]);
+    microkit_ppcall(arp_ch, microkit_msginfo_new(0, 3));
+
+    return true;
+}
