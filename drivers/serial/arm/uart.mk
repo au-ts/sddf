c022918522bafc325aaddab9ca96ffd6a6299c88
Peter Chubb <Peter.Chubb@unsw.edu.au>
2024-03-19 12:33:58 +1100

    Add Makefile snippets
    Add Makefile snippets for serial components, and for the util directory.
    
    Signed-off-by: Peter Chubb <Peter.Chubb@unsw.edu.au>

--- drivers/serial/arm/uart.mk
+++ drivers/serial/arm/uart.mk
@@ -4,19 +4,18 @@
 # SPDX-License-Identifier: BSD-2-Clause
 #
 # Include this snippet in your project Makefile to build
-# the Meson UART driver
+# the PL011 UART driver
 
-UART_DRIVER:= ${SDDF}/drivers/serial/meson
+UART_DRIVER:= ${SDDF}/drivers/serial/arm
 
 uart_driver.elf: uart_driver.o sddf_libutil.a
 	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
 
-uart_driver.o: ${SDDF}/drivers/serial/meson/uart.c
+uart_driver.o: ${SDDF}/drivers/serial/arm/uart.c
 	$(CC) -c $(CFLAGS) -I${UART_DRIVER}/include -o $@ $< 
 
 -include uart_driver.d
 
 include ${SDDF}/util/util.mk
-
 clean::
 	rm -f uart_driver.[do]
