42f613b44679a3613660eae9088bd0db828cf262
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-03-19 13:21:03 +1100

    net: fix more renames to queues

--- examples/echo_server/include/ethernet_config/ethernet_config.h
+++ examples/echo_server/include/ethernet_config/ethernet_config.h
@@ -63,12 +63,12 @@ _Static_assert(RX_DATA_REGION_SIZE_CLI1 >= RX_QUEUE_SIZE_CLI1 * NET_BUFFER_SIZE,
 #define MAX(a,b) (((a) > (b)) ? (a) : (b))
 
 _Static_assert(NET_MAX_BUFFERS >= TX_QUEUE_SIZE_DRIV, "Queue capacity must be >= largest TX queue.");
-_Static_assert(NET_MAX_BUFFERS >= MAX(RX_QUEUE_SIZE_DRIV, MAX(RX_QUEUE_SIZE_CLI0, RX_QUEUE_SIZE_CLI1)), "Shared ring buffer capacity must be >=  largest RX ring.");
-_Static_assert(TX_QUEUE_SIZE_DRIV >= TX_QUEUE_SIZE_ARP + TX_QUEUE_SIZE_CLI0 + TX_QUEUE_SIZE_CLI1, "Driver TX ring buffer must have capacity to fit all of client's TX buffers.");
-_Static_assert(RX_QUEUE_SIZE_ARP >= RX_QUEUE_SIZE_DRIV, "Arp ring buffers must have capacity to fit all rx buffers.");
-_Static_assert(RX_QUEUE_SIZE_COPY0 >= RX_QUEUE_SIZE_DRIV, "Copy0 ring buffers must have capacity to fit all RX buffers.");
-_Static_assert(RX_QUEUE_SIZE_COPY1 >= RX_QUEUE_SIZE_DRIV, "Copy1 ring buffers must have capacity to fit all RX buffers.");
-_Static_assert(sizeof(net_queue_t) <= DATA_REGION_SIZE, "Ring buffer must fit into a single data region.");
+_Static_assert(NET_MAX_BUFFERS >= MAX(RX_QUEUE_SIZE_DRIV, MAX(RX_QUEUE_SIZE_CLI0, RX_QUEUE_SIZE_CLI1)), "Shared queue capacity must be >=  largest RX ring.");
+_Static_assert(TX_QUEUE_SIZE_DRIV >= TX_QUEUE_SIZE_ARP + TX_QUEUE_SIZE_CLI0 + TX_QUEUE_SIZE_CLI1, "Driver TX queue must have capacity to fit all of client's TX buffers.");
+_Static_assert(RX_QUEUE_SIZE_ARP >= RX_QUEUE_SIZE_DRIV, "Arp queues must have capacity to fit all rx buffers.");
+_Static_assert(RX_QUEUE_SIZE_COPY0 >= RX_QUEUE_SIZE_DRIV, "Copy0 queues must have capacity to fit all RX buffers.");
+_Static_assert(RX_QUEUE_SIZE_COPY1 >= RX_QUEUE_SIZE_DRIV, "Copy1 queues must have capacity to fit all RX buffers.");
+_Static_assert(sizeof(net_queue_t) <= DATA_REGION_SIZE, "net_queue_t must fit into a single data region.");
 
 static bool __str_match(const char *s0, const char *s1)
 {
