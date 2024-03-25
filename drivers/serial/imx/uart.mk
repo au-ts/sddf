48bad9f0c623d9043cce232cff909b2127464894
Peter Chubb <Peter.Chubb@unsw.edu.au>
2024-03-25 17:35:00 +1100

    Add IMX Uart snippet
    Signed-off-by: Peter Chubb <Peter.Chubb@unsw.edu.au>

---
+++ drivers/serial/imx/uart.mk
@@ -0,0 +1,21 @@
+#
+# Copyright 2024, UNSW
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Include this snippet in your project Makefile to build
+# the IMX8 UART driver
+
+UART_DRIVER:= ${SDDF}/drivers/serial/imx
+
+uart_driver.elf: uart_driver.o sddf_libutil.a
+	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
+
+uart_driver.o: ${SDDF}/drivers/serial/arm/uart.c
+	$(CC) -c $(CFLAGS) -I${UART_DRIVER}/include -o $@ $< 
+
+-include uart_driver.d
+
+include ${SDDF}/util/util.mk
+clean::
+	rm -f uart_driver.[do]
