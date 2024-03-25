b888adb84ca5e205b6b9ab6d1cd7619ca955bf25
Courtney Darville <courtneydarville94@outlook.com>
2024-03-25 19:07:38 +1100

    Stop bench and idle from crashing in debug and add fault printing in bench

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -12,6 +12,7 @@
 #include <sddf/util/fence.h>
 #include <sddf/util/util.h>
 #include <sddf/util/printf.h>
+#include <ethernet_config.h>
 
 #define LOG_BUFFER_CAP 7
 
@@ -59,6 +60,21 @@ event_id_t benchmarking_events[] = {
     SEL4BENCH_EVENT_BRANCH_MISPREDICT,
 };
 
+static void print_pdid_name(uint64_t pd_id) {
+        switch (pd_id) {
+        case PD_ETH_ID: sddf_printf(ETH_NAME); break;
+        case PD_VIRT_RX_ID: sddf_printf(VIRT_RX_NAME); break;
+        case PD_VIRT_TX_ID: sddf_printf(VIRT_TX_NAME); break;
+        case PD_COPY_ID: sddf_printf(COPY0_NAME); break;
+        case PD_COPY1_ID: sddf_printf(COPY1_NAME); break;
+        case PD_LWIP_ID: sddf_printf(CLI0_NAME); break;
+        case PD_LWIP1_ID: sddf_printf(CLI1_NAME); break;
+        case PD_ARP_ID: sddf_printf(ARP_NAME); break;
+        case PD_TIMER_ID: sddf_printf(TIMER_NAME); break;
+        default: sddf_printf("unknown"); break;
+    }
+}
+
 #ifdef CONFIG_BENCHMARK_TRACK_UTILISATION
 static void microkit_benchmark_start(void)
 {
@@ -100,21 +116,13 @@ static void microkit_benchmark_stop_tcb(uint64_t pd_id, uint64_t *total, uint64_
 
 static void print_benchmark_details(uint64_t pd_id, uint64_t kernel_util, uint64_t kernel_entries, uint64_t number_schedules, uint64_t total_util)
 {
-    if (pd_id == PD_TOTAL) sddf_printf("Total utilisation details: ");
-    else sddf_printf("Utilisation details for PD: ");
-    switch (pd_id) {
-        case PD_ETH_ID: sddf_printf("ETH DRIVER"); break;
-        case PD_VIRT_RX_ID: sddf_printf("VIRT RX"); break;
-        case PD_VIRT_TX_ID: sddf_printf("VIRT TX"); break;
-        case PD_COPY_ID: sddf_printf("COPIER0"); break;
-        case PD_COPY1_ID: sddf_printf("COPIER1"); break;
-        case PD_LWIP_ID: sddf_printf("LWIP CLIENT0"); break;
-        case PD_LWIP1_ID: sddf_printf("LWIP CLIENT1"); break;
-        case PD_ARP_ID: sddf_printf("ARP"); break;
-        case PD_TIMER_ID: sddf_printf("TIMER"); break;
+    if (pd_id == PD_TOTAL) sddf_printf("Total utilisation details: \n");
+    else {
+        sddf_printf("Utilisation details for PD: ");
+        print_pdid_name(pd_id);
+        sddf_printf(" (%llx)\n", pd_id);
     }
-    if (pd_id != PD_TOTAL) sddf_printf(" (%lx)", pd_id);
-    sddf_printf("\n{\nKernelUtilisation:  %lx\nKernelEntries:  %lx\nNumberSchedules:  %lx\nTotalUtilisation:  %lx\n}\n",
+    sddf_printf("{\nKernelUtilisation:  %lx\nKernelEntries:  %lx\nNumberSchedules:  %lx\nTotalUtilisation:  %lx\n}\n",
             kernel_util, kernel_entries, number_schedules, total_util);
 }
 #endif
@@ -231,6 +239,7 @@ void notified(microkit_channel ch)
 
 void init(void)
 {
+    #ifdef MICROKIT_CONFIG_benchmark
     sel4bench_init();
     seL4_Word n_counters = sel4bench_get_num_counters();
 
@@ -245,6 +254,9 @@ void init(void)
     sel4bench_reset_counters();
     sel4bench_start_counters(mask);
     benchmark_bf = mask;
+    #else
+    sddf_dprintf("BENCH|LOG: Bench running in debug mode, no access to counters\n");
+    #endif
 
     /* Notify the idle thread that the sel4bench library is initialised. */
     microkit_notify(INIT);
@@ -254,4 +266,41 @@ void init(void)
     if (res_buf) sddf_printf("Could not set log buffer:  %llx\n", res_buf);
     else sddf_printf("Log buffer set\n");
 #endif
+}
+
+void fault(microkit_id id, microkit_msginfo msginfo) {
+    sddf_printf("BENCH|LOG: Faulting PD ");
+    print_pdid_name(id);
+    sddf_printf(" (%llx)\n", id);
+
+    seL4_UserContext regs;
+    seL4_TCB_ReadRegisters(BASE_TCB_CAP + id, false, 0, sizeof(seL4_UserContext) / sizeof(seL4_Word), &regs);
+    sddf_printf("Registers: \npc : %llx\nspsr : %llx\nx0 : %llx\nx1 : %llx\nx2 : %llx\nx3 : %llx\nx4 : %llx\nx5 : %llx\nx6 : %llx\nx7 : %llx\n", 
+                    regs.pc, regs.spsr, regs.x0, regs.x1, regs.x2, regs.x3, regs.x4, regs.x5, regs.x6, regs.x7);
+
+    switch (microkit_msginfo_get_label(msginfo))
+    {
+        case seL4_Fault_CapFault: {
+            uint64_t ip = seL4_GetMR(seL4_CapFault_IP);
+            uint64_t fault_addr = seL4_GetMR(seL4_CapFault_Addr);
+            uint64_t in_recv_phase = seL4_GetMR(seL4_CapFault_InRecvPhase);
+            sddf_printf("CapFault: ip=%llx  fault_addr=%llx  in_recv_phase=%llx\n", ip, fault_addr, (in_recv_phase == 0 ? "false" : "true"));
+            break;
+        }
+        case seL4_Fault_UserException: {
+            sddf_printf("UserException\n");
+            break;
+        }
+        case seL4_Fault_VMFault: {
+            uint64_t ip = seL4_GetMR(seL4_VMFault_IP);
+            uint64_t fault_addr = seL4_GetMR(seL4_VMFault_Addr);
+            uint64_t is_instruction = seL4_GetMR(seL4_VMFault_PrefetchFault);
+            uint64_t fsr = seL4_GetMR(seL4_VMFault_FSR);
+            sddf_printf("VMFault: ip=%llx  fault_addr=%llx  fsr=%llx %s\n", ip, fault_addr, fsr, (is_instruction ? "(instruction fault)" : "(data fault)"));
+            break;
+        }
+        default:
+            sddf_printf("Unknown fault\n");
+            break;
+    }
 }
