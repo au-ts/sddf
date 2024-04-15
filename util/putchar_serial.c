423205b6f98f1d3b7334a15cac44e6f19435c8cc
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-04-15 16:00:20 +1000

    examples/echo_server: add QEMU support
    Co-authored-by: Krishnan Winter <krishnanwinter1@gmail.com>
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- util/putchar_serial.c
+++ util/putchar_serial.c
@@ -5,7 +5,6 @@ extern uintptr_t uart_base;
 #define REG_PTR(offset) ((volatile uint32_t *)((uart_base) + (offset)))
 
 #if defined(CONFIG_PLAT_IMX8MM_EVK) || defined(CONFIG_PLAT_MAAXBOARD)
-
 #define UART_STATUS 0x98
 #define TRANSMIT 0x40
 #define STAT_TDRE (1 << 14)
@@ -15,11 +14,7 @@ void _sddf_putchar(char character)
     while (!(*REG_PTR(UART_STATUS) & STAT_TDRE)) { }
     *REG_PTR(TRANSMIT) = character;
 }
-
-#endif
-
-#ifdef CONFIG_PLAT_ODROIDC4
-
+#elif defined(CONFIG_PLAT_ODROIDC4)
 #define UART_STATUS 0xC
 #define UART_WFIFO 0x0
 #define UART_TX_FULL (1 << 21)
@@ -29,5 +24,16 @@ void _sddf_putchar(char character)
     while ((*REG_PTR(UART_STATUS) & UART_TX_FULL)) {}
     *REG_PTR(UART_WFIFO) = character & 0x7f;
 }
+#elif defined(CONFIG_PLAT_QEMU_ARM_VIRT)
+#define UARTDR 0x000
+#define UARTFR 0x018
+#define PL011_UARTFR_TXFF (1 << 5)
 
+void _sddf_putchar(char character)
+{
+    while ((*REG_PTR(UARTFR) & PL011_UARTFR_TXFF) != 0);
+    *REG_PTR(UARTDR) = character;
+}
+#else
+#error "Missing sDDF serial putchar function"
 #endif
