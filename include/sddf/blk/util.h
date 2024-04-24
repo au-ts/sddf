d43a56909196f1d6ccbdcd09a8ab7974145ccc48
Eric Chan <eric.chan2@unsw.edu.au>
2024-04-24 13:22:30 +1000

    change memcpy to be externally linked
    Signed-off-by: Eric Chan <eric.chan2@unsw.edu.au>

--- include/sddf/blk/util.h
+++ include/sddf/blk/util.h
@@ -1,15 +1,7 @@
 #include <stddef.h>
 
 // @ericc: When we have libc implementation replace this
-static void *memcpy(void *restrict dest, const void *restrict src, size_t n)
-{
-    unsigned char *d = dest;
-    const unsigned char *s = src;
-    for (; n; n--) {
-        *d++ = *s++;
-    }
-    return dest;
-}
 /* We need an externally linked memset definition for
    copying structs */
+void *memcpy(void *restrict dest, const void *restrict src, size_t n);
 void *memset(void *dest, int c, size_t n);
