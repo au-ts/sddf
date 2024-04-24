57cb5e011a4d836deb0afc0f8f7c1a981add50e6
Eric Chan <eric.chan2@unsw.edu.au>
2024-04-24 13:22:30 +1000

    change memcpy to be externally linked
    Signed-off-by: Eric Chan <eric.chan2@unsw.edu.au>

--- blk/util/util.c
+++ blk/util/util.c
@@ -8,3 +8,13 @@ void *memset(void *dest, int c, size_t n)
     }
     return dest;
 }
+
+void *memcpy(void *restrict dest, const void *restrict src, size_t n)
+{
+    unsigned char *d = dest;
+    const unsigned char *s = src;
+    for (; n; n--) {
+        *d++ = *s++;
+    }
+    return dest;
+}
