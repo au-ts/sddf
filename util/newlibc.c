83ae130b089422f31f6f452598371c131ec751dc
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-05-06 10:22:58 +1000

    Remove checked in libc
    This was a hack done to make it easier to develop but turns out
    the toolchain we use packages newlibc which can be easily used
    instead.
    
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

---
+++ util/newlibc.c
@@ -0,0 +1,34 @@
+#include <microkit.h>
+
+/*
+ * This source file is intended to be compiled with other code
+ * that links with the newlib C library. Newlib expects these
+ * function definitions to exist. At the time of writing, we do
+ * not depend on any of the OS-level calls and hence leave them
+ * unimplemented.
+ */
+
+void _close(void)
+{
+    microkit_dbg_puts("NEWLIB ERROR: calling unimplemented _close()");
+}
+
+void _lseek(void)
+{
+    microkit_dbg_puts("NEWLIB ERROR: calling unimplemented _lseek()");
+}
+
+void _read(void)
+{
+    microkit_dbg_puts("NEWLIB ERROR: calling unimplemented _read()");
+}
+
+void _write(void)
+{
+    microkit_dbg_puts("NEWLIB ERROR: calling unimplemented _write()");
+}
+
+void _sbrk(void)
+{
+    microkit_dbg_puts("NEWLIB ERROR: calling unimplemented _sbrk()");
+}
