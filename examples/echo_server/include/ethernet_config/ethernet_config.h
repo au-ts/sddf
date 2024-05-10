91f7d0cc4b21359eb00487e48c7466c97d7d1fac
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-05-10 11:39:29 +1000

    examples/echo_server: style ethernet config
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- examples/echo_server/include/ethernet_config/ethernet_config.h
+++ examples/echo_server/include/ethernet_config/ethernet_config.h
@@ -42,9 +42,12 @@
 #define TX_DATA_REGION_SIZE_CLI0            DATA_REGION_SIZE
 #define TX_DATA_REGION_SIZE_CLI1            DATA_REGION_SIZE
 
-_Static_assert(TX_DATA_REGION_SIZE_ARP >= TX_QUEUE_SIZE_ARP * NET_BUFFER_SIZE, "Arp TX data region size must fit Arp TX buffers");
-_Static_assert(TX_DATA_REGION_SIZE_CLI0 >= TX_QUEUE_SIZE_CLI0 * NET_BUFFER_SIZE, "Client0 TX data region size must fit Client0 TX buffers");
-_Static_assert(TX_DATA_REGION_SIZE_CLI1 >= TX_QUEUE_SIZE_CLI1 * NET_BUFFER_SIZE, "Client1 TX data region size must fit Client1 TX buffers");
+_Static_assert(TX_DATA_REGION_SIZE_ARP >= TX_QUEUE_SIZE_ARP *NET_BUFFER_SIZE,
+               "Arp TX data region size must fit Arp TX buffers");
+_Static_assert(TX_DATA_REGION_SIZE_CLI0 >= TX_QUEUE_SIZE_CLI0 *NET_BUFFER_SIZE,
+               "Client0 TX data region size must fit Client0 TX buffers");
+_Static_assert(TX_DATA_REGION_SIZE_CLI1 >= TX_QUEUE_SIZE_CLI1 *NET_BUFFER_SIZE,
+               "Client1 TX data region size must fit Client1 TX buffers");
 
 #define RX_QUEUE_SIZE_DRIV                   512
 #define RX_QUEUE_SIZE_ARP                    RX_QUEUE_SIZE_DRIV
@@ -57,18 +60,23 @@ _Static_assert(TX_DATA_REGION_SIZE_CLI1 >= TX_QUEUE_SIZE_CLI1 * NET_BUFFER_SIZE,
 #define RX_DATA_REGION_SIZE_CLI0            DATA_REGION_SIZE
 #define RX_DATA_REGION_SIZE_CLI1            DATA_REGION_SIZE
 
-_Static_assert(RX_DATA_REGION_SIZE_DRIV >= RX_QUEUE_SIZE_DRIV * NET_BUFFER_SIZE, "Driver RX data region size must fit Driver RX buffers");
-_Static_assert(RX_DATA_REGION_SIZE_CLI0 >= RX_QUEUE_SIZE_CLI0 * NET_BUFFER_SIZE, "Client0 RX data region size must fit Client0 RX buffers");
-_Static_assert(RX_DATA_REGION_SIZE_CLI1 >= RX_QUEUE_SIZE_CLI1 * NET_BUFFER_SIZE, "Client1 RX data region size must fit Client1 RX buffers");
+_Static_assert(RX_DATA_REGION_SIZE_DRIV >= RX_QUEUE_SIZE_DRIV *NET_BUFFER_SIZE,
+               "Driver RX data region size must fit Driver RX buffers");
+_Static_assert(RX_DATA_REGION_SIZE_CLI0 >= RX_QUEUE_SIZE_CLI0 *NET_BUFFER_SIZE,
+               "Client0 RX data region size must fit Client0 RX buffers");
+_Static_assert(RX_DATA_REGION_SIZE_CLI1 >= RX_QUEUE_SIZE_CLI1 *NET_BUFFER_SIZE,
+               "Client1 RX data region size must fit Client1 RX buffers");
 
 #define MAX(a,b) (((a) > (b)) ? (a) : (b))
 
 #define ETH_MAX_QUEUE_SIZE MAX(TX_QUEUE_SIZE_DRIV, MAX(RX_QUEUE_SIZE_DRIV, MAX(RX_QUEUE_SIZE_CLI0, RX_QUEUE_SIZE_CLI1)))
-_Static_assert(TX_QUEUE_SIZE_DRIV >= TX_QUEUE_SIZE_ARP + TX_QUEUE_SIZE_CLI0 + TX_QUEUE_SIZE_CLI1, "Driver TX queue must have capacity to fit all of client's TX buffers.");
+_Static_assert(TX_QUEUE_SIZE_DRIV >= TX_QUEUE_SIZE_ARP + TX_QUEUE_SIZE_CLI0 + TX_QUEUE_SIZE_CLI1,
+               "Driver TX queue must have capacity to fit all of client's TX buffers.");
 _Static_assert(RX_QUEUE_SIZE_ARP >= RX_QUEUE_SIZE_DRIV, "Arp queues must have capacity to fit all rx buffers.");
 _Static_assert(RX_QUEUE_SIZE_COPY0 >= RX_QUEUE_SIZE_DRIV, "Copy0 queues must have capacity to fit all RX buffers.");
 _Static_assert(RX_QUEUE_SIZE_COPY1 >= RX_QUEUE_SIZE_DRIV, "Copy1 queues must have capacity to fit all RX buffers.");
-_Static_assert(sizeof(net_queue_t) + ETH_MAX_QUEUE_SIZE * sizeof(net_buff_desc_t) <= DATA_REGION_SIZE, "net_queue_t must fit into a single data region.");
+_Static_assert(sizeof(net_queue_t) + ETH_MAX_QUEUE_SIZE *sizeof(net_buff_desc_t) <= DATA_REGION_SIZE,
+               "net_queue_t must fit into a single data region.");
 
 static inline bool __str_match(const char *s0, const char *s1)
 {
@@ -94,7 +102,7 @@ static inline void cli_mac_addr_init_sys(char *pd_name, uint8_t *macs)
         __set_mac_addr(macs, MAC_ADDR_CLI0);
     } else if (__str_match(pd_name, CLI1_NAME)) {
         __set_mac_addr(macs, MAC_ADDR_CLI1);
-    } 
+    }
 }
 
 static inline void arp_mac_addr_init_sys(char *pd_name, uint8_t *macs)
@@ -102,7 +110,7 @@ static inline void arp_mac_addr_init_sys(char *pd_name, uint8_t *macs)
     if (__str_match(pd_name, ARP_NAME)) {
         __set_mac_addr(macs, MAC_ADDR_CLI0);
         __set_mac_addr(&macs[ETH_HWADDR_LEN], MAC_ADDR_CLI1);
-    } 
+    }
 }
 
 static inline void virt_mac_addr_init_sys(char *pd_name, uint8_t *macs)
@@ -110,12 +118,13 @@ static inline void virt_mac_addr_init_sys(char *pd_name, uint8_t *macs)
     if (__str_match(pd_name, VIRT_RX_NAME)) {
         __set_mac_addr(macs, MAC_ADDR_ARP);
         __set_mac_addr(&macs[ETH_HWADDR_LEN], MAC_ADDR_CLI0);
-        __set_mac_addr(&macs[2*ETH_HWADDR_LEN], MAC_ADDR_CLI1);
+        __set_mac_addr(&macs[2 * ETH_HWADDR_LEN], MAC_ADDR_CLI1);
     }
 }
 
-static inline void cli_queue_init_sys(char *pd_name, net_queue_handle_t *rx_queue, uintptr_t rx_free, uintptr_t rx_active,
-                                net_queue_handle_t *tx_queue, uintptr_t tx_free, uintptr_t tx_active)
+static inline void cli_queue_init_sys(char *pd_name, net_queue_handle_t *rx_queue, uintptr_t rx_free,
+                                      uintptr_t rx_active,
+                                      net_queue_handle_t *tx_queue, uintptr_t tx_free, uintptr_t tx_active)
 {
     if (__str_match(pd_name, CLI0_NAME)) {
         net_queue_init(rx_queue, (net_queue_t *) rx_free, (net_queue_t *) rx_active, RX_QUEUE_SIZE_CLI0);
@@ -126,8 +135,9 @@ static inline void cli_queue_init_sys(char *pd_name, net_queue_handle_t *rx_queu
     }
 }
 
-static inline void copy_queue_init_sys(char *pd_name, net_queue_handle_t *cli_queue, uintptr_t cli_free, uintptr_t cli_active,
-                                net_queue_handle_t *virt_queue, uintptr_t virt_free, uintptr_t virt_active)
+static inline void copy_queue_init_sys(char *pd_name, net_queue_handle_t *cli_queue, uintptr_t cli_free,
+                                       uintptr_t cli_active,
+                                       net_queue_handle_t *virt_queue, uintptr_t virt_free, uintptr_t virt_active)
 {
     if (__str_match(pd_name, COPY0_NAME)) {
         net_queue_init(cli_queue, (net_queue_t *) cli_free, (net_queue_t *) cli_active, RX_QUEUE_SIZE_CLI0);
@@ -138,20 +148,26 @@ static inline void copy_queue_init_sys(char *pd_name, net_queue_handle_t *cli_qu
     }
 }
 
-static inline void virt_queue_init_sys(char *pd_name, net_queue_handle_t *cli_queue, uintptr_t cli_free, uintptr_t cli_active)
+static inline void virt_queue_init_sys(char *pd_name, net_queue_handle_t *cli_queue, uintptr_t cli_free,
+                                       uintptr_t cli_active)
 {
     if (__str_match(pd_name, VIRT_RX_NAME)) {
         net_queue_init(cli_queue, (net_queue_t *) cli_free, (net_queue_t *) cli_active, RX_QUEUE_SIZE_ARP);
-        net_queue_init(&cli_queue[1], (net_queue_t *) (cli_free + 2 * DATA_REGION_SIZE), (net_queue_t *) (cli_active + 2 * DATA_REGION_SIZE), RX_QUEUE_SIZE_COPY0);
-        net_queue_init(&cli_queue[2], (net_queue_t *) (cli_free + 4 * DATA_REGION_SIZE), (net_queue_t *) (cli_active + 4 * DATA_REGION_SIZE), RX_QUEUE_SIZE_COPY1);
+        net_queue_init(&cli_queue[1], (net_queue_t *)(cli_free + 2 * DATA_REGION_SIZE),
+                       (net_queue_t *)(cli_active + 2 * DATA_REGION_SIZE), RX_QUEUE_SIZE_COPY0);
+        net_queue_init(&cli_queue[2], (net_queue_t *)(cli_free + 4 * DATA_REGION_SIZE),
+                       (net_queue_t *)(cli_active + 4 * DATA_REGION_SIZE), RX_QUEUE_SIZE_COPY1);
     } else if (__str_match(pd_name, VIRT_TX_NAME)) {
         net_queue_init(cli_queue, (net_queue_t *) cli_free, (net_queue_t *) cli_active, TX_QUEUE_SIZE_ARP);
-        net_queue_init(&cli_queue[1], (net_queue_t *) (cli_free + 2 * DATA_REGION_SIZE), (net_queue_t *) (cli_active + 2 * DATA_REGION_SIZE), TX_QUEUE_SIZE_CLI0);
-        net_queue_init(&cli_queue[2], (net_queue_t *) (cli_free + 4 * DATA_REGION_SIZE), (net_queue_t *) (cli_active + 4 * DATA_REGION_SIZE), TX_QUEUE_SIZE_CLI1);
+        net_queue_init(&cli_queue[1], (net_queue_t *)(cli_free + 2 * DATA_REGION_SIZE),
+                       (net_queue_t *)(cli_active + 2 * DATA_REGION_SIZE), TX_QUEUE_SIZE_CLI0);
+        net_queue_init(&cli_queue[2], (net_queue_t *)(cli_free + 4 * DATA_REGION_SIZE),
+                       (net_queue_t *)(cli_active + 4 * DATA_REGION_SIZE), TX_QUEUE_SIZE_CLI1);
     }
 }
 
-static inline void mem_region_init_sys(char *pd_name, uintptr_t *mem_regions, uintptr_t start_region) {
+static inline void mem_region_init_sys(char *pd_name, uintptr_t *mem_regions, uintptr_t start_region)
+{
     if (__str_match(pd_name, VIRT_TX_NAME)) {
         mem_regions[0] = start_region;
         mem_regions[1] = start_region + DATA_REGION_SIZE;
