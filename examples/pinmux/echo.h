250d085a96e8f1d71d07f03d20b804113892dbc8
michaelm <michaelcmaheraus@yahoo.com.au>
2024-05-22 17:37:09 +1000

    Skeleton complete + scanning of device tree complete

---
+++ examples/pinmux/echo.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2022, UNSW
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#define UDP_ECHO_PORT 1235
+#define UTILIZATION_PORT 1236
+
+#define LINK_SPEED 1000000000 // Gigabit
+#define ETHER_MTU 1500
+
+int setup_udp_socket(void);
+int setup_utilization_socket(void);
