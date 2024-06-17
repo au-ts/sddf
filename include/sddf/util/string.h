efe5f54cf4a5fedf0acf8f32e0ea781f7d825b4c
Courtney Darville <courtneydarville94@outlook.com>
2024-06-17 17:48:00 +1000

    Remove builtin ifdefs that don't always work and fix sddf string function definitions
    Signed-off-by: Courtney Darville <courtneydarville94@outlook.com>

--- include/sddf/util/string.h
+++ include/sddf/util/string.h
@@ -15,37 +15,26 @@
 
 static inline void *sddf_memset(void *s, int c, size_t n)
 {
-#if __has_builtin(__builtin_memset)
-    return __builtin_memset(s, c, n);
-#else
     unsigned char *p = s;
     while (n-- > 0) {
         *p++ = c;
     }
     return s;
-#endif
 }
 
 static inline void *sddf_memcpy(void *dest, const void *src, size_t n)
 {
-#if __has_builtin(__builtin_memcpy)
-    return __builtin_memcpy(dest, src, n);
-#else
     unsigned char *to = dest;
     const unsigned char *from = src;
     while (n-- > 0) {
         *to++ = *from++;
     }
     return dest;
-#endif
 }
 
 static inline char *sddf_strncpy(char *dest, const char *restrict src,
                                  size_t dsize)
 {
-#if __has_builtin(__builtin_strcpy)
-    return __builtin_strncpy(dest, src, dsize);
-#else
     char *to = dest;
     while (dsize-- && (*to = *src++)) {
         to++;
@@ -54,101 +43,76 @@ static inline char *sddf_strncpy(char *dest, const char *restrict src,
         *to++ = '\0';
     }
     return dest;
-#endif
 }
 
 static inline int sddf_strcmp(const char *a, const char *b)
 {
-#if __has_builtin(__builtin_strcmp)
-    return __builtin_strcmp(a, b);
-#else
     while (*a != '\0' && *b != '\0' && *a == *b) {
         a++;
         b++;
     }
     return (int)(*a) - *b;
-#endif
 }
 
 static inline int sddf_strncmp(const char *a, const char *b, size_t n)
 {
-#if __has_builtin(__builtin_strncmp)
-    return __builtin_strncmp(a, b, n);
-#else
     for (size_t i = 0; i < n; i++) {
         if (a[i] == '\0' || b[i] == '\0' || a[i] != b[i]) {
             return (int)a[i] - b[i];
         }
     }
     return 0;
-#endif
 }
 
 static inline char *sddf_strchr(const char *s, int c)
 {
-#if __has_builtin(__builtin_strncmp)
-    return __builtin_strchr(s, c);
-#else
     while (*s != '\0') {
         if (*s == c) {
-            return s;
+            return (char *)s;
         }
         s++;
     }
     if (c == '\0') {
-        return s;
+        return (char *)s;
     }
     return NULL;
-#endif
 }
 
 static inline int sddf_memcmp(const void *a, const void *b, size_t n)
 {
-#if __has_builtin(__builtin_memcmp)
-    return __builtin_memcmp(a, b, n);
-#else
-    unsigned char *_a = a;
-    unsigned char *_b = b;
+    unsigned char *_a = (unsigned char *)a;
+    unsigned char *_b = (unsigned char *)b;
     for (size_t i = 0; i < n; i++) {
         if (_a[i] != _b[i]) {
             return (int)_a[i] - (int)_b[i];
         }
     }
     return 0;
-#endif
 }
 
 
 static inline size_t sddf_strlen(const char *s)
 {
-#if __has_builtin(__builtin_strlen)
-    return __builtin_strlen(s);
-#else
-    const char *_s;
+    const char *_s = s;
     while (*_s != '\0') {
         _s++;
     }
-    return (size_t)(s_ - s);
-#endif
+    return (size_t)(_s - s);
 }
 
 
 static inline void *sddf_memmove(void *dest, const void *src, size_t n)
 {
-#if __has_builtin(__builtin_memmove)
-    return __builtin_memmove(dest, src, n);
-#else
     if (dest == src) {
         return dest;
     }
     int copy_backwards = dest > src;
     for (size_t i = 0; i < n; i++) {
         if (copy_backwards) {
-            dest[n - 1 - i] = src[n - 1 - i];
+            ((uint8_t *)dest)[n - 1 - i] = ((uint8_t *)src)[n - 1 - i];
         } else {
-            dest[i] = src[i];
+            ((uint8_t *)dest)[i] = ((uint8_t *)src)[i];
         }
     }
     return dest;
-#endif
 }
