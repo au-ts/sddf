au-ts/sddf

commit 22d208b2903c74a8088e9df5bedb71686309515d

move microkit dependency to c file and add ifndef to ARRAY_SIZE definition
Signed-off-by: Courtney Darville <courtneydarville94@outlook.com>

include/sddf/util/util.h
@@ -6,9 +6,10 @@
 #pragma once
 
 #include <stddef.h>
-#include <microkit.h>
 
+#ifndef ARRAY_SIZE
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
+#endif
 #define ALIGN(x, align)   (((x) + (align) - 1) & ~((align) - 1))
 
 #define BIT(nr) (1UL << (nr))

util/cache.c
@@ -1,3 +1,4 @@
+#include <microkit.h>
 #include <stdint.h>
 #include <sddf/util/cache.h>
 #include <sddf/util/util.h>

commit a56968a24e64394afa7d8d11fb698ac70c019ad1

Fix assert from being undefined
Broke in 22d208b2903c74a8088e9df5bedb71686309515d.

Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

include/sddf/util/util.h
@@ -6,6 +6,7 @@
 #pragma once
 
 #include <stddef.h>
+#include <microkit.h>
 
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))