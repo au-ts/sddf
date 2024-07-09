78487b45dbb3b990cf3c49fcaf52d063a7f55c28
Alex Brown <alexandermichaelbrown@outlook.com>
2024-07-09 16:58:22 +1000

    Move client count from Makefile to *_config.h
    Signed-off-by: Alex Brown <alexandermichaelbrown@outlook.com>

--- network/components/network_components.mk
+++ network/components/network_components.mk
@@ -9,14 +9,7 @@
 #
 # NOTES:
 # Generates network_virt_rx.elf network_virt_tx.elf arp.elf copy.elf
-# Relies on the variable NUM_NETWORK_CLIENTS to be a flag for
-# the C compiler to configure the virtualisers
 # Requires ${SDDF}/util/util.mk to build the utility library for debug output
-
-ifeq ($(strip $(NUM_NETWORK_CLIENTS)),)
-$(error Specify the number of clients for the network virtualisers.  Expect -DNUM_NETWORK_CLIENTS=3 or similar)
-endif
-
 
 NETWORK_COMPONENTS_DIR := $(abspath $(dir $(lastword ${MAKEFILE_LIST})))
 NETWORK_IMAGES:= network_virt_rx.elf network_virt_tx.elf arp.elf copy.elf
@@ -24,8 +17,6 @@ network/components/%.o: ${SDDF}/network/components/%.c
 	${CC} ${CFLAGS} -c -o $@ $<
 
 NETWORK_COMPONENT_OBJ := $(addprefix network/components/, copy.o arp.o network_virt_tx.o network_virt_rx.o)
-
-CFLAGS_network += ${NUM_NETWORK_CLIENTS}
 
 CHECK_NETWORK_FLAGS_MD5:=.network_cflags-$(shell echo -- ${CFLAGS} ${CFLAGS_network} | shasum | sed 's/ *-//')
 
