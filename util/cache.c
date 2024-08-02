0caa427f4e924c5b28b9cac18a10677af5bf2ea5
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-08-02 12:38:11 +1000

    Cleanup comments in cache.c
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- util/cache.c
+++ util/cache.c
@@ -2,7 +2,13 @@
 #include <stdint.h>
 #include <sddf/util/cache.h>
 #include <sddf/util/util.h>
-/* This is a small utility library for performing manual cache operations on AArch64 from user-level. The primary use-case is for managing regions of memory that are mapped as cached but are accessible by DMA capable devices. */
+
+/*
+ * This is a small utility library for performing manual cache operations on
+ * AArch64 from user-level. The primary use-case is for managing regions of
+ * memory that are mapped as cached but are accessible by DMA capable devices.
+ */
+
 #ifndef CONFIG_AARCH64_USER_CACHE_ENABLE
 #error "CONFIG_AARCH64_USER_CACHE_ENABLE must be enabled"
 #error "seL4 must be configured with CONFIG_AARCH64_USER_CACHE_ENABLE"
@@ -43,12 +49,7 @@ void cache_clean_and_invalidate(unsigned long start, unsigned long end)
 {
     unsigned long line;
     unsigned long index;
-    /* Clean the L1 range */
 
-    /* Finally clean and invalidate the L1 range. The extra clean is only strictly neccessary
-     * in a multiprocessor environment to prevent a write being lost if another core is
-     * attempting a store at the same time. As the range should already be clean asking
-     * it to clean again should not affect performance */
     for (index = LINE_INDEX(start); index < LINE_INDEX(end) + 1; index++) {
         line = index << CONFIG_L1_CACHE_LINE_SIZE_BITS;
         clean_and_invalidate_by_va(line);
