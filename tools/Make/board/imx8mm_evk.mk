7744f877dd81ec374278395e0cc52d753cbb36d2
Peter Chubb <Peter.Chubb@unsw.edu.au>
2024-08-26 15:24:43 +1000

    Abstract away board and toolchain details
    Put the common bits in a makefile snippet.
    
    Signed-off-by: Peter Chubb <Peter.Chubb@unsw.edu.au>

---
+++ tools/Make/board/imx8mm_evk.mk
@@ -0,0 +1,16 @@
+#
+# Copyright 2023, UNSW
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Set up variables for IMX8MM_evk
+# Should be included _before_ toolchain makefile.
+PLATFORM := imx
+NET_DRIV_DIR := ${PLATFORM}
+ETH_DRIV := eth_driver_${PLATFORM}.elf
+UART_DRIV_DIR := ${PLATFORM}
+TIMER_DRIV_DIR := ${PLATFORM}
+I2C_DRIV_DIR := ${PLATFORM}
+CPU := cortex-a53
+TRIPLE := aarch64-none-elf
+BOARD_DIR := $(MICROKIT_SDK)/board/$(MICROKIT_BOARD)/$(MICROKIT_CONFIG)
---
+++ tools/Make/board/star64.mk
@@ -0,0 +1,12 @@
+#
+# Copyright 2023, UNSW
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+BOARD_DIR := $(MICROKIT_SDK)/board/$(MICROKIT_BOARD)/$(MICROKIT_CONFIG)
+PLATFORM := riscv
+NET_DRIV_DIR := dwmac-5.10a
+ETH_DRIV := eth_driver_dwmac-5.10a.elf
+UART_DRIV_DIR := ns16550a
+TIMER_DRIV_DIR := jh7110
+TRIPLE := aarch64-none-elf
