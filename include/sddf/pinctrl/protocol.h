586eb282fd4c90f743a6ce7220f5c883cd207dda
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2024-08-27 16:30:12 +1000

    pinctrl: add query DTS value call to driver and improve client interface
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- include/sddf/pinctrl/protocol.h
+++ include/sddf/pinctrl/protocol.h
@@ -7,12 +7,40 @@
 
 /* Shared functionality/definitions between pinctrl drivers and clients */
 
-enum ipc_index {
-    REGISTER_OFFSET_WORD,
-    REGISTER_VALUE_WORD
+enum sddf_pinctrl_request_label {
+    SDDF_PINCTRL_RESERVED = 0,
+    SDDF_PINCTRL_SET_MUX,
+    SDDF_PINCTRL_QUERY_DTS,
 };
 
-enum request_label {
-    SDDF_PINCTRL_RESERVED,
-    SDDF_PINCTRL_SET_MUX
+
+enum set_mux_request_ipc_index {
+    SET_MUX_REQ_OFFSET = 0,
+    SET_MUX_REQ_VALUE,
+    SET_MUX_REQ_NUM_ARGS,
 };
+
+enum set_mux_reponse_ipc_index {
+    SET_MUX_RESP_NUM_RESULTS = 0,
+};
+
+
+enum query_dts_request_ipc_index {
+    QUERY_DTS_REQ_OFFSET = 0,
+    QUERY_DTS_REQ_NUM_ARGS,
+};
+
+enum query_dts_reponse_ipc_index {
+    QUERY_DTS_RESP_VALUE = 0,
+    QUERY_DTS_RESP_NUM_RESULTS,
+};
+
+
+enum sddf_pinctrl_request_response_label {
+    SDDF_PINCTRL_SUCCESS = 0,
+    SDDF_PINCTRL_UNKNOWN_REQ,
+    SDDF_PINCTRL_INVALID_ARGS,
+    SDDF_PINCTRL_NOT_IN_DTS,
+};
+
+typedef enum sddf_pinctrl_request_response_label sddf_pinctrl_response_t;
