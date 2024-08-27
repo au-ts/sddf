a2339501f3c4e2c005ec086498cb6518491394cb
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2024-08-27 17:08:09 +1000

    pinctrl: fix style in DTS python script
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- drivers/pinctrl/imx/create_pinctrl_config.py
+++ drivers/pinctrl/imx/create_pinctrl_config.py
@@ -29,7 +29,7 @@ def get_pinctrl_info(device_nodes):
         if pin_info != None:
             # At this stage, we have the array of values
             # Since each device configuration comes in a set of six values, we'll loop through in sets of 6
-            for i in range(len(pin_info.value)//(6*UINT32_T_SIZE)):
+            for i in range(len(pin_info.value) // (6 * UINT32_T_SIZE)):
                 return_dict['mux_reg'].append(get_value_from_bytes_array(pin_info.value, 6*i))
                 return_dict['conf_reg'].append(get_value_from_bytes_array(pin_info.value, 6*i+1))
                 return_dict['input_reg'].append(get_value_from_bytes_array(pin_info.value, 6*i+2))
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     devicetree = dtlib.DT(sys.argv[2], force=True)
     out_dir = sys.argv[3]
 
-    # For the imx8mm-evk, we have to locate the "pinctrl" device in the dts to be able to get our relevant info
+    # For the imx8mq, we have to locate the "pinctrl" device in the dts to be able to get our relevant info
     for node in devicetree.node_iter():
         if "iomuxc" in node.name:
             pinmux_dict = get_pinctrl_info(node.nodes[soc_name])
