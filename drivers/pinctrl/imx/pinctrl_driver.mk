910412c1a51b2adad2ac0b32d9ef716d70e4c85e
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2024-08-28 13:58:26 +1000

    pinctrl: add parameters validation to driver makefile snippet
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- drivers/pinctrl/imx/pinctrl_driver.mk
+++ drivers/pinctrl/imx/pinctrl_driver.mk
@@ -22,13 +22,35 @@
 
 #  These two memory region must be contiguous, with iomuxc_device coming before iomuxc_gpr.
 
+ifndef PYTHON
+$(error PYTHON is not set)
+endif
+
+ifndef DTS_FILE 
+$(error DTS_FILE is not set)
+endif
+
+ifndef PINMUX_DEVICE 
+$(error PINMUX_DEVICE is not set)
+endif
+
+ifndef SOC 
+$(error SOC is not set)
+endif
+
+ifeq (imx8mm-evk,$(findstring imx8mm-evk,$(SOC)))
+else ifeq (imx8mq-evk,$(findstring imx8mq-evk,$(SOC)))
+else
+$(error Pinctrl driver: unsupported SoC: $(SOC))
+endif
+
 PINCTRL_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
 pinctrl_driver.elf: pinctrl/pinctrl.o pinctrl/pinctrl_config_data.o
 	${LD} ${LDFLAGS} $? ${LIBS} -o $@
 
 pinctrl/pinctrl.o: $(PINCTRL_DIR)/pinctrl.c pinctrl
-	${CC} ${CFLAGS} -DCONFIG_DEBUG_BUILD -c $< -o $@
+	${CC} ${CFLAGS} -DCONFIG_DEBUG_BUILD -DSOC_$(shell echo $(SOC) | tr a-z A-Z | tr - _) -c $< -o $@
 
 pinctrl/pinctrl_config_data.o: pinctrl/pinctrl_config_data.s
 	${AS} $< -o $@
