3cc85247fb72e40bdb5d5fa2e0699d264b1ac686
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2024-08-28 10:55:46 +1000

    pinctrl: update driver makefile snippet parameters docs and python invocation
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- drivers/pinctrl/imx/pinctrl_driver.mk
+++ drivers/pinctrl/imx/pinctrl_driver.mk
@@ -8,17 +8,24 @@
 #
 # NOTES
 #  Generates pinctrl.elf
-#  Has X parameters: TODO
+#  Has 4 parameters: 
+#    PYTHON
+#    DTS_FILE: absolute path to the device tree source file.
+#    SOC: System-on-Chip name that referenced by the pinmux device in DTS.
+#    PINMUX_DEVICE: name of the pinmux device in DTS, e.g. imx8mq calls it "iomuxc" whereas imx8mm calls it "pinctrl"
+
 #  Expects variable iomuxc_device to be set in the system description file to the
 #    mapped address of the I/O Mux controller.
+
 #  Expects variable iomuxc_gpr to be set in the system description file to the
 #    mapped address of the General Purpose Registers (GPR) of the Mux controller.
+
 #  These two memory region must be contiguous, with iomuxc_device coming before iomuxc_gpr.
 
 PINCTRL_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
 pinctrl_driver.elf: pinctrl/pinctrl.o pinctrl/pinctrl_config_data.o
-	$(LD) $(LDFLAGS) $? $(LIBS) -o $@
+	${LD} ${LDFLAGS} $? ${LIBS} -o $@
 
 pinctrl/pinctrl.o: $(PINCTRL_DIR)/pinctrl.c pinctrl
 	${CC} ${CFLAGS} -DCONFIG_DEBUG_BUILD -c $< -o $@
@@ -27,7 +34,7 @@ pinctrl/pinctrl_config_data.o: pinctrl/pinctrl_config_data.s
 	${AS} $< -o $@
 
 pinctrl/pinctrl_config_data.s:
-	python3 $(PINCTRL_DIR)/create_pinctrl_config.py imx8mq-evk $(TOP)/dts/maaxboard.dts pinctrl
+	${PYTHON} ${PINCTRL_DIR}/create_pinctrl_config.py ${SOC} ${DTS_FILE} ${PINMUX_DEVICE} pinctrl
 
 pinctrl: 
 	mkdir -p pinctrl
