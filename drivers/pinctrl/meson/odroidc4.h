c18b296118c099647b861d7efe410ea1b5f1c55f
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2024-09-04 16:47:10 +1000

    pinctrl: add WIP default odroidc4 pinmux data values
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

---
+++ drivers/pinctrl/meson/odroidc4.h
@@ -0,0 +1,103 @@
+// Copyright 2024, UNSW
+// SPDX-License-Identifier: BSD-2-Clause
+
+// Document referenced: Chapter 7.9 of Amlogic S905X3 Datasheet revision 02.
+
+// Unlike odroidc4.py, this file simply define the default pinmux values
+
+#include <stdint.h>
+
+// Even though these default values are set during a watchdog event,
+// it is better to set them up again to remove u-boot's initialisation effort so that
+// our driver can be properly tested.
+
+// Final address = base + (offset * 4)
+
+typedef struct {
+    uint32_t offset;
+    uint32_t value;
+} default_pindata_t;
+
+// "Always-On" domain
+// Contains input-output state of the pad
+#define AO_GPIO_I_OFFSET             0x0A
+#define AO_GPIO_O_OFFSET             0x0D
+// Enable this pad to be used as a GPIO if not connected to any port
+#define AO_GPIO_O_EN_N_OFFSET        0x09
+// Enable biasing
+#define AO_RTI_PULL_UP_REG_OFFSET    0x0B
+// Pull up or down if biasing is enabled
+#define AO_RTI_PULL_UP_EN_REG_OFFSET 0x0C
+// Drive strength, 4 possible values: { 500uA, 2500uA, 3000uA, 4000uA }
+#define AO_PAD_DS_A_OFFSET           0x07
+#define AO_PAD_DS_B_OFFSET           0x08
+// Muxing registers, connect this pad to what port
+#define AO_RTI_PINMUX_REG0_OFFSET    0x05
+#define AO_RTI_PINMUX_REG1_OFFSET    0x06
+
+// Some registers above are excluded if they are read-only
+default_pindata_t default_ao_pinmux[] = {
+    { .offset = AO_RTI_PINMUX_REG0_OFFSET, .value = 0x0 },
+    { .offset = AO_RTI_PINMUX_REG1_OFFSET, .value = 0x0 },
+    { .offset = AO_GPIO_O_EN_N_OFFSET, .value = 0xFFFFFFFF },
+    { .offset = AO_GPIO_O_OFFSET, .value = 0xFFFFFFFF },
+    { .offset = AO_RTI_PULL_UP_EN_REG_OFFSET, .value = 0x80040FFF },
+    { .offset = AO_RTI_PULL_UP_REG_OFFSET, .value = 0x800005AB },
+    { .offset = AO_PAD_DS_A_OFFSET, .value = 0xAAAAAAAA },
+    { .offset = AO_PAD_DS_B_OFFSET, .value = 0xAAAAAAAA }
+};
+
+// Peripherals domain, the same definitions in AO domain applies.
+#define PERIPHS_PIN_MUX_0_OFFSET   0xB0
+#define PERIPHS_PIN_MUX_1_OFFSET   0xB1
+#define PERIPHS_PIN_MUX_2_OFFSET   0xB2
+#define PERIPHS_PIN_MUX_3_OFFSET   0xB3
+#define PERIPHS_PIN_MUX_4_OFFSET   0xB4
+#define PERIPHS_PIN_MUX_5_OFFSET   0xB5
+#define PERIPHS_PIN_MUX_6_OFFSET   0xB6
+#define PERIPHS_PIN_MUX_7_OFFSET   0xB7
+#define PERIPHS_PIN_MUX_8_OFFSET   0xB8
+#define PERIPHS_PIN_MUX_9_OFFSET   0xB9
+#define PERIPHS_PIN_MUX_A_OFFSET   0xBA
+#define PERIPHS_PIN_MUX_B_OFFSET   0xBB
+#define PERIPHS_PIN_MUX_C_OFFSET   0xBC
+#define PERIPHS_PIN_MUX_D_OFFSET   0xBD
+#define PERIPHS_PIN_MUX_E_OFFSET   0xBE
+#define PERIPHS_PIN_MUX_F_OFFSET   0xBF
+#define PAD_DS_REG0A_OFFSET        0xd0
+#define PAD_DS_REG1A_OFFSET        0xd1
+#define PAD_DS_REG2A_OFFSET        0xd2
+#define PAD_DS_REG2B_OFFSET        0xd3
+#define PAD_DS_REG3A_OFFSET        0xd4
+#define PAD_DS_REG4A_OFFSET        0xd5
+#define PAD_DS_REG5A_OFFSET        0xd6
+#define PAD_PULL_UP_REG0_OFFSET    0x3A
+#define PAD_PULL_UP_REG1_OFFSET    0x3B
+#define PAD_PULL_UP_REG2_OFFSET    0x3C
+#define PAD_PULL_UP_REG3_OFFSET    0x3D
+#define PAD_PULL_UP_REG4_OFFSET    0x3E
+#define PAD_PULL_UP_REG5_OFFSET    0x3F
+#define PAD_PULL_UP_EN_REG0_OFFSET 0x48
+#define PAD_PULL_UP_EN_REG1_OFFSET 0x49
+#define PAD_PULL_UP_EN_REG2_OFFSET 0x4A
+#define PAD_PULL_UP_EN_REG3_OFFSET 0x4B
+#define PAD_PULL_UP_EN_REG4_OFFSET 0x4C
+#define PAD_PULL_UP_EN_REG5_OFFSET 0x4D
+#define PREG_PAD_GPIO0_EN_N_OFFSET 0x10
+#define PREG_PAD_GPIO0_O_OFFSET    0x11
+#define PREG_PAD_GPIO0_I_OFFSET    0x12
+#define PREG_PAD_GPIO1_EN_N_OFFSET 0x13
+#define PREG_PAD_GPIO1_O_OFFSET    0x14
+#define PREG_PAD_GPIO1_I_OFFSET    0x15
+#define PREG_PAD_GPIO2_EN_N_OFFSET 0x16
+#define PREG_PAD_GPIO2_O_OFFSET    0x17
+#define PREG_PAD_GPIO2_I_OFFSET    0x18
+#define PREG_PAD_GPIO3_EN_N_OFFSET 0x19
+#define PREG_PAD_GPIO3_O_OFFSET    0x1a
+#define PREG_PAD_GPIO3_I_OFFSET    0x1b
+#define PREG_PAD_GPIO4_EN_N_OFFSET 0x1c
+#define PREG_PAD_GPIO4_O_OFFSET    0x1d
+#define PREG_PAD_GPIO4_I_OFFSET    0x1e
+#define PREG_PAD_GPIO5_EN_N_OFFSET 0x20
+#define PREG_PAD_GPIO5_O_OFFSET    0x21
+#define PREG_PAD_GPIO5_I_OFFSET    0x22
