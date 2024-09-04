325235311cd4653a0a5fa0aea116dae3d53e0715
Peter Chubb <Peter.Chubb@unsw.edu.au>
2024-09-04 15:18:34 +1000

    Add imx8mp_evk board
    This also works for the Compulab iotg-imx8plus board.
    
    Signed-off-by: Peter Chubb <Peter.Chubb@unsw.edu.au>

--- include/sddf/network/constants.h
+++ include/sddf/network/constants.h
@@ -31,6 +31,6 @@ struct ethernet_header {
  * cannot generate checksums on transmit. We use this macro
  * to know whether to calculate it in the IP stack.
  */
-#if defined(CONFIG_PLAT_IMX8MM_EVK) || defined(CONFIG_PLAT_MAAXBOARD)
+#if defined(CONFIG_PLAT_IMX8MM_EVK) || defined(CONFIG_PLAT_MAAXBOARD) || defined(CONFIG_PLAT_IMX8MP_EVK)
 #define NETWORK_HW_HAS_CHECKSUM
 #endif
