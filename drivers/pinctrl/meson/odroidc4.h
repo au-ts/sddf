c95d98cd8c36a9af97e5802ae153b75f1a1dc101
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2024-09-06 10:12:20 +1000

    pinctrl: complete odroidc4 default pinmux initialisation table
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- drivers/pinctrl/meson/odroidc4.h
+++ drivers/pinctrl/meson/odroidc4.h
@@ -11,12 +11,12 @@
 // it is better to set them up again to remove u-boot's initialisation effort so that
 // our driver can be properly tested.
 
-// Final address = base + (offset * 4)
+#define REG_ADDR(base, offset) base + (offset * 4)
 
 typedef struct {
     uint32_t offset;
     uint32_t value;
-} default_pindata_t;
+} pindata_t;
 
 // "Always-On" domain
 // Contains input-output state of the pad
@@ -36,7 +36,7 @@ typedef struct {
 #define AO_RTI_PINMUX_REG1_OFFSET    0x06
 
 // Some registers above are excluded if they are read-only
-default_pindata_t default_ao_pinmux[] = {
+pindata_t default_ao_pinmux[] = {
     { .offset = AO_RTI_PINMUX_REG0_OFFSET, .value = 0x0 },
     { .offset = AO_RTI_PINMUX_REG1_OFFSET, .value = 0x0 },
     { .offset = AO_GPIO_O_EN_N_OFFSET, .value = 0xFFFFFFFF },
@@ -64,13 +64,13 @@ default_pindata_t default_ao_pinmux[] = {
 #define PERIPHS_PIN_MUX_D_OFFSET   0xBD
 #define PERIPHS_PIN_MUX_E_OFFSET   0xBE
 #define PERIPHS_PIN_MUX_F_OFFSET   0xBF
-#define PAD_DS_REG0A_OFFSET        0xd0
-#define PAD_DS_REG1A_OFFSET        0xd1
-#define PAD_DS_REG2A_OFFSET        0xd2
-#define PAD_DS_REG2B_OFFSET        0xd3
-#define PAD_DS_REG3A_OFFSET        0xd4
-#define PAD_DS_REG4A_OFFSET        0xd5
-#define PAD_DS_REG5A_OFFSET        0xd6
+#define PAD_DS_REG0A_OFFSET        0xD0
+#define PAD_DS_REG1A_OFFSET        0xD1
+#define PAD_DS_REG2A_OFFSET        0xD2
+#define PAD_DS_REG2B_OFFSET        0xD3
+#define PAD_DS_REG3A_OFFSET        0xD4
+#define PAD_DS_REG4A_OFFSET        0xD5
+#define PAD_DS_REG5A_OFFSET        0xD6
 #define PAD_PULL_UP_REG0_OFFSET    0x3A
 #define PAD_PULL_UP_REG1_OFFSET    0x3B
 #define PAD_PULL_UP_REG2_OFFSET    0x3C
@@ -93,11 +93,64 @@ default_pindata_t default_ao_pinmux[] = {
 #define PREG_PAD_GPIO2_O_OFFSET    0x17
 #define PREG_PAD_GPIO2_I_OFFSET    0x18
 #define PREG_PAD_GPIO3_EN_N_OFFSET 0x19
-#define PREG_PAD_GPIO3_O_OFFSET    0x1a
-#define PREG_PAD_GPIO3_I_OFFSET    0x1b
-#define PREG_PAD_GPIO4_EN_N_OFFSET 0x1c
-#define PREG_PAD_GPIO4_O_OFFSET    0x1d
-#define PREG_PAD_GPIO4_I_OFFSET    0x1e
+#define PREG_PAD_GPIO3_O_OFFSET    0x1A
+#define PREG_PAD_GPIO3_I_OFFSET    0x1B
+#define PREG_PAD_GPIO4_EN_N_OFFSET 0x1C
+#define PREG_PAD_GPIO4_O_OFFSET    0x1D
+#define PREG_PAD_GPIO4_I_OFFSET    0x1E
 #define PREG_PAD_GPIO5_EN_N_OFFSET 0x20
 #define PREG_PAD_GPIO5_O_OFFSET    0x21
 #define PREG_PAD_GPIO5_I_OFFSET    0x22
+
+// If a register does not have a value here, it is either reserved or read-only.
+pindata_t default_periphs_pinmux[] = {
+    { .offset = PERIPHS_PIN_MUX_0_OFFSET, .value = 0x0 },
+    { .offset = PERIPHS_PIN_MUX_1_OFFSET, .value = 0x0 },
+    { .offset = PERIPHS_PIN_MUX_3_OFFSET, .value = 0x0 },
+    { .offset = PERIPHS_PIN_MUX_4_OFFSET, .value = 0x0 },
+    { .offset = PERIPHS_PIN_MUX_5_OFFSET, .value = 0x0 },
+    { .offset = PERIPHS_PIN_MUX_6_OFFSET, .value = 0x0 },
+    { .offset = PERIPHS_PIN_MUX_7_OFFSET, .value = 0x0 },
+    { .offset = PERIPHS_PIN_MUX_9_OFFSET, .value = 0x0 },
+    { .offset = PERIPHS_PIN_MUX_B_OFFSET, .value = 0x0 },
+    { .offset = PERIPHS_PIN_MUX_C_OFFSET, .value = 0x0 },
+    { .offset = PERIPHS_PIN_MUX_D_OFFSET, .value = 0x0 },
+    { .offset = PERIPHS_PIN_MUX_E_OFFSET, .value = 0x0 },
+    { .offset = PAD_DS_REG0A_OFFSET, .value = 0xAAAAAAAA },
+    { .offset = PAD_DS_REG1A_OFFSET, .value = 0xAAAA9AAA },
+    { .offset = PAD_DS_REG2A_OFFSET, .value = 0x55955AAA },
+    { .offset = PAD_DS_REG2B_OFFSET, .value = 0xAAAAAA55 },
+    { .offset = PAD_DS_REG3A_OFFSET, .value = 0xAAAA55AA },
+    { .offset = PAD_DS_REG4A_OFFSET, .value = 0xAAAAAAA5 },
+    { .offset = PAD_DS_REG5A_OFFSET, .value = 0x5695555A },
+    { .offset = PAD_PULL_UP_REG0_OFFSET, .value = 0x0000CFFF },
+    { .offset = PAD_PULL_UP_REG1_OFFSET, .value = 0x000000FF },
+    { .offset = PAD_PULL_UP_REG2_OFFSET, .value = 0x0005FFBF },
+    { .offset = PAD_PULL_UP_REG3_OFFSET, .value = 0x0000010F },
+    { .offset = PAD_PULL_UP_REG4_OFFSET, .value = 0x0000C1FF },
+    { .offset = PAD_PULL_UP_REG5_OFFSET, .value = 0x0000C000 },
+    { .offset = PAD_PULL_UP_EN_REG0_OFFSET, .value = 0x0000FFFF },
+    { .offset = PAD_PULL_UP_EN_REG1_OFFSET, .value = 0x000000FF },
+    { .offset = PAD_PULL_UP_EN_REG2_OFFSET, .value = 0x0007FFFF },
+    { .offset = PAD_PULL_UP_EN_REG3_OFFSET, .value = 0x000001FF },
+    { .offset = PAD_PULL_UP_EN_REG4_OFFSET, .value = 0x00003FFF },
+    { .offset = PAD_PULL_UP_EN_REG5_OFFSET, .value = 0x0000FFFF },
+    { .offset = PREG_PAD_GPIO0_EN_N_OFFSET, .value = 0xFFFFFFFF },
+    { .offset = PREG_PAD_GPIO0_O_OFFSET, .value = 0xFFFFFFFF },
+    { .offset = PREG_PAD_GPIO0_I_OFFSET, .value = 0x0 },
+    { .offset = PREG_PAD_GPIO1_EN_N_OFFSET, .value = 0xFFFFFFFF },
+    { .offset = PREG_PAD_GPIO1_O_OFFSET, .value = 0xFFFFFFFF },
+    { .offset = PREG_PAD_GPIO1_I_OFFSET, .value = 0x0 },
+    { .offset = PREG_PAD_GPIO2_EN_N_OFFSET, .value = 0xFFFFFFFF },
+    { .offset = PREG_PAD_GPIO2_O_OFFSET, .value = 0xFFFFFFFF },
+    { .offset = PREG_PAD_GPIO2_I_OFFSET, .value = 0x0 },
+    { .offset = PREG_PAD_GPIO3_EN_N_OFFSET, .value = 0xFFFFFFFF },
+    { .offset = PREG_PAD_GPIO3_O_OFFSET, .value = 0xFFFFFFFF },
+    { .offset = PREG_PAD_GPIO3_I_OFFSET, .value = 0x0 },
+    { .offset = PREG_PAD_GPIO4_EN_N_OFFSET, .value = 0xFFFFFFFF },
+    { .offset = PREG_PAD_GPIO4_O_OFFSET, .value = 0xFFFFFFFF },
+    { .offset = PREG_PAD_GPIO4_I_OFFSET, .value = 0x0 },
+    { .offset = PREG_PAD_GPIO5_EN_N_OFFSET, .value = 0xFFFFFFFF },
+    { .offset = PREG_PAD_GPIO5_O_OFFSET, .value = 0xFFFFFFFF },
+    { .offset = PREG_PAD_GPIO5_I_OFFSET, .value = 0x0 }
+};
