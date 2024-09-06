93a3ce6fd00657908ee1eeff1205a963ffc12c01
James Archer <j.archer@unsw.edu.au>
2024-09-06 14:54:51 +1000

    examples/timer: use printf
    Signed-off-by: James Archer <j.archer@unsw.edu.au>

--- examples/timer/client.c
+++ examples/timer/client.c
@@ -6,50 +6,32 @@
 #include <stdint.h>
 #include <microkit.h>
 #include <sddf/timer/client.h>
+#include <sddf/util/printf.h>
 
 #define TIMER_CHANNEL 1
 
-static void
-put64(uint64_t val) {
-    char buffer[20];
-    unsigned i = 19;
-    buffer[19] = 0;
-    do {
-        uint8_t c = val % 10;
-        buffer[--i] = '0' + c;
-        val /= 10;
-    } while (val);
-    microkit_dbg_puts(&buffer[i]);
-}
-
-void
-notified(microkit_channel ch)
+void notified(microkit_channel ch)
 {
     /*
      * In this example we only have a single possible channel,
      * so we know it's a notification from the driver telling
      * us that the timeout we set has gone off.
      */
-    microkit_dbg_puts("CLIENT|INFO: Got a timeout!\n");
+    sddf_printf("CLIENT|INFO: Got a timeout!\n");
     /* Get the current time */
     uint64_t time = sddf_timer_time_now(TIMER_CHANNEL);
-    microkit_dbg_puts("CLIENT|INFO: Now the time (in nanoseconds) is: ");
-    put64(time);
-    microkit_dbg_puts("\n");
+    sddf_printf("CLIENT|INFO: Now the time (in nanoseconds) is: %lu\n", time);
     /* Set another timeout */
     sddf_timer_set_timeout(TIMER_CHANNEL, NS_IN_S);
 }
 
-void
-init(void)
+void init(void)
 {
     // lets get the time!
     uint64_t time = sddf_timer_time_now(TIMER_CHANNEL);
-    microkit_dbg_puts("CLIENT|INFO: The time now is: ");
-    put64(time);
-    microkit_dbg_puts("\n");
+    sddf_printf("CLIENT|INFO: The time now is: %lu\n", time);
 
     // lets set a timeout
-    microkit_dbg_puts("CLIENT|INFO: Setting a time out for 1 second\n");
+    sddf_printf("CLIENT|INFO: Setting a time out for 1 second\n");
     sddf_timer_set_timeout(TIMER_CHANNEL, NS_IN_S);
 }
