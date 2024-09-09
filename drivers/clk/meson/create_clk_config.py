1492e1f2acb25750db90733f37a364e5e106c3ed
Terry Bai <terry.z.bai@gmail.com>
2024-09-09 16:09:49 +1000

    extract clocks from dts

--- drivers/clk/meson/create_clk_config.py
+++ drivers/clk/meson/create_clk_config.py
@@ -2,16 +2,42 @@
 
 # Copyright 2024, UNSW
 # SPDX-License-Identifier: BSD-2-Clause
+#
+# The template is stolen from Bill's pinmux driver
 
 import os
 import sys
 
 from devicetree import edtlib, dtlib
 
+supported_compat_str_board = { "hardkernel,odroid-c4" }
+
+debug_parser = True
+
+def log_normal_parser(print_str: str) -> None:
+    if (debug_parser):
+        print("PARSER|INFO: " + print_str)
+
+def log_warning_parser(print_str: str) -> None:
+    sys.stderr.write("PARSER|WARNING: " + print_str)
+
+def log_error_parser(print_str: str) -> None:
+    sys.stderr.write("PARSER|ERROR: " + print_str)
+
 if __name__ == "__main__":
     print("Creating a config file for clock driver to intialise the system...")
 
-    print(sys.argv[0])
-    print(sys.argv[1])
-
     devicetree = dtlib.DT(sys.argv[1], force=True)
+    for compat_str in devicetree.root.props["compatible"].to_strings():
+        if compat_str in supported_compat_str_board:
+            supported = True
+            break
+
+    if not supported:
+        log_error_parser("this board is not supported.")
+        exit(1)
+
+    for node in devicetree.node_iter():
+        props = list(node.props.keys())
+        if "status" in props and "clocks" in props:
+            print(node.name, node.props["clocks"])
