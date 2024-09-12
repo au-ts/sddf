873ea4b5f7fd30d43f20fa5e0afb9c458081b8f6
Terry Bai <terry.z.bai@gmail.com>
2024-09-12 13:41:32 +1000

    clk_divider ops

--- drivers/clk/meson/include/clk.h
+++ drivers/clk/meson/include/clk.h
@@ -3,6 +3,47 @@
 
 #include <stdint.h>
 
+#define CLK_SET_RATE_GATE    BIT(0) /* must be gated across rate change */
+#define CLK_SET_PARENT_GATE    BIT(1) /* must be gated across re-parent */
+#define CLK_SET_RATE_PARENT    BIT(2) /* propagate rate change up one level */
+#define CLK_IGNORE_UNUSED    BIT(3) /* do not gate even if unused */
+                /* unused */
+                /* unused */
+#define CLK_GET_RATE_NOCACHE    BIT(6) /* do not use the cached clk rate */
+#define CLK_SET_RATE_NO_REPARENT BIT(7) /* don't re-parent on rate change */
+#define CLK_GET_ACCURACY_NOCACHE BIT(8) /* do not use the cached clk accuracy */
+#define CLK_RECALC_NEW_RATES    BIT(9) /* recalc rates after notifications */
+#define CLK_SET_RATE_UNGATE    BIT(10) /* clock needs to run to set rate */
+#define CLK_IS_CRITICAL        BIT(11) /* do not gate, ever */
+/* parents need enable during gate/ungate, set rate and re-parent */
+#define CLK_OPS_PARENT_ENABLE    BIT(12)
+/* duty cycle call may be forwarded to the parent clock */
+#define CLK_DUTY_CYCLE_PARENT    BIT(13)
+
+#define CLK_GATE_SET_TO_DISABLE        BIT(0)
+#define CLK_GATE_HIWORD_MASK        BIT(1)
+#define CLK_GATE_BIG_ENDIAN        BIT(2)
+
+#define CLK_DIVIDER_ONE_BASED        BIT(0)
+#define CLK_DIVIDER_POWER_OF_TWO    BIT(1)
+#define CLK_DIVIDER_ALLOW_ZERO        BIT(2)
+#define CLK_DIVIDER_HIWORD_MASK        BIT(3)
+#define CLK_DIVIDER_ROUND_CLOSEST    BIT(4)
+#define CLK_DIVIDER_READ_ONLY        BIT(5)
+#define CLK_DIVIDER_MAX_AT_ZERO        BIT(6)
+#define CLK_DIVIDER_BIG_ENDIAN        BIT(7)
+
+#define CLK_MUX_INDEX_ONE        BIT(0)
+#define CLK_MUX_INDEX_BIT        BIT(1)
+#define CLK_MUX_HIWORD_MASK        BIT(2)
+#define CLK_MUX_READ_ONLY        BIT(3) /* mux can't be changed */
+#define CLK_MUX_ROUND_CLOSEST        BIT(4)
+#define CLK_MUX_BIG_ENDIAN        BIT(5)
+
+#define CLK_FRAC_DIVIDER_ZERO_BASED        BIT(0)
+#define CLK_FRAC_DIVIDER_BIG_ENDIAN        BIT(1)
+#define CLK_FRAC_DIVIDER_POWER_OF_TWO_PS    BIT(2)
+
 #define BIT(nr) (1UL << (nr))
 
 static uintptr_t clk_base;
@@ -17,9 +58,11 @@ struct clk_hw {
 
 
 struct clk_ops {
+    unsigned long (*recalc_rate)(struct clk_hw *hw, unsigned long parent_rate);
     int (*set_rate)(struct clk_hw *hw, uint32_t rate, uint32_t parent_rate);
     int (*enable)(struct clk_hw *hw);
     void (*disable)(struct clk_hw *hw);
+    int (*is_enabled)(struct clk_hw *hw);
 };
 
 struct clk {
@@ -47,12 +90,16 @@ struct clk_gate_data {
     uint8_t flags;
 };
 
+struct clk_div_table {
+
+};
+
 struct clk_div_data {
     uint32_t offset;
     uint8_t shift;
     uint8_t width;
     uint8_t flags;
-    /* const struct clk_div_table *table; */
+    const struct clk_div_table *table;
 };
 
 struct clk_fixed_factor_data {
