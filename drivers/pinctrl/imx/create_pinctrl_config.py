16444694dc41898ae44b59eb4e412b61495b7997
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2024-09-13 13:55:16 +1000

    pinctrl: properly handle enabled vs disabled devices for imx
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- drivers/pinctrl/imx/create_pinctrl_config.py
+++ drivers/pinctrl/imx/create_pinctrl_config.py
@@ -1,8 +1,6 @@
 # Copyright 2024, UNSW
 # SPDX-License-Identifier: BSD-2-Clause
 
-# Only works with device tree source files found in https://github.com/seL4/seL4/tree/master/tools/dts
-
 import os
 import sys
 from devicetree import edtlib, dtlib
@@ -14,7 +12,7 @@ def get_value_from_bytes_array(byte_array: bytes, index: int):
     "Extracts a 4-byte integer value from a 'bytes' array at a certain index"
     return int.from_bytes(byte_array[index*UINT32_T_SIZE : (index+1)* UINT32_T_SIZE], 'big', signed=False)
 
-def get_pinctrl_info(device_nodes):
+def get_pinctrl_info(device_nodes, enabled_phandles):
 
     # Our information dictionary will look like this
     return_dict = {
@@ -26,18 +24,34 @@ def get_pinctrl_info(device_nodes):
         'pad_setting': [] # Pad setting value to be applied to `conf_reg`
     }
 
+    device: dtlib.Node
     for device in device_nodes.node_iter():
         pin_info = device.props.get('fsl,pins')
         if pin_info != None:
+            # Check whether the device was enabled
+            if device.props["phandle"].to_num() in enabled_phandles:
+                print(f"pinmux group {device.name} is enabled")
+            else:
+                print(f"pinmux group {device.name} is disabled")
+                continue
+
             # At this stage, we have the array of values
             # Since each device configuration comes in a set of six values, we'll loop through in sets of 6
             for i in range(len(pin_info.value) // (6 * UINT32_T_SIZE)):
-                return_dict['mux_reg'].append(get_value_from_bytes_array(pin_info.value, 6*i))
-                return_dict['conf_reg'].append(get_value_from_bytes_array(pin_info.value, 6*i+1))
-                return_dict['input_reg'].append(get_value_from_bytes_array(pin_info.value, 6*i+2))
-                return_dict['mux_val'].append(get_value_from_bytes_array(pin_info.value, 6*i+3))
-                return_dict['input_val'].append(get_value_from_bytes_array(pin_info.value, 6*i+4))
-                return_dict['pad_setting'].append(get_value_from_bytes_array(pin_info.value, 6*i+5))
+                mux_reg = get_value_from_bytes_array(pin_info.value, 6*i)
+                conf_reg = get_value_from_bytes_array(pin_info.value, 6*i+1)
+                input_reg = get_value_from_bytes_array(pin_info.value, 6*i+2)
+                mux_val = get_value_from_bytes_array(pin_info.value, 6*i+3)
+                input_val = get_value_from_bytes_array(pin_info.value, 6*i+4)
+                pad_setting = get_value_from_bytes_array(pin_info.value, 6*i+5)
+
+                return_dict['mux_reg'].append(mux_reg)
+                return_dict['conf_reg'].append(conf_reg)
+                return_dict['input_reg'].append(input_reg)
+
+                return_dict['mux_val'].append(mux_val)
+                return_dict['input_val'].append(input_val)
+                return_dict['pad_setting'].append(pad_setting)
 
     return return_dict
 
@@ -57,31 +71,43 @@ if __name__ == "__main__":
 
     # Check which devices are actually enabled
     enabled_phandles = set()
+    node: dtlib.Node
     for node in devicetree.node_iter():
-        if "status" in node.props and node.props["status"] == "okay":
-            pass
+        if "status" in node.props.keys() and node.props["status"].to_string() == "okay":
+            # Grab the pinctrl phandle, there's an issue though:
+            # Some device, e.g. mmc@30b40000 of maaxboard for example
+            # have multiple pinmux configuration it can be in:
+            # default, 100mhz or 200mhz, right now we just grab the default state
+            # This need to be revisited in the future if we work on an mmc driver for imx.
+            if "pinctrl-0" in node.props.keys():
+                for phandle in node.props["pinctrl-0"].to_nums():
+                    print(f"device {node.name} is enabled")
+                    enabled_phandles.add(phandle)
 
     # For the imx8mq, we have to locate the "pinctrl" device in the dts to be able to get our relevant info
     for node in devicetree.node_iter():
         if device_name in node.name:
-            pinmux_dict = get_pinctrl_info(node.nodes[soc_name])
+            pinmux_dict = get_pinctrl_info(node.nodes[soc_name], enabled_phandles)
             nums_pin_properties = len(pinmux_dict['mux_reg'])
 
             errored = False
             if len(set(pinmux_dict['mux_reg'])) != len(pinmux_dict['mux_reg']):
-                print("there were duplicate mux registers!")
+                print(f"there were duplicate mux registers!: offsets are {pinmux_dict['mux_reg']}")
                 errored = True
+
             if len(set(pinmux_dict['conf_reg'])) != len(pinmux_dict['conf_reg']):
-                print("there were duplicate config registers!")
+                print(f"there were duplicate config registers!, offsets are {pinmux_dict['conf_reg']}")
                 errored = True
-            if len(set(pinmux_dict['input_reg'])) != len(pinmux_dict['input_reg']):
-                print("there were duplicate input registers!")
+
+            # There can be multiple zero offsets where the input settings are not defined
+            santised_input_regs = [reg for reg in pinmux_dict['input_reg'] if reg != 0]
+            if len(set(santised_input_regs)) != len(santised_input_regs):
+                print(f"there were duplicate input registers!, offsets are {pinmux_dict['input_reg']}")
                 errored = True
+
             if errored:
                 exit(1)
 
-            # This is an interesting way of writing my dict values to an assembly file
-            # It works so I won't bother changing it right now
             with open(out_dir + "/pinctrl_config_data.s", "w") as file:
                 file.write(".section .data\n")
 
