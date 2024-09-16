d982f5331fe362fea3451d8505488f2dbfb13c19
julia <git.ts@trainwit.ch>
2024-09-16 10:54:59 +1000

    ci(style): Use LLVM19 for clang-format
    There was a bug with clang-format-18 where multiplication was
    treated as a type declaration.
    
    Signed-off-by: julia <git.ts@trainwit.ch>

--- shell.nix
+++ shell.nix
@@ -1,11 +1,19 @@
 # Copyright 2024, UNSW
 # SPDX-License-Identifier: BSD-2-Clause
 let
-    pkgs = import <nixpkgs> {};
-    llvm = pkgs.llvmPackages_18;
+    nixpkgs =
+        let rev = "345c263f2f53a3710abe117f28a5cb86d0ba4059";
+        in builtins.fetchTarball {
+            name = "source";
+            url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+            sha256 = "sha256:1llzyzw7a0jqdn7p3px0sqa35jg24v5pklwxdybwbmbyr2q8cf5j";
+        };
+
+    pkgs = import nixpkgs {};
+    llvm = pkgs.llvmPackages_19;
 in
   pkgs.mkShell {
-    buildInputs = with pkgs.buildPackages; [
+    packages = with pkgs; [
         qemu
         gnumake
         llvm.clang
@@ -16,6 +24,7 @@ in
         # for git-clang-format. nix is weird.
         llvm.libclang.python
     ];
+
     # Nix will automatically add flags to Clang that depend
     # on OS functionality we do not provide (e.g stack protection).
     hardeningDisable = [ "all" ];
