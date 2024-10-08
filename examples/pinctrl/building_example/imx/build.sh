6487bda6b90926e19c6bca1ead7f16aea88dda8f
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2024-10-08 15:53:14 +1100

    pinctrl: checkpoint, delete pinctrl example
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- examples/pinctrl/building_example/imx/build.sh
+++
@@ -1,5 +0,0 @@
-rm -rf build/* && make MICROKIT_BOARD=maaxboard && mq.sh run -s maaxboard2 -f build/loader.img -c "ABCXYZ"
-
-# rm -rf build/* && make MICROKIT_BOARD=imx8mq_evk && mq.sh run -s imx8mq -f build/loader.img -c "ABCXYZ"
-
-# rm -rf build/* && make MICROKIT_BOARD=imx8mm_evk && mq.sh run -s imx8mm -f build/loader.img -c "AAAAAA"
