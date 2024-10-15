df9eab7a4a422c8de3c8f8207dfe26c034b7e41b
julia <git.ts@trainwit.ch>
2024-10-15 11:44:55 +1100

    dodgy stuff
    Signed-off-by: julia <git.ts@trainwit.ch>

--- drivers/pcie/starfive/pcie.c
+++ drivers/pcie/starfive/pcie.c
@@ -147,10 +147,81 @@ void device_print(uint8_t bus, uint8_t device, uint8_t function)
     }
 }
 
+void device_ack_irq(uint8_t bus, uint8_t device, uint8_t function, bool to_mask)
+{
+    uintptr_t offset = get_bdf_offset(bus, device, function);
+    assert(offset < PCIE_CONFIG_SIZE);
+    void *config_base = (void *)(pcie_config + offset);
+    volatile pcie_header_t *header = (pcie_header_t *)config_base;
+
+    if (header->vendor_id == PCIE_VENDOR_INVALID) return;
+
+    sddf_dprintf("\nB.D:F: %02x:%02x.%01x\n", bus, device, function);
+    sddf_dprintf("ACK: command register: 0x%04x\n", header->command);
+    sddf_dprintf("ACK: status register: 0x%04x\n", header->status);
+    sddf_dprintf("\t(PIN-based) interrupt disable: %s\n", (header->command & BIT(10)) ? "is disabled" : "is not disabled");
+    sddf_dprintf("\t(PIN-based) interrupt status: %s\n", (header->status & BIT(3)) ? "asserted" : "none");
+
+    /* mask out the interrupt... */
+    // https://elixir.bootlin.com/linux/v5.18-rc4/source/drivers/pci/pci.c#L4595
+    if (to_mask) {
+        header->command |= BIT(10);
+    } else {
+        header->command &= ~BIT(10);
+    }
+
+    // we only access IRQ PIn and Line an Capiblites which are actually comon (TODO)
+    // assert((header->header_type & PCIE_HEADER_TYPE_LAYOUT_MASK) == PCIE_HEADER_TYPE_GENERAL);
+    volatile pcie_header_type0_t *type0_header = (pcie_header_type0_t *)config_base;
+        // 0xff (255) means "unknown" or "no connection" on interrupt line
+    // interrupt pin: 0 = no, 1 => INTA#, 2 => INTB#, 3 => INTC#, 4 => INTD#
+    sddf_printf("\t(PIN-based) interrupt line: %02x, interrupt pin: %02x\n", type0_header->interrupt_line, type0_header->interrupt_pin);
+
+    assert(header->status & BIT(4)); /* assert capabilities list status exists */
+    sddf_dprintf("start capabilities PTR: %u\n", type0_header->capabilities_pointer);
+
+    /* ach capability
+must be DWORD aligned. The bottom two bits of all pointers (including the initial pointer
+at 34h) are reserved and must be implemented as 00b although software must mask them to
+allow for future uses of these bits. A pointer value of 00h is used to indicate the last
+capability in the list.*/
+    uint8_t current_ptr = type0_header->capabilities_pointer & ~0b11;
+    /* process capabilities list to get MSI */
+    while (current_ptr != 0) {
+        volatile uint8_t *capability = (void *)(config_base + current_ptr);
+        /* 6.8.1 of the pci spec */
+        uint8_t cap_id = capability[0];
+        sddf_dprintf("cap ptr: %x, id: %x\n", current_ptr, cap_id);
+        uint8_t PCIE_CAPABILITY_ID_MSI = 0x05; /* #define this. */
+        uint8_t PCIE_CAPABILITY_ID_MSIX = 0x11; /* #define this. */
+        uint8_t PCIE_CAPABILITY_ID_PM = 0x1; /* #define this. */
+        if (cap_id == PCIE_CAPABILITY_ID_MSI) {
+            uint16_t message_control = capability[2] | ((uint16_t)capability[3] << 8);
+            sddf_dprintf("\tmessage control(msi): %x\n", message_control);
+        } else if (cap_id == PCIE_CAPABILITY_ID_MSIX) {
+            uint16_t message_control = capability[2] | ((uint16_t)capability[3] << 8);
+            sddf_dprintf("\tmessage control(msix): %x\n", message_control);
+        } else if (cap_id == PCIE_CAPABILITY_ID_PM) {
+            // https://lekensteyn.nl/files/docs/PCI_Power_Management_12.pdf¸ §3.2
+            // not this...
+            uint16_t control = capability[4] | ((uint16_t)capability[5] << 8);
+            sddf_dprintf("\tpower management ctrl: %x\n", control);
+        } else if (cap_id == 0x10 /* PCI Express */) {
+            sddf_dprintf("\troot control(pcie): %x\n", *(uint16_t *)&(capability[0x1C]));
+            sddf_dprintf("\troot status(pcie): %x\n", *(uint32_t *)&(capability[0x20]));
+        } else {
+            sddf_dprintf("\tunknown\n");
+        }
+
+        current_ptr = (capability[1] & ~0b11);
+    }
+}
+
 void init()
 {
     sddf_dprintf("pcie driver starting!\n");
 
+#if 0
     for (uint8_t bus = 0; bus <= 255; bus++) {
         for (uint8_t device = 0; device < 32; device++) {
             for (uint8_t function = 0; function < 8; function++) {
@@ -166,12 +237,29 @@ void init()
 
 out:
     sddf_dprintf("\n\nPCIE_ENUM_COMPLETE\n");
+#endif
 
+    device_ack_irq(0, 0, 0, false);
+    device_ack_irq(1, 0, 0, false);
     nvme_init();
 }
 
 void notified(microkit_channel ch)
 {
+    static int i = 0;
     sddf_dprintf("notified on ch: %u\n", ch);
-    microkit_irq_ack(ch);
+
+    // if (i == 0) {
+    //     device_print(1, 0, 0);
+    //     i++;
+    // }
+
+    if (i < 10) {
+        microkit_irq_ack(ch);
+        i++;
+    } else {
+        device_ack_irq(0, 0, 0, true);
+        device_ack_irq(1, 0, 0, true);
+        sddf_dprintf("stopped ACKing seL4\n");
+    }
 }
