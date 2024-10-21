1df49d7d509196558b0e5a0740f0a76db121c85c
Cheng <lichengchaoreng@gmail.com>
2024-10-21 13:20:40 +1100

    sdmmc: some clean up before pull request
    Signed-off-by: Cheng <lichengchaoreng@gmail.com>

--- drivers/blk/sdmmc/src/sddf_blk/mod.rs
+++ drivers/blk/sdmmc/src/sddf_blk/mod.rs
@@ -4,11 +4,13 @@ extern "C" {
     pub fn blk_queue_empty_req_helper() -> u8;
     pub fn blk_queue_full_resp_helper() -> u8;
     pub fn blk_enqueue_resp_helper(status: BlkStatus, success: u32, id: u32) -> u8;
-    pub fn blk_dequeue_req_helper(code: *mut BlkOp,
-                                io_or_offset: *mut u64,
-                                block_number: *mut u32,
-                                count: *mut u16,
-                                id: *mut u32);
+    pub fn blk_dequeue_req_helper(
+        code: *mut BlkOp,
+        io_or_offset: *mut u64,
+        block_number: *mut u32,
+        count: *mut u16,
+        id: *mut u32,
+    );
 }
 
 #[repr(C)]
