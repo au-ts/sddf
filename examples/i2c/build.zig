ab88988178a6fb229abcc8ba12d68594543272dd
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2024-10-29 15:59:58 +1100

    build.zig: apply strict-align to all AArch64 targets
    Follows what we do in the Makefiles as well.
    
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- examples/i2c/build.zig
+++ examples/i2c/build.zig
@@ -17,6 +17,7 @@ const targets = [_]Target{
         .zig_target = std.Target.Query{
             .cpu_arch = .aarch64,
             .cpu_model = .{ .explicit = &std.Target.aarch64.cpu.cortex_a55 },
+            .cpu_features_add = std.Target.aarch64.featureSet(&[_]std.Target.aarch64.Feature{ .strict_align }),
             .os_tag = .freestanding,
             .abi = .none,
         },
