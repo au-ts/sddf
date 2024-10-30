7cb103bac8be42b7f2027cddbe75844842276148
James Archer <j.archer@unsw.edu.au>
2024-10-30 13:36:49 +1100

    convert serial qemu example
    Signed-off-by: James Archer <j.archer@unsw.edu.au>
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- drivers/serial/arm/uart.c
+++ drivers/serial/arm/uart.c
@@ -7,23 +7,29 @@
 #include <stdint.h>
 #include <microkit.h>
 #include <sddf/util/printf.h>
-#include <serial_config.h>
 #include <uart.h>
 
 #define IRQ_CH 0
 #define TX_CH  1
 #define RX_CH  2
 
-serial_queue_t *rx_queue;
-serial_queue_t *tx_queue;
+typedef struct config {
+    void *uart_regs;
+    void *rx_queue;
+    void *tx_queue;
+    void *rx_data;
+    void *tx_data;
+    uint64_t rx_capacity;
+    uint64_t tx_capacity;
+    uint64_t default_baud;
+    bool rx_enabled;
+} config_t;
 
-char *rx_data;
-char *tx_data;
+config_t config;
 
 serial_queue_handle_t rx_queue_handle;
 serial_queue_handle_t tx_queue_handle;
 
-uintptr_t uart_base;
 volatile pl011_uart_regs_t *uart_regs;
 
 /*
@@ -125,7 +131,7 @@ static void handle_irq(void)
 
 static void uart_setup(void)
 {
-    uart_regs = (pl011_uart_regs_t *) uart_base;
+    uart_regs = config.uart_regs;
 
     /* Wait for UART to finish transmitting. */
     while (uart_regs->fr & PL011_FR_UART_BUSY);
@@ -140,7 +146,7 @@ static void uart_setup(void)
     uart_regs->lcr_h |= (0b11 < PL011_LCR_WLEN_SHFT);
 
     /* Configure the reference clock and baud rate. Difficult to use automatic detection here as it requires the next incoming character to be 'a' or 'A'. */
-    set_baud(UART_DEFAULT_BAUD);
+    set_baud(config.default_baud);
 
     /* Enable FIFOs */
     uart_regs->lcr_h |= PL011_LCR_FIFO_EN;
@@ -149,10 +155,10 @@ static void uart_setup(void)
     uart_regs->lcr_h |= PL011_LCR_PARTY_EN;
 
     /* Enable receive interrupts when FIFO level exceeds 1/8 or after 32 ticks */
-#if !SERIAL_TX_ONLY
-    uart_regs->ifls &= ~(PL011_IFLS_RX_MASK << PL011_IFLS_RX_SHFT);
-    uart_regs->imsc |= (PL011_IMSC_RX_TIMEOUT | PL011_IMSC_RX_INT);
-#endif
+    if (config.rx_enabled) {
+        uart_regs->ifls &= ~(PL011_IFLS_RX_MASK << PL011_IFLS_RX_SHFT);
+        uart_regs->imsc |= (PL011_IMSC_RX_TIMEOUT | PL011_IMSC_RX_INT);
+    }
 
     /* Enable transmit interrupts if the FIFO drops below 1/8 - used when the write fifo becomes full */
     uart_regs->ifls &= ~(PL011_IFLS_TX_MASK << PL011_IFLS_TX_SHFT);
@@ -165,19 +171,31 @@ static void uart_setup(void)
     uart_regs->tcr |= PL011_CR_TX_EN;
 
     /* Enable receive */
-#if !SERIAL_TX_ONLY
-    uart_regs->tcr |= PL011_CR_RX_EN;
-#endif
+    if (config.rx_enabled) {
+        uart_regs->tcr |= PL011_CR_RX_EN;
+    }
 }
 
 void init(void)
 {
+    config = (config_t) {
+        .uart_regs = (void *)0x5000000,
+        .rx_queue = (void *)0x4000000,
+        .tx_queue = (void *)0x4001000,
+        .rx_data = (void *)0x4002000,
+        .tx_data = (void *)0x4004000,
+        .rx_capacity = 0x2000,
+        .tx_capacity = 0x2000,
+        .default_baud = 115200,
+        .rx_enabled = true,
+    };
+
     uart_setup();
 
-#if !SERIAL_TX_ONLY
-    serial_queue_init(&rx_queue_handle, rx_queue, SERIAL_RX_DATA_REGION_CAPACITY_DRIV, rx_data);
-#endif
-    serial_queue_init(&tx_queue_handle, tx_queue, SERIAL_TX_DATA_REGION_CAPACITY_DRIV, tx_data);
+    if (config.rx_enabled) {
+        serial_queue_init(&rx_queue_handle, config.rx_queue, config.rx_capacity, config.rx_data);
+    }
+    serial_queue_init(&tx_queue_handle, config.tx_queue, config.tx_capacity, config.tx_data);
 }
 
 void notified(microkit_channel ch)
