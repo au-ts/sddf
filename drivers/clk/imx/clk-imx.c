3bb3c77fdfa7dd5d819ac949eaf71fac8fbf1615
Terry Bai <terry.z.bai@gmail.com>
2024-10-31 16:19:36 +1100

    recalc_rate() of frac_pll

--- drivers/clk/imx/clk-imx.c
+++ drivers/clk/imx/clk-imx.c
@@ -2,6 +2,8 @@
 #include <clk-operations.h>
 #include <clk-imx.h>
 
+#define PLL_FRAC_DENOM 0x1000000
+
 static int clk_gate2_enable(struct clk *clk)
 {
     struct clk_gate_data *data = (struct clk_gate_data *)(clk->data);
@@ -33,14 +35,64 @@ const struct clk_ops clk_gate2_ops = {
     .is_enabled = clk_gate2_is_enabled,
 };
 
+static unsigned long clk_pll_recalc_rate(const struct clk *clk,
+                                unsigned long prate)
+{
+    /* TODO: This function is derived from Linux codebase, but seems wrong
+     * according to the datasheet as PLL_REFCLK_DIV_VAL[5:10] is never used. */
+    struct clk_frac_pll_data *data = (struct clk_frac_pll_data *)(clk->data);
+    uint64_t temp_rate = prate;
+    uint64_t rate;
 
+    /* Output Divider value is (n + 1) * 2 */
+    uint32_t output_div_val = regmap_read_bits(clk->base, data->offset, 0, 5);
+    output_div_val = (output_div_val + 1 ) * 2;
+
+    /* Valid Frac Divider value is 1 to 2^24 */
+    uint32_t frac_div_val = regmap_read_bits(clk->base, data->offset + 0x4, 7, 24);
+
+    /* Valid Int Divider value is 1 to 32 */
+    uint32_t int_div_val = regmap_read_bits(clk->base, data->offset + 0x4, 0, 7);
+
+    temp_rate *= prate;
+    temp_rate *= frac_div_val;
+    do_div(temp_rate, PLL_FRAC_DENOM);
+    do_div(temp_rate, output_div_val);
+
+    /* Frac Divider value is (n) */
+    rate = prate * 8 * (int_div_val + 1);
+    do_div(rate, output_div_val);
+    rate += temp_rate;
+
+    return rate;
+}
 
 const struct clk_ops clk_frac_pll_ops = {
-
+    /* .prepare     = clk_pll_prepare, */
+    /* .unprepare   = clk_pll_unprepare, */
+    /* .is_prepared = clk_pll_is_prepared, */
+    .recalc_rate = clk_pll_recalc_rate,
+    /* .round_rate  = clk_pll_round_rate, */
+    /* .set_rate    = clk_pll_set_rate, */
 };
 
-const struct clk_ops clk_sscg_pll_ops = {
+static unsigned long clk_pll_recalc_rate(const struct clk *clk,
+                                unsigned long prate)
+{
+    struct clk_sscg_pll_data *data = (struct clk_sscg_pll_data *)(clk->data);
 
+    return 0;
+}
+
+const struct clk_ops clk_sscg_pll_ops = {
+    /* .prepare = clk_sscg_pll_prepare, */
+    /* .unprepare = clk_sscg_pll_unprepare, */
+    /* .is_prepared = clk_sscg_pll_is_prepared, */
+    .recalc_rate = clk_sscg_pll_recalc_rate,
+    /* .set_rate = clk_sscg_pll_set_rate, */
+    .set_parent = clk_sscg_pll_set_parent,
+    .get_parent = clk_sscg_pll_get_parent,
+    /* .determine_rate = clk_sscg_pll_determine_rate, */
 };
 
 static unsigned long imx8m_clk_core_slice_recalc_rate(const struct clk *clk,
