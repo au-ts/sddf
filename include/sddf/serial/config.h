cc51976c6e9cb93e25d2a30cd27092be4653df41
James Archer <j.archer@unsw.edu.au>
2024-10-31 18:24:45 +1100

    amend
    Signed-off-by: James Archer <j.archer@unsw.edu.au>

---
+++ include/sddf/serial/config.h
@@ -0,0 +1,62 @@
+
+#include <microkit.h>
+
+#define SDDF_SERIAL_MAX_CLIENTS (MICROKIT_MAX_CHANNELS - 1)
+#define SDDF_SERIAL_BEGIN_STR_MAX_LEN 128
+
+typedef struct serial_driver_config {
+    void *uart_regs;
+    void *rx_queue;
+    void *tx_queue;
+    void *rx_data;
+    void *tx_data;
+    uint64_t rx_capacity;
+    uint64_t tx_capacity;
+    uint64_t default_baud;
+    bool rx_enabled;
+} serial_driver_config_t;
+
+typedef struct serial_virt_rx_client_config {
+    void *rx_queue;
+    void *rx_data;
+    uint64_t rx_capacity;
+} serial_virt_rx_client_config_t;
+
+typedef struct serial_virt_rx_config {
+    void *rx_queue_drv;
+    void *rx_data_drv;
+    uint64_t rx_capacity_drv;
+    char switch_char;
+    char terminate_num_char;
+    uint64_t num_clients;
+    serial_virt_rx_client_config_t clients[SDDF_SERIAL_MAX_CLIENTS];
+} serial_virt_rx_config_t;
+
+typedef struct serial_virt_tx_client_config {
+    char name[MICROKIT_PD_NAME_LENGTH];
+    void *tx_queue;
+    void *tx_data;
+    uint64_t tx_capacity;
+} serial_virt_tx_client_config_t;
+
+typedef struct serial_virt_tx_config {
+    void *tx_queue_drv;
+    void *tx_data_drv;
+    uint64_t tx_capacity_drv;
+    char begin_str[SDDF_SERIAL_BEGIN_STR_MAX_LEN];
+    uint64_t begin_str_len;
+    bool enable_colour;
+    bool enable_rx;
+    uint64_t num_clients;
+    serial_virt_tx_client_config_t clients[SDDF_SERIAL_MAX_CLIENTS];
+} serial_virt_tx_config_t;
+
+typedef struct serial_client_config {
+    void *rx_queue;
+    void *rx_data;
+    uint64_t rx_capacity;
+
+    void *tx_queue;
+    void *tx_data;
+    uint64_t tx_capacity;
+} serial_client_config_t;
