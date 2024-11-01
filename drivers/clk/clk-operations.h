0245c1bdac47d23f610835adeea501eb2b329a1c
Terry Bai <terry.z.bai@gmail.com>
2024-11-01 14:47:03 +1100

    Clock driver for odroidc4
    This implementation includes all clock definitions, basic operations
    of clock compoents, as well as interfaces to interact with clicnets
    via PPC. Client should be able to enable/disable a specific gate
    clock or query the current clock rate by passing the corresponding
    clock identifier, which is listed in g12a-bindings.h.
    
    set_rate() assumes that the rate requst is valid and compatible with
    the current configurations. Complete implementation that considers
    more incompatible requests will come in the future when dynamic
    clock configuration is necessary in lionsos
    
    Signed-off-by: Terry Bai <terry.z.bai@gmail.com>

---
+++ drivers/clk/clk-operations.h
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2024, UNSW
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <clk.h>
+
+#define CLK_INCORRECT_ARGS 1
+#define CLK_INVALID_OP 2
+
+int reg_write(uint64_t base, uint32_t offset, uint32_t val);
+int regmap_update_bits(uint64_t base, uint32_t offset, uint8_t shift, uint8_t width, uint32_t val);
+int regmap_read_bits(uint64_t base, uint32_t offset, uint8_t shift, uint8_t width);
+int regmap_mux_update_bits(uint64_t base, uint32_t offset, uint8_t shift, uint32_t mask, uint32_t val);
+int regmap_mux_read_bits(uint64_t base, uint32_t offset, uint8_t shift, uint32_t mask);
+
+extern const struct clk_ops clk_source_ops;
+extern const struct clk_ops clk_fixed_factor_ops;
+extern const struct clk_ops clk_divider_ops;
+extern const struct clk_ops clk_divider_ro_ops;
+extern const struct clk_ops clk_mux_ops;
+extern const struct clk_ops clk_mux_ro_ops;
+extern const struct clk_ops clk_gate_ops;
+extern const struct clk_ops clk_gate_ro_ops;
+
+#define CLK_FIXED_FACTOR(_name, _mult, _div, _data_flags, _parent_clks, _num_parents, _init_flags)                                 \
+struct clk _name = {                                                                \
+    .data = &(struct clk_fixed_factor_data) {                                       \
+        .mult = (_mult),                                                            \
+        .div = (_div),                                                              \
+        .flags = (_data_flags),                                                     \
+    },                                                                              \
+    .hw.init = &(struct clk_init_data) {                                            \
+        .name = #_name,                                                             \
+        .ops = &clk_fixed_factor_ops,                                               \
+        .parent_clks = (const struct clk *[]) _parent_clks,                         \
+        .num_parents = (_num_parents),                                              \
+        .flags = (_init_flags),                                                     \
+    },                                                                              \
+}
+
+#define CLK_GATE(_name, _offset, _bit, _data_flags, _parent_clks, _num_parents, _init_flags)                                  \
+struct clk _name = {                                                                \
+    .data = &(struct clk_gate_data) {                                               \
+        .offset = (_offset),                                                        \
+        .bit_idx = (_bit),                                                          \
+        .flags = (_data_flags),                                                     \
+    },                                                                              \
+    .hw.init = &(struct clk_init_data) {                                            \
+        .name = #_name,                                                             \
+        .ops = &clk_gate_ops,                                                       \
+        .parent_clks = (const struct clk *[]) _parent_clks,                         \
+        .num_parents = _num_parents,                                                \
+        .flags = (_init_flags),                                                     \
+    },                                                                              \
+}
+
+#define CLK_GATE_RO(_name, _offset, _bit, _data_flags, _parent_clks, _num_parents, _init_flags)                                  \
+struct clk _name = {                                                                \
+    .data = &(struct clk_gate_data) {                                               \
+        .offset = (_offset),                                                        \
+        .bit_idx = (_bit),                                                          \
+        .flags = (_data_flags),                                                     \
+    },                                                                              \
+    .hw.init = &(struct clk_init_data) {                                            \
+        .name = #_name,                                                             \
+        .ops = &clk_gate_ro_ops,                                                    \
+        .parent_clks = (const struct clk *[]) _parent_clks,                         \
+        .num_parents = _num_parents,                                                \
+        .flags = (_init_flags),                                                     \
+    },                                                                              \
+}
+
+#define CLK_MUX(_name, _offset, _mask, _shift, _table, _data_flags, _parent_data, _num_parents, _init_flags)             \
+struct clk _name = {                                                                \
+    .data = &(struct clk_mux_data) {                                                \
+        .offset = (_offset),                                                        \
+        .mask = (_mask),                                                            \
+        .shift = (_shift),                                                          \
+        .table = (_table),                                                          \
+        .flags = (_data_flags),                                                     \
+    },                                                                              \
+    .hw.init = &(struct clk_init_data) {                                            \
+        .name = #_name,                                                             \
+        .ops = &clk_mux_ops,                                                        \
+        .parent_data = (_parent_data),                                              \
+        .num_parents = (_num_parents),                                              \
+        .flags = (_init_flags),                                                     \
+    },                                                                              \
+}
+
+#define CLK_MUX_RO(_name, _offset, _mask, _shift, _table, _data_flags, _parent_data, _num_parents, _init_flags)             \
+struct clk _name = {                                                                \
+    .data = &(struct clk_mux_data) {                                                \
+        .offset = (_offset),                                                        \
+        .mask = (_mask),                                                            \
+        .shift = (_shift),                                                          \
+        .table = (_table),                                                          \
+        .flags = (_data_flags),                                                     \
+    },                                                                              \
+    .hw.init = &(struct clk_init_data) {                                            \
+        .name = #_name,                                                             \
+        .ops = &clk_mux_ro_ops,                                                     \
+        .parent_data = (_parent_data),                                              \
+        .num_parents = (_num_parents),                                              \
+        .flags = (_init_flags),                                                     \
+    },                                                                              \
+}
+
+#define CLK_DIV(_name, _offset, _shift, _width, _data_flags, _parent_clks, _num_parents, _init_flags)                          \
+struct clk _name = {                                                                \
+    .data = &(struct clk_div_data) {                                                \
+        .offset = (_offset),                                                        \
+        .shift = (_shift),                                                          \
+        .width = (_width),                                                          \
+        .flags = (_data_flags),                                                     \
+    },                                                                              \
+    .hw.init = &(struct clk_init_data) {                                            \
+        .name = #_name,                                                             \
+        .ops = &clk_divider_ops,                                                    \
+        .parent_clks = (const struct clk *[]) _parent_clks,                         \
+        .num_parents = (_num_parents),                                              \
+        .flags = (_init_flags),                                                     \
+    },                                                                              \
+}
+
+#define CLK_DIV_RO(_name, _offset, _shift, _width, _data_flags, _parent_clks, _num_parents, _init_flags)                          \
+struct clk _name = {                                                                \
+    .data = &(struct clk_div_data) {                                                \
+        .offset = (_offset),                                                        \
+        .shift = (_shift),                                                          \
+        .width = (_width),                                                          \
+        .flags = (_data_flags),                                                     \
+    },                                                                              \
+    .hw.init = &(struct clk_init_data) {                                            \
+        .name = #_name,                                                             \
+        .ops = &clk_divider_ro_ops,                                                 \
+        .parent_clks = (const struct clk *[]) _parent_clks,                         \
+        .num_parents = (_num_parents),                                              \
+        .flags = (_init_flags),                                                     \
+    },                                                                              \
+}
---
+++ drivers/clk/meson/clk.c
@@ -0,0 +1,257 @@
+/*
+ * Copyright 2024, UNSW
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Terry Bai: tianyi.bai@unsw.edu.au
+ */
+
+#include <microkit.h>
+#include <stdint.h>
+#include <sddf/util/string.h>
+#include <sddf/util/printf.h>
+#include <sddf/clk/protocol.h>
+#include <clk_config.h>
+
+#include <clk.h>            /* common definitions and interfaces */
+#include <clk-operations.h> /* ops of common clocks e.g., div, mux, fixed factor, and gate*/
+#include <clk-measure.h>    /* implementation of clock measurements */
+#include <clk-meson.h>      /* operations for meson-specific clocks */
+#include <g12a-regs.h>      /* offsets of control registers */
+#include <g12a-bindings.h>  /* clock id bindings*/
+
+// Logging
+#define DEBUG_DRIVER
+
+#ifdef DEBUG_DRIVER
+#define LOG_DRIVER(...) do{ sddf_dprintf("CLK DRIVER|INFO: "); sddf_dprintf(__VA_ARGS__); }while(0)
+#else
+#define LOG_DRIVER(...) do{}while(0)
+#endif
+
+#define LOG_DRIVER_ERR(...) do{ sddf_printf("CLK DRIVER|ERROR: "); sddf_printf(__VA_ARGS__); }while(0)
+
+#define I2C_CLK_OFFSET 320
+#define I2C_CLK_BIT (1 << 9) // bit 9
+
+uintptr_t clk_regs;
+uintptr_t msr_clk_base;
+
+struct clk **clk_list;
+
+/* TODO: Should be configured with init_regs */
+/* static struct clk_cfg fixed_clk_configs[] = { */
+/*     { .clk_id = CLKID_FCLK_DIV2_DIV, .frequency = 1000000000 }, */
+/*     { .clk_id = CLKID_FCLK_DIV3_DIV, .frequency = 666666667 }, */
+/*     { .clk_id = CLKID_FCLK_DIV4_DIV, .frequency = 500000000 }, */
+/*     { .clk_id = CLKID_FCLK_DIV5_DIV, .frequency = 400000000 }, */
+/*     { .clk_id = CLKID_FCLK_DIV7_DIV, .frequency = 285700000 }, */
+/* } */
+
+void clk_probe(struct clk *clk_list[])
+{
+    int i;
+    for (i = 0; i < CLKID_PCIE_PLL; i++) {
+        clk_list[i]->base = (uint64_t)clk_regs;
+        if (clk_list[i] && clk_list[i]->hw.init->ops->init) {
+            clk_list[i]->hw.init->ops->init(clk_list[i]);
+            LOG_DRIVER("Initialise %s\n", clk_list[i]->hw.init->name);
+        }
+    }
+}
+
+const struct clk *get_parent(const struct clk *clk)
+{
+    const struct clk_init_data *init = (struct clk_init_data *)clk->hw.init;
+    uint32_t num_parents = init->num_parents;
+
+    if (init->parent_data) {
+        uint8_t parent_idx = num_parents > 1 ? init->ops->get_parent(clk) : 0;
+        struct clk_parent_data parent_data = init->parent_data[parent_idx];
+
+        if (parent_data.clk) {
+            return parent_data.clk;
+        } else if (sddf_strcmp(parent_data.name, "xtal") == 0) {
+            return &g12a_xtal;
+        }
+    }
+
+    if (num_parents > 0) {
+        return init->parent_clks[0];
+    }
+
+    return NULL;
+}
+
+/* TODO: Should be just read from the structure, but need to update everytime when */
+/*     related clocks are modified */
+unsigned long clk_get_rate(const struct clk *clk)
+{
+    const struct clk_init_data *init = (struct clk_init_data *)clk->hw.init;
+    unsigned long parent_rate = 0;
+
+    const struct clk *parent_clk = get_parent(clk);
+    if (parent_clk) {
+        parent_rate = clk_get_rate(parent_clk);
+    }
+
+    unsigned long rate = parent_rate;
+    if (init->ops->recalc_rate) {
+        rate = init->ops->recalc_rate(clk, parent_rate);
+    }
+
+    return rate;
+}
+
+uint32_t clk_enable(struct clk *clk)
+{
+    if (clk->hw.init->ops->enable != NULL) {
+        return clk->hw.init->ops->enable(clk);
+    }
+
+    return CLK_INVALID_OP;
+}
+
+uint32_t clk_disable(struct clk *clk)
+{
+    if (clk->hw.init->ops->disable != NULL) {
+        return clk->hw.init->ops->disable(clk);
+    }
+
+    return CLK_INVALID_OP;
+}
+
+uint32_t clk_set_rate(struct clk *clk, uint32_t rate)
+{
+    if (clk->hw.init->ops->init) {
+        clk->hw.init->ops->init(clk);
+    }
+
+    const struct clk *pclk = get_parent(clk);
+    uint64_t prate = clk_get_rate(pclk);
+    if (clk->hw.init->ops->set_rate) {
+        /* TODO: determine_rate() needs to be implemented */
+        LOG_DRIVER("set %s to %dHz\n", clk->hw.init->name, rate);
+        clk->hw.init->ops->set_rate(clk, rate, prate);
+    } else {
+        /* TODO: We only propagate one level right now */
+        if (pclk->hw.init->ops->set_rate) {
+            const struct clk *ppclk = get_parent(pclk);
+            uint64_t pprate = clk_get_rate(ppclk);
+            /* TODO: determine_rate() needs to be implemented */
+            LOG_DRIVER("set %s to %dHz\n", pclk->hw.init->name, rate);
+            pclk->hw.init->ops->set_rate(pclk, prate, pprate);
+        }
+    }
+
+    return 0;
+}
+
+int clk_msr_stat()
+{
+    unsigned long clk_freq;
+    int i = 0;
+
+    const char *const *clk_msr_list = get_msr_clk_list();
+    for (i = 0; i < 128; i++) {
+        clk_freq = clk_msr(i);
+        LOG_DRIVER("[%4d][%4ldHz] %s\n", i, clk_freq, clk_msr_list[i]);
+    }
+
+    return 0;
+}
+
+void notified(microkit_channel ch)
+{
+}
+
+void init(void)
+{
+    LOG_DRIVER("Clock driver initialising...\n");
+
+    clk_list = get_clk_list();
+
+    clk_probe(clk_list);
+
+    volatile uint32_t *clk_i2c_ptr = ((void *)clk_regs + I2C_CLK_OFFSET);
+
+    for (int i = 0; i < NUM_DEVICE_CLKS; i++) {
+        struct clk *clk = clk_list[clk_configs[i].clk_id];
+
+        /* Enable the clock */
+        clk_enable(clk);
+
+        /* TODO: Set parent */
+
+        /* Set rate for the target clock */
+        if (clk_configs[i].frequency > 0) {
+            clk_set_rate(clk, clk_configs[i].frequency);
+        }
+    }
+
+    // Check that registers actually changed
+    if (!(*clk_i2c_ptr & I2C_CLK_BIT)) {
+        LOG_DRIVER_ERR("failed to toggle clock!\n");
+    }
+
+    /* clk_msr_stat(); */
+}
+
+microkit_msginfo protected(microkit_channel ch, microkit_msginfo msginfo)
+{
+    uint32_t ret = 0;
+    uint32_t argc = microkit_msginfo_get_count(msginfo);
+
+    /* TODO: Check if the channel is valid */
+    switch (microkit_msginfo_get_label(msginfo)) {
+
+    case SDDF_CLK_ENABLE: {
+        if (argc != 1) {
+            LOG_DRIVER_ERR("Incorrect number of arguments %u != 1\n", argc);
+            ret = CLK_INCORRECT_ARGS;
+            break;
+        }
+        uint32_t clk_id = (uint32_t)microkit_mr_get(SDDF_CLK_PARAM_ID);
+        LOG_DRIVER("get request clk_enable(%d)\n", clk_id);
+        ret = clk_enable(clk_list[clk_id]);
+        break;
+    }
+    case SDDF_CLK_DISABLE: {
+        if (argc != 1) {
+            LOG_DRIVER_ERR("Incorrect number of arguments %u != 1\n", argc);
+            ret = CLK_INCORRECT_ARGS;
+            break;
+        }
+        uint32_t clk_id = (uint32_t)microkit_mr_get(SDDF_CLK_PARAM_ID);
+        LOG_DRIVER("get request clk_disable(%d)\n", clk_id);
+        ret = clk_disable(clk_list[clk_id]);
+        break;
+    }
+    case SDDF_CLK_GET_RATE: {
+        if (argc != 1) {
+            LOG_DRIVER_ERR("Incorrect number of arguments %u != 1\n", argc);
+            ret = CLK_INCORRECT_ARGS;
+            break;
+        }
+        uint32_t clk_id = (uint32_t)microkit_mr_get(SDDF_CLK_PARAM_ID);
+        ret = clk_get_rate(clk_list[clk_id]);
+        break;
+    }
+    case SDDF_CLK_SET_RATE: {
+        if (argc != 2) {
+            LOG_DRIVER_ERR("Incorrect number of arguments %u != 1\n", argc);
+            ret = CLK_INCORRECT_ARGS;
+            break;
+        }
+        uint32_t clk_id = (uint32_t)microkit_mr_get(SDDF_CLK_PARAM_ID);
+        uint32_t rate = (uint32_t)microkit_mr_get(SDDF_CLK_PARAM_RATE);
+        ret = clk_set_rate(clk_list[clk_id], rate);
+        break;
+    }
+    default:
+        LOG_DRIVER_ERR("Unknown request %lu to clockk driver from channel %u\n", microkit_msginfo_get_label(msginfo),
+                       ch);
+        ret = 5;
+    }
+    return microkit_msginfo_new(ret, 0);
+}
