9f9cfc7d90fb2f1d8cc3e26e69b2422f6e17e1f1
Terry Bai <terry.z.bai@gmail.com>
2024-11-05 16:24:26 +1100

    clk: modify interfaces and fix issues during initialisation
    This commit includes modifiation to the interfaces, primarily for set/get rate, which
    return error state as well as final rate.
    
    More tests are conducted to verify the basic operations of types of clock components.
    A few bugs are fixed as well.
    
    Signed-off-by: Terry Bai <terry.z.bai@gmail.com>

--- include/sddf/clk/client.h
+++ include/sddf/clk/client.h
@@ -47,14 +47,16 @@ static inline uint32_t sddf_clk_disable(microkit_channel channel, uint32_t clk_i
  * Use the label to indicate this request.
  * @param channel of clock driver.
  * @param identifier of target clock.
+ * @param pointer to result variable.
  */
-static inline uint32_t sddf_clk_get_rate(microkit_channel channel, uint32_t clk_id)
+static inline uint32_t sddf_clk_get_rate(microkit_channel channel, uint32_t clk_id, uint64_t *rate)
 {
     microkit_msginfo msginfo = microkit_msginfo_new(SDDF_CLK_GET_RATE, 1);
     microkit_mr_set(SDDF_CLK_PARAM_ID, clk_id);
 
     msginfo = microkit_ppcall(channel, msginfo);
 
+    *rate = microkit_mr_get(0);
     return (uint32_t)microkit_msginfo_get_label(msginfo);
 }
 
@@ -64,14 +66,16 @@ static inline uint32_t sddf_clk_get_rate(microkit_channel channel, uint32_t clk_
  * @param channel of clock driver.
  * @param identifier of target clock.
  * @param target clock frequency.
+ * @param pointer to result variable.
  */
-static inline uint32_t sddf_clk_set_rate(microkit_channel channel, uint32_t clk_id, uint32_t rate)
+static inline uint32_t sddf_clk_set_rate(microkit_channel channel, uint32_t clk_id, uint64_t req_rate, uint64_t *rate)
 {
     microkit_msginfo msginfo = microkit_msginfo_new(SDDF_CLK_SET_RATE, 2);
     microkit_mr_set(SDDF_CLK_PARAM_ID, clk_id);
-    microkit_mr_set(SDDF_CLK_PARAM_RATE, rate);
+    microkit_mr_set(SDDF_CLK_PARAM_RATE, req_rate);
 
     msginfo = microkit_ppcall(channel, msginfo);
 
+    *rate = microkit_mr_get(0);
     return (uint32_t)microkit_msginfo_get_label(msginfo);
 }
