e462802a7f9d6fe46faab6e971ad9e664c845a65
James Archer <j.archer@unsw.edu.au>
2024-11-06 16:30:42 +1100

    i2c: add missing config resources header
    Signed-off-by: James Archer <j.archer@unsw.edu.au>

---
+++ include/sddf/i2c/config.h
@@ -0,0 +1,30 @@
+#include <microkit.h>
+
+#define SDDF_I2C_MAX_CLIENTS (MICROKIT_MAX_CHANNELS - 1)
+
+typedef struct i2c_driver_config {
+    uint64_t bus_num;
+    void *request_region;
+    void *response_region;
+    void *data_region;
+} i2c_driver_config_t;
+
+typedef struct i2c_virt_client_config {
+    void *request_queue;
+    void *response_queue;
+    uint64_t driver_data_offset;
+    uint64_t data_size;
+} i2c_virt_client_config_t;
+
+typedef struct i2c_virt_config {
+    void *driver_request_queue;
+    void *driver_response_queue;
+    uint64_t num_clients;
+    i2c_virt_client_config_t clients[SDDF_I2C_MAX_CLIENTS];
+} i2c_virt_config_t;
+
+typedef struct i2c_client_config {
+    void *request_region;
+    void *response_region;
+    void *data_region;
+} i2c_client_config_t;
