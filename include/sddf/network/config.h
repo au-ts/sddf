0c53c28115e7a88510c957d4c42fbf52056f707e
James Archer <j.archer@unsw.edu.au>
2024-11-10 15:48:53 +1100

    net: use config structs in tx virtualiser
    Signed-off-by: James Archer <j.archer@unsw.edu.au>

--- include/sddf/network/config.h
+++ include/sddf/network/config.h
@@ -17,37 +17,37 @@ typedef struct net_driver_config {
 } net_driver_config_t;
 
 typedef struct net_virt_tx_client_config {
-    uint64_t tx_free;
-    uint64_t tx_active;
-    uint64_t queue_capacity;
-    uint8_t client_id;
-    uint64_t buffer_data_region_vaddr;
-    uint64_t buffer_data_region_paddr;
+    void *free;
+    void *active;
+    size_t capacity;
+    uint8_t id;
+    uintptr_t buffer_data_region_vaddr;
+    uintptr_t buffer_data_region_paddr;
 } net_virt_tx_client_config_t;
 
 typedef struct net_virt_tx_config {
-    uint64_t tx_free_drv;
-    uint64_t tx_active_drv;
-    uint64_t drv_queue_capacity;
+    void *free_drv;
+    void *active_drv;
+    size_t capacity_drv;
     uint8_t drv_id;
     uint8_t num_clients;
     net_virt_tx_client_config_t clients[SDDF_NET_MAX_CLIENTS];
 } net_virt_tx_config_t;
 
 typedef struct net_virt_rx_config_client {
-    uint64_t free;
-    uint64_t active;
+    void *free;
+    void *active;
     uint64_t capacity;
     uint8_t mac_addr[6];
     uint8_t client_id;
 } net_virt_rx_config_client_t;
 
 typedef struct net_virt_rx_config {
-    uint64_t free_drv;
-    uint64_t active_drv;
+    void *free_drv;
+    void *active_drv;
     uint64_t capacity_drv;
-    uint64_t buffer_data_vaddr;
-    uint64_t buffer_data_paddr;
+    uintptr_t buffer_data_vaddr;
+    uintptr_t buffer_data_paddr;
     // The system designer must allocate a buffer metadata region for internal
     // use by the RX virtualiser. The size of this region must be at least
     // 4*drv_queue_capacity. It must be mapped R-W and zero-initialised.
