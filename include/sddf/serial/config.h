8c51ab87e931b7ccd4db7e0b717b3aa539f66eaf
James Archer <j.archer@unsw.edu.au>
2024-11-15 16:43:59 +1100

    serial: add component IDs to config resources
    Signed-off-by: James Archer <j.archer@unsw.edu.au>

--- include/sddf/serial/config.h
+++ include/sddf/serial/config.h
@@ -13,12 +13,15 @@ typedef struct serial_driver_config {
     uint64_t tx_capacity;
     uint64_t default_baud;
     bool rx_enabled;
+    uint8_t rx_id;
+    uint8_t tx_id;
 } serial_driver_config_t;
 
 typedef struct serial_virt_rx_client_config {
     void *rx_queue;
     void *rx_data;
     uint64_t rx_capacity;
+    uint8_t id;
 } serial_virt_rx_client_config_t;
 
 typedef struct serial_virt_rx_config {
@@ -28,6 +31,7 @@ typedef struct serial_virt_rx_config {
     char switch_char;
     char terminate_num_char;
     uint64_t num_clients;
+    uint8_t driver_id;
     serial_virt_rx_client_config_t clients[SDDF_SERIAL_MAX_CLIENTS];
 } serial_virt_rx_config_t;
 
@@ -36,6 +40,7 @@ typedef struct serial_virt_tx_client_config {
     void *tx_queue;
     void *tx_data;
     uint64_t tx_capacity;
+    uint8_t id;
 } serial_virt_tx_client_config_t;
 
 typedef struct serial_virt_tx_config {
@@ -47,6 +52,7 @@ typedef struct serial_virt_tx_config {
     bool enable_colour;
     bool enable_rx;
     uint64_t num_clients;
+    uint8_t driver_id;
     serial_virt_tx_client_config_t clients[SDDF_SERIAL_MAX_CLIENTS];
 } serial_virt_tx_config_t;
 
@@ -58,4 +64,7 @@ typedef struct serial_client_config {
     void *tx_queue;
     void *tx_data;
     uint64_t tx_capacity;
+
+    uint8_t rx_id;
+    uint8_t tx_id;
 } serial_client_config_t;
