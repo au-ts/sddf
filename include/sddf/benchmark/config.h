0e52abb0f7ea49488d0d2ecc894e15ff1141092b
James Archer <j.archer@unsw.edu.au>
2024-11-15 16:43:59 +1100

    benchmark: define config resources
    Signed-off-by: James Archer <j.archer@unsw.edu.au>
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

---
+++ include/sddf/benchmark/config.h
@@ -0,0 +1,27 @@
+#include <microkit.h>
+
+#define BENCHMARK_MAX_CHILDREN 64 // TODO: is there an upper bound?
+
+typedef struct benchmark_child_config {
+    char name[MICROKIT_PD_NAME_LENGTH];
+    uint8_t child_id;
+} benchmark_child_config_t;
+
+typedef struct benchmark_config {
+    uint8_t start_channel;
+    uint8_t stop_channel;
+    uint8_t init_channel;
+    uint64_t num_children;
+    benchmark_child_config_t children[BENCHMARK_MAX_CHILDREN];
+} benchmark_config_t;
+
+typedef struct benchmark_idle_config {
+    void *cycle_counters;
+    uint8_t init_channel;
+} benchmark_idle_config_t;
+
+typedef struct benchmark_client_config {
+    void *cycle_counters;
+    uint8_t start_channel;
+    uint8_t stop_channel;
+} benchmark_client_config_t;
