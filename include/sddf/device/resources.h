263361918a2fe9d25c0bc36bfd56b85d0a470cd0
James Archer <j.archer@unsw.edu.au>
2024-11-21 13:02:28 +1100

    include/device/resources.h: re-order fields to match sdfgen
    Signed-off-by: James Archer <j.archer@unsw.edu.au>

--- include/sddf/device/resources.h
+++ include/sddf/device/resources.h
@@ -2,19 +2,19 @@
 #define DEVICE_MAX_IRQS 64
 
 typedef struct device_region_resource {
-    uint64_t dt_index;
     void *vaddr;
     uint64_t size;
+    uint8_t dt_index;
 } device_region_resource_t;
 
 typedef struct device_irq_resource {
-    uint64_t dt_index;
+    uint8_t dt_index;
     uint8_t id;
 } device_irq_resource_t;
 
 typedef struct device_resources {
-    uint64_t num_regions;
-    uint64_t num_irqs;
+    uint8_t num_regions;
+    uint8_t num_irqs;
     device_region_resource_t regions[DEVICE_MAX_REGIONS];
     device_irq_resource_t irqs[DEVICE_MAX_IRQS];
 } device_resources_t;
