3e69baf80a9efafaed6ae57632a55f6c24a6332b
James Archer <j.archer@unsw.edu.au>
2024-11-21 13:02:28 +1100

    examples/timer: update client to use config resources
    Signed-off-by: James Archer <j.archer@unsw.edu.au>
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- examples/timer/client.c
+++ examples/timer/client.c
@@ -6,9 +6,13 @@
 #include <stdint.h>
 #include <microkit.h>
 #include <sddf/timer/client.h>
+#include <sddf/timer/config.h>
 #include <sddf/util/printf.h>
 
-#define TIMER_CHANNEL 1
+#include "timer_client_config.h"
+
+timer_client_config_t timer_config;
+microkit_channel timer_channel;
 
 void notified(microkit_channel ch)
 {
@@ -19,19 +23,22 @@ void notified(microkit_channel ch)
      */
     sddf_printf("CLIENT|INFO: Got a timeout!\n");
     /* Get the current time */
-    uint64_t time = sddf_timer_time_now(TIMER_CHANNEL);
+    uint64_t time = sddf_timer_time_now(timer_channel);
     sddf_printf("CLIENT|INFO: Now the time (in nanoseconds) is: %lu\n", time);
     /* Set another timeout */
-    sddf_timer_set_timeout(TIMER_CHANNEL, NS_IN_S);
+    sddf_timer_set_timeout(timer_channel, NS_IN_S);
 }
 
 void init(void)
 {
+    sddf_memcpy(&timer_config, timer_client_client_data, timer_client_client_data_len);
+    timer_channel = timer_config.driver_id;
+
     // lets get the time!
-    uint64_t time = sddf_timer_time_now(TIMER_CHANNEL);
+    uint64_t time = sddf_timer_time_now(timer_channel);
     sddf_printf("CLIENT|INFO: The time now is: %lu\n", time);
 
     // lets set a timeout
     sddf_printf("CLIENT|INFO: Setting a time out for 1 second\n");
-    sddf_timer_set_timeout(TIMER_CHANNEL, NS_IN_S);
+    sddf_timer_set_timeout(timer_channel, NS_IN_S);
 }
