5f8e275c68983f28c36f196654fe4c987885812e
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2024-12-04 13:07:04 +1100

    add more initialisation code
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- drivers/blk/ram/ram.c
+++ drivers/blk/ram/ram.c
@@ -8,18 +8,45 @@
 #include <sddf/blk/queue.h>
 #include <sddf/blk/storage_info.h>
 
+#include <blk_config.h>
+
+#ifdef DEBUG_DRIVER
+#define LOG_DRIVER(...) do{ sddf_dprintf("BLK DRIVER|INFO: "); sddf_dprintf(__VA_ARGS__); }while(0)
+#else
+#define LOG_DRIVER(...) do{}while(0)
+#endif
+
+#define LOG_DRIVER_ERR(...) do{ sddf_printf("BLK DRIVER|ERROR: "); sddf_printf(__VA_ARGS__); }while(0)
+
+#define PAGE_SIZE_4K 0x1000
+#define SECTOR_SIZE PAGE_SIZE_4K
+
+#ifndef SERIAL_NUMBER
+#define SERIAL_NUMBER "sddf_ramdisk0"
+#endif
+
 #define VIRT_CH 0
 
 blk_storage_info_t *blk_storage_info;
 blk_req_queue_t *blk_req_queue;
 blk_resp_queue_t *blk_resp_queue;
-
-uintptr_t requests_paddr;
-uintptr_t requests_vaddr;
-
 static blk_queue_handle_t blk_queue;
 
-char *ram_disk;
+/* Need this as our ramdisk isn't a DMA device so we need to convert DMA addresses to offsets in ramdisk. */
+/* NOTE: This ramdisk implementation current assume only 1 client! */
+uintptr_t blk_client0_data_paddr;
+uintptr_t blk_client0_data_vaddr;
+
+extern uintptr_t _ramdisk_begin;
+extern uintptr_t _ramdisk_end;
+
+bool check_cli_data_paddr_bound(uintptr_t cli_paddr) {
+    if (cli_paddr >= blk_client0_data_paddr && cli_paddr < (blk_client0_data_paddr + BLK_REGION_SIZE)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 void handle_request()
 {
@@ -52,7 +79,7 @@ void handle_request()
         }
         case BLK_REQ_FLUSH:
         case BLK_REQ_BARRIER: {
-            /* Nothing to do. */
+            /* Nothing to do. Requests are completed as they come in. */
             int err = blk_enqueue_resp(&blk_queue, BLK_RESP_OK, 0, id);
             assert(!err);
             microkit_notify(VIRT_CH);
@@ -68,7 +95,26 @@ void handle_request()
 
 void init(void)
 {
-    blk_queue_init(&blk_queue, blk_req_queue, blk_resp_queue, QUEUE_SIZE);
+    assert(!blk_storage_info->ready);
+
+    /* Initialise device information. */
+    sddf_strncpy(blk_storage_info->serial_number, SERIAL_NUMBER, BLK_MAX_SERIAL_NUMBER);
+    blk_storage_info->read_only = false;
+
+    /* Optimal block size same as sector size. */
+    blk_storage_info->sector_size = SECTOR_SIZE;
+    blk_storage_info->block_size = 1;
+
+    /* NOTE: Last block unusable if the total bytes size is not divisible by the sector size. */
+    blk_storage_info->capacity = (uint64_t) ((_ramdisk_begin - _ramdisk_end) / SECTOR_SIZE);
+
+    /* Our ramdisk has one platter and one reading head. */
+    blk_storage_info->cylinders = 1;
+    blk_storage_info->heads = 1;
+    blk_storage_info->blocks = blk_storage_info->capacity;
+
+    blk_queue_init(&blk_queue, blk_req_queue, blk_resp_queue, BLK_QUEUE_CAPACITY_DRIV);
+    __atomic_store_n(&blk_storage_info->ready, true, __ATOMIC_RELEASE);
 }
 
 void notified(microkit_channel ch)
