d7ccd4ce242abe8ba6f78e72784c022bfb2704ad
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2024-12-04 16:12:23 +1100

    More work on meta.py scripts
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- examples/i2c/meta.py
+++ examples/i2c/meta.py
@@ -1,13 +1,18 @@
 import argparse
+from typing import Dict, List, Any
 from sdfgen import SystemDescription, ProtectionDomain, Sddf, DeviceTree
 
-PLATFORMS = [
-    {
-        "name": "odroidc4",
-        "arch": 1,
-        "paddr_top": 0x80000000,
-        "i2c_device_node": "soc/bus@ffd00000/i2c@1d000",
-    },
+
+class Platform:
+    def __init__(self, name: str, arch: SystemDescription.Arch, paddr_top: int, i2c_device_node: str):
+        self.name = name
+        self.arch = arch
+        self.paddr_top = paddr_top
+        self.i2c_device_node = i2c_device_node
+
+
+PLATFORMS: List[Platform] = [
+    Platform("odroidc4", SystemDescription.Arch.AARCH64, 0x80000000, "soc/bus@ffd00000/i2c@1d000"),
 ]
 
 def generate_sdf():
@@ -15,7 +20,7 @@ def generate_sdf():
     i2c_virt = ProtectionDomain("i2c_virt", "i2c_virt.elf", priority=2)
     client = ProtectionDomain("client", "client.elf", priority=1)
 
-    i2c_node = dtb.node(platform["i2c_device_node"])
+    i2c_node = dtb.node(platform.i2c_device_node)
     assert i2c_node is not None
 
     i2c_system = Sddf.I2c(sdf, i2c_node, i2c_driver, i2c_virt)
@@ -31,24 +36,25 @@ def generate_sdf():
 
     i2c_system.connect()
 
-    print(sdf.xml())
+    with open(output + "/i2c.system", "w+") as f:
+        f.write(sdf.xml())
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--dtbs", required=True)
     parser.add_argument("--sddf", required=True)
-    parser.add_argument("--platform", required=True, choices=[p["name"] for p in PLATFORMS])
+    parser.add_argument("--platform", required=True, choices=[p.name for p in PLATFORMS])
+    parser.add_argument("--output", required=True)
 
     args = parser.parse_args()
 
-    platform = next(filter(lambda p: p["name"] == args.platform, PLATFORMS))
-    platform_name = platform["name"]
+    platform = next(filter(lambda p: p.name == args.platform, PLATFORMS))
 
-    sdf = SystemDescription(platform["arch"], platform["paddr_top"])
+    sdf = SystemDescription(platform.arch, platform.paddr_top)
     sddf = Sddf(args.sddf)
 
-    with open(args.dtbs + f"/{platform_name}.dtb", "rb") as f:
+    with open(args.dtbs + f"/{platform.name}.dtb", "rb") as f:
         dtb = DeviceTree(f.read())
 
     generate_sdf()
