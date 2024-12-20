c3a7bfe3cb490456d22e8c6e524c5268d335b4a3
Szymon Duchniewicz <s.duchniewicz@unsw.edu.au>
2024-12-20 17:06:47 +1100

    Use libmicrokit ARM caching operations in blk virt
    * Works towards: #195
    * virt.c patch from @Ivan-Velickovic .
    * Needed for blk_benchmark to achieve acceptable CPU utilisation (See:
      #348).
    
    Signed-off-by: Szymon Duchniewicz <s.duchniewicz@unsw.edu.au>

--- blk/components/virt.c
+++ blk/components/virt.c
@@ -159,10 +159,8 @@ static bool handle_mbr_reply()
         return false;
     }
 
-    /* TODO: This is a raw seL4 system call because Microkit does not (currently)
-     * include a corresponding libmicrokit API. */
 #ifdef CONFIG_ARCH_ARM
-    seL4_ARM_VSpace_Invalidate_Data(3, mbr_bk.vaddr, mbr_bk.vaddr + (BLK_TRANSFER_SIZE * mbr_bk.count));
+    cache_clean_and_invalidate(mbr_bk.vaddr, mbr_bk.vaddr + (BLK_TRANSFER_SIZE * mbr_bk.count));
 #endif
     sddf_memcpy(&msdos_mbr, (void *)mbr_bk.vaddr, sizeof(struct msdos_mbr));
 
@@ -218,10 +216,8 @@ static void handle_driver()
         case BLK_REQ_READ:
             if (drv_status == BLK_RESP_OK) {
                 /* Invalidate cache */
-                /* TODO: This is a raw seL4 system call because Microkit does not (currently)
-                    * include a corresponding libmicrokit API. */
 #ifdef CONFIG_ARCH_ARM
-                seL4_ARM_VSpace_Invalidate_Data(3, reqbk.vaddr, reqbk.vaddr + (BLK_TRANSFER_SIZE * reqbk.count));
+                cache_clean_and_invalidate(reqbk.vaddr, reqbk.vaddr + (BLK_TRANSFER_SIZE * reqbk.count));
 #endif
             }
             break;
