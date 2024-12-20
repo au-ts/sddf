e15267bed35e0e93e5835c0c1d7a7654a85a1b23
Szymon Duchniewicz <s.duchniewicz@unsw.edu.au>
2024-12-20 16:59:53 +1100

    WIP: add cycle count tracking in final report. Fix cpu util computation to include kernel cycles.
    Signed-off-by: Szymon Duchniewicz <s.duchniewicz@unsw.edu.au>

--- examples/blk_bench/include/benchmark_config/benchmark_config.h
+++ examples/blk_bench/include/benchmark_config/benchmark_config.h
@@ -5,10 +5,11 @@
 
 // Values in terms of include/sddf//blk/queue.h: BLK_TRANSFER_SIZE
 // decided on runs: 4KiB, 32KiB, 256KiB, 2MiB, 8MiB
-#define BENCHMARK_BLOCKS_PER_REQUEST (uint32_t[]) {1, 8, 64, 512, 2048}
-//, 4096, 32768}
+//#define BENCHMARK_BLOCKS_PER_REQUEST (uint32_t[]) {1, 8, 64, 512, 2048}
+// sizes 4X at each step: 8KiB, 32KiB, 128KiB 512KiB 2MiB 8MiB
+#define BENCHMARK_BLOCKS_PER_REQUEST (uint32_t[]) {1, 2, 4, 8, 32, 128, 256, 512, 513, 640, 768, 896, 1024, 2048}
 // Need to be a max of 8 MiB (client data region in SDF)
-#define REQUEST_COUNT (uint32_t[]) {256, 256, 256, 256, 256}
+#define REQUEST_COUNT (uint32_t[]) {256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256}
 //#define REQUEST_COUNT (uint32_t[]) {10, 10, 10, 10, 10}
 #define BENCHMARK_RUN_COUNT ((int) sizeof(BENCHMARK_BLOCKS_PER_REQUEST)/sizeof(uint32_t))
 #define BENCHMARK_INDIVIDUAL_RUN_REPEATS 3
