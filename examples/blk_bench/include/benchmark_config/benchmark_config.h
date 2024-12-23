8795591b4f6bfe91693dd53d1fd7de43d33e61bd
Szymon Duchniewicz <s.duchniewicz@unsw.edu.au>
2024-12-23 16:06:33 +1100

    blk_bench: benchmark_config: update benchmark sizes and granularity.
    Signed-off-by: Szymon Duchniewicz <s.duchniewicz@unsw.edu.au>

--- examples/blk_bench/include/benchmark_config/benchmark_config.h
+++ examples/blk_bench/include/benchmark_config/benchmark_config.h
@@ -4,14 +4,14 @@
  */
 
 // Values in terms of include/sddf//blk/queue.h: BLK_TRANSFER_SIZE
-// decided on runs: 4KiB, 32KiB, 256KiB, 2MiB, 8MiB
-//#define BENCHMARK_BLOCKS_PER_REQUEST (uint32_t[]) {1, 8, 64, 512, 2048}
-// sizes 4X at each step: 8KiB, 32KiB, 128KiB 512KiB 2MiB 8MiB
-#define BENCHMARK_BLOCKS_PER_REQUEST (uint32_t[]) {1, 2, 4, 8, 32, 128, 256, 512, 513, 640, 768, 896, 1024, 2048}
+// sizes for debugging SD card block bundle boundries (odroidc4 for SDXC can pack up to 0x1ff of 512 byte blocks, so boundry every 255.5 KiB)
+//#define BENCHMARK_BLOCKS_PER_REQUEST (uint16_t[]) {1, 2, 4, 8, 32, 63, 64, 128, 256, 512, 640, 768, 896, 1024, 2048}
+// sizes 2X at each step: 4KiB, 8KiB 16KiB, 32KiB, 64KiB, 128KiB, 256KiB, 512KiB, 1MiB, 2MiB, 4MiB, 8MiB
+#define BENCHMARK_BLOCKS_PER_REQUEST (uint16_t[]) {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048}
 // Need to be a max of 8 MiB (client data region in SDF)
-#define REQUEST_COUNT (uint32_t[]) {256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256}
+#define REQUEST_COUNT (uint16_t[]) {256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256}
 //#define REQUEST_COUNT (uint32_t[]) {10, 10, 10, 10, 10}
-#define BENCHMARK_RUN_COUNT ((int) sizeof(BENCHMARK_BLOCKS_PER_REQUEST)/sizeof(uint32_t))
+#define BENCHMARK_RUN_COUNT ((int) sizeof(BENCHMARK_BLOCKS_PER_REQUEST)/sizeof(uint16_t))
 #define BENCHMARK_INDIVIDUAL_RUN_REPEATS 3
 // 16 MiB interval to counter the caching of block device's sequential READs
 // XXX and to counter the batching of WRITE commits
