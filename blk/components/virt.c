931ab14f31b2bad9bf59b0f421a6ded992771570
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2025-01-08 12:05:55 +1100

    blk/components/virt.c: hack for RISC-V platforms
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- blk/components/virt.c
+++ blk/components/virt.c
@@ -176,7 +176,9 @@ static bool handle_mbr_reply()
 
     /* TODO: This is a raw seL4 system call because Microkit does not (currently)
      * include a corresponding libmicrokit API. */
+#ifdef CONFIG_ARCH_ARM
     seL4_ARM_VSpace_Invalidate_Data(3, mbr_bk.vaddr, mbr_bk.vaddr + (BLK_TRANSFER_SIZE * mbr_bk.count));
+#endif
     sddf_memcpy(&msdos_mbr, (void *)mbr_bk.vaddr, sizeof(struct msdos_mbr));
 
     return true;
@@ -228,7 +230,9 @@ static void handle_driver()
                 /* Invalidate cache */
                 /* TODO: This is a raw seL4 system call because Microkit does not (currently)
                     * include a corresponding libmicrokit API. */
+#ifdef CONFIG_ARCH_ARM
                 seL4_ARM_VSpace_Invalidate_Data(3, reqbk.vaddr, reqbk.vaddr + (BLK_TRANSFER_SIZE * reqbk.count));
+#endif
             }
             break;
         case BLK_REQ_WRITE:
