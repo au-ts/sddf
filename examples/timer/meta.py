592746d235ab917473a01ad230a0824163763c5f
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2025-01-09 15:01:08 +1100

    timer: meta.py cleanup
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- examples/timer/meta.py
+++ examples/timer/meta.py
@@ -1,28 +1,64 @@
 # Copyright 2025, UNSW
 # SPDX-License-Identifier: BSD-2-Clause
 import argparse
-from typing import Dict, List, Any
+from typing import List
+from dataclasses import dataclass
 from sdfgen import SystemDescription, Sddf, DeviceTree
 
-Arch = SystemDescription.Arch
 ProtectionDomain = SystemDescription.ProtectionDomain
 
+
+@dataclass
 class Board:
-    def __init__(self, name: str, arch: SystemDescription.Arch, paddr_top: int, timer_device_node: str):
-        self.name = name
-        self.arch = arch
-        self.paddr_top = paddr_top
-        self.timer_device_node = timer_device_node
+    name: str
+    arch: SystemDescription.Arch
+    paddr_top: int
+    timer: str
 
 
 BOARDS: List[Board] = [
-    Board("qemu_virt_aarch64", Arch.AARCH64, 0xa_000_000, "timer"),
-    Board("odroidc4", Arch.AARCH64, 0x80000000, "soc/bus@ffd00000/watchdog@f0d0"),
-    Board("star64", Arch.RISCV64, 0x100000000, "soc/timer@13050000"),
-    Board("maaxboard", Arch.AARCH64, 0xa0000000, "soc@0/bus@30000000/timer@302d0000"),
-    Board("imx8mm_evk", Arch.AARCH64, 0xa0000000, "soc@0/bus@30000000/timer@302d0000"),
-    Board("imx8mp_evk", Arch.AARCH64, 0xa0000000, "soc@0/bus@30000000/timer@302d0000"),
-    Board("imx8mq_evk", Arch.AARCH64, 0xa0000000, "soc@0/bus@30000000/timer@302d0000"),
+    Board(
+        name="qemu_virt_aarch64",
+        arch=SystemDescription.Arch.AARCH64,
+        paddr_top=0xa_000_000,
+        timer="timer"
+    ),
+    Board(
+        name="odroidc4",
+        arch=SystemDescription.Arch.AARCH64,
+        paddr_top=0x80000000,
+        timer="soc/bus@ffd00000/watchdog@f0d0"
+    ),
+    Board(
+        name="star64",
+        arch=SystemDescription.Arch.RISCV64,
+        paddr_top=0x100000000,
+        timer="soc/timer@13050000"
+    ),
+    Board(
+        name="maaxboard",
+        arch=SystemDescription.Arch.AARCH64,
+        paddr_top=0xa0000000,
+        timer="soc@0/bus@30000000/timer@302d0000"
+    ),
+    Board(
+        name="imx8mm_evk",
+        arch=SystemDescription.Arch.AARCH64,
+        paddr_top=0xa0000000,
+        timer="soc@0/bus@30000000/timer@302d0000"
+    ),
+    Board(
+        name="imx8mp_evk",
+        arch=SystemDescription.Arch.AARCH64,
+        paddr_top=0xa0000000,
+        timer="soc@0/bus@30000000/timer@302d0000"
+    ),
+    Board(
+        name="imx8mq_evk",
+        arch=SystemDescription.Arch.AARCH64,
+        paddr_top=0xa0000000,
+        timer="soc@0/bus@30000000/timer@302d0000"
+    ),
 ]
 
 
@@ -30,7 +66,7 @@ def generate_sdf(sdf_file: str, output_dir: str, dtb: DeviceTree):
     timer_driver = ProtectionDomain("timer_driver", "timer_driver.elf", priority=254)
     client = ProtectionDomain("client", "client.elf", priority=1)
 
-    timer_node = dtb.node(board.timer_device_node)
+    timer_node = dtb.node(board.timer)
     assert timer_node is not None
 
     timer_system = Sddf.Timer(sdf, timer_node, timer_driver)
