0f893d28ba5b7e95b8fb756026a564a425689da0
James Archer <j.archer@unsw.edu.au>
2025-01-10 15:56:00 +1100

    lib_sddf_lwip: allow building multiple differently-configured libraries
    Signed-off-by: James Archer <j.archer@unsw.edu.au>

--- network/lib_sddf_lwip/lib_sddf_lwip.mk
+++ network/lib_sddf_lwip/lib_sddf_lwip.mk
@@ -4,29 +4,32 @@
 # SPDX-License-Identifier: BSD-2-Clause
 #
 
-ifeq ($(strip $(SDDF_LWIP_NUM_BUFS)),)
-$(error SDDF_LWIP_NUM_BUFS must be specified)
-endif
-
 LIB_SDDF_LWIP_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
-LIB_SDDF_LWIP_FILES := $(addprefix ${LIB_SDDF_LWIP_DIR}/, lib_sddf_lwip.c)
-LIB_SDDF_LWIP_OBJS := $(LIB_SDDF_LWIP_FILES:.c=.o)
 
-${LIB_SDDF_LWIP_OBJS}: CFLAGS += -DSDDF_LWIP_NUM_BUFS=$(SDDF_LWIP_NUM_BUFS)
+lib_sddf_lwip.o: CFLAGS += -DSDDF_LWIP_NUM_BUFS=$(SDDF_LWIP_NUM_BUFS)
+lib_sddf_lwip.o: ${CHECK_FLAGS_BOARD_MD5}
+lib_sddf_lwip.o: $(LIB_SDDF_LWIP_DIR)/lib_sddf_lwip.c
+	${if ${SDDF_LWIP_NUM_BUFS},,${error "SDDF_LWIP_NUM_BUFS not defined"}}
+	${CC} ${CFLAGS} -c -o $@ $<
 
-${LIB_SDDF_LWIP_OBJS}: ${CHECK_FLAGS_BOARD_MD5} | ${LIB_SDDF_LWIP_DIR}
-${LIB_SDDF_LWIP_DIR}:
-	mkdir -p $@
+lib_sddf_lwip%.o: CFLAGS += ${CFLAGS$*} -DSDDF_LWIP_NUM_BUFS=$(SDDF_LWIP_NUM_BUFS$*)
+lib_sddf_lwip%.o: ${CHECK_FLAGS_BOARD_MD5}
+lib_sddf_lwip%.o: $(LIB_SDDF_LWIP_DIR)/lib_sddf_lwip.c
+	${if ${SDDF_LWIP_NUM_BUFS$*},,${error "SDDF_LWIP_NUM_BUFS$* not defined for $@"}}
+	${CC} ${CFLAGS} -c -o $@ $<
 
-lib_sddf_lwip.a: ${LIB_SDDF_LWIP_OBJS}
+lib_sddf_lwip.a: lib_sddf_lwip.o
+	${AR} rv $@ $^
+	${RANLIB} $@
+
+lib_sddf_lwip%.a: lib_sddf_lwip%.o
 	${AR} rv $@ $^
 	${RANLIB} $@
 
 clean::
-	${RM} -f ${LIB_SDDF_LWIP_OBJS} ${LIB_SDDF_LWIP_OBJS:.o=.d}
+	${RM} -f lib_sddf_lwip*.o
 
 clobber:: clean
-	${RM} -f lib_sddf_lwip.a
-	rmdir ${LIB_SDDF_LWIP_DIR}
+	${RM} -f lib_sddf_lwip*.a
 
--include ${LIB_SDDF_LWIP_OBJS:.o=.d}
+-include ${wildcard lib_sddf_lwip*.d}
