3f3e321eb437b11a499001bb13b16430647ecba8
julia <git.ts@trainwit.ch>
2025-01-31 17:02:49 +1100

    nix: update to nixpkgs-unstable to stop building aarch64-none-elf-gcc
    Also make zig/sdfgen follow our nixpkgs.
    
    Signed-off-by: julia <git.ts@trainwit.ch>

--- flake.nix
+++ flake.nix
@@ -6,9 +6,11 @@
   description = "A flake for building sDDF";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     zig-overlay.url = "github:mitchellh/zig-overlay";
+    zig-overlay.inputs.nixpkgs.follows = "nixpkgs";
     sdfgen.url = "github:au-ts/microkit_sdf_gen/0.16.1";
+    sdfgen.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { nixpkgs, zig-overlay, sdfgen, ... }:
