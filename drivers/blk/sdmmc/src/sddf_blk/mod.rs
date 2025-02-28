5c4eba99983bb4e2e86bc4de8534b1df8c8e8a24
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2025-02-28 15:43:00 +1100

    Fixes for rebase onto tooling changes and metaprogram, blk example works
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- drivers/blk/sdmmc/src/sddf_blk/mod.rs
+++ drivers/blk/sdmmc/src/sddf_blk/mod.rs
@@ -1,13 +1,17 @@
 unsafe extern "C" {
     pub unsafe fn blk_queue_init_helper();
 
+    pub unsafe fn blk_device_regs_vaddr() -> u64;
+    pub unsafe fn blk_device_init_data_vaddr() -> u64;
+    pub unsafe fn blk_device_init_data_ioaddr() -> u64;
+
     pub unsafe fn blk_queue_empty_req_helper() -> u8;
     pub unsafe fn blk_queue_full_resp_helper() -> u8;
     pub unsafe fn blk_enqueue_resp_helper(status: BlkStatus, success: u16, id: u32) -> u8;
     pub unsafe fn blk_dequeue_req_helper(
         code: *mut BlkOp,
         io_or_offset: *mut u64,
-        block_number: *mut u32,
+        block_number: *mut u64,
         count: *mut u16,
         id: *mut u32,
     );
@@ -30,7 +34,7 @@ pub enum BlkStatus {
 pub struct BlkRequest {
     pub request_code: BlkOp,
     pub io_or_offset: u64,
-    pub block_number: u32,
+    pub block_number: u64,
     pub count: u16,
     // I suggest use u32 here and change the count to use u32 in sddf_blk
     pub success_count: u16,
