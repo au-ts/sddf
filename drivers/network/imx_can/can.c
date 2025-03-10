bb380af84d157981b442ef2b0f8ffef08f1d77c9
jamesrulz <jamesmilo42@gmail.com>
2025-03-10 15:49:51 +1100

    Added MCR register offsets, memory map + filled out config

--- drivers/network/imx_can/can.c
+++ drivers/network/imx_can/can.c
@@ -1,16 +1,70 @@
-// TODO fill this out
+#include <microkit.h>
 
-// What mode does it start in on startup???
+#include "can.h"
 
-// << Reset >>
-// Hard reset -- is chip level (assume power?)
-// Soft reset 
-// -- assert MCR[SOFTRST] -- this resets some of the memory mappe registers synchronously.
-// -- MCR[SOFTRST] will remain asserted while soft reset is pending (it needs to propogate as is synchronous). Can poll this until deasserted to indicate soft-reset is complete
+__attribute__((__section__(".device_resources"))) device_resources_t device_resources;
 
-// << Init >>
-// Required that FlexCAN be put into Freeze mode to initialize (must be initialized after every reset).
+volatile struct control_registers *can_regs;
+
+static void can_setup(void) {
+    can_regs = device_resources.regions[0].region.vaddr;
+}
 
 
-// Alternate way to start is to try and just read device registers to check connected to the board correctly
-// start by attempting to read, write and poll the MCR reset register to determine if we're actually able to connect to the board or not
+// flexcan_chip_start << this function seems to be the equivalent for the linux kernel for starting FlexCAN
+void init (void) {
+    // call all initialisation for the device / software interface here 
+
+    // first thing is basically going to be reading and writing a register
+}
+
+void notified(microkit_channel ch) {
+    ;
+}
+
+// Initially just boot the device and try to read and write one of the registers in the Module Configuration Register (MCR)
+
+// Implementation for initialisation
+// Reset --> I think it's safe to assume booting is chip-level reset so we don't need to reset again on startup
+// Freeze --> before any initialisation can begin, need to set freeze mode
+// Init process
+
+// << INIT PROCESS >> 
+/*
+    For any configuration change/initialization it is required that FlexCAN be put into Freeze
+    mode (see Freeze mode). Note that the module needs to be initialized after every reset.
+    The following is a generic initialization sequence applicable to the FlexCAN module:
+    1. Initialize the Module Configuration register (MCR).
+    a. Enable the individual filtering per MB and reception queue features by setting
+    IRMQ.
+    b. Enable the warning interrupts by setting WRNEN.
+    c. If required, disable frame self reception by setting SRXDIS.
+    d. Enable the Rx FIFO by setting RFEN.
+    e. If Rx FIFO is enabled and DMA is required, set DMA.
+    f. Enable the abort mechanism by setting AEN.
+    g. Enable the local priority feature by setting LPRIOEN.
+    2. Initialize the Control 1 register (CTRL1) and optionally the CAN Bit Timing register
+    (CBT). Initialize also the CAN FD CAN Bit Timing register (FDCBT).
+    a. Determine the bit timing parameters: PROPSEG, PSEG1, PSEG2, and RJW.
+    b. Optionally determine the bit timing parameters: EPROPSEG, EPSEG1,
+    EPSEG2, and ERJW.
+    c. Determine the CAN FD bit timing parameters: FPROPSEG, FPSEG1, FPSEG2,
+    and FRJW.
+    d. Determine the bit rate by programming the PRESDIV field and optionally the
+    EPRESDIV field.
+    e. Determine the CAN FD bit rate by programming the FPRESDIV field.
+    f. Determine the internal arbitration mode (LBUF).
+    3. All FlexCAN memory must be initialized if Error Code Correction (ECC) is enabled.
+    See Detection and correction of memory errors.
+    4. Initialize the message buffers.
+    a. The control and status word of all message buffers must be initialized.
+    b. If Rx FIFO was enabled, the ID filter table must be initialized.
+    c. Other entries in each message buffer should be initialized as required.
+    5. Initialize the Rx Individual Mask registers (RXIMRn).
+    6. Set required interrupt mask bits in
+    • IMASK registers (for all MB interrupts)
+    • MCR register (for Wake-Up interrupt)
+    • CTRL1 / CTRL2 registers (for Bus Off and Error interrupts)
+    7. Negate MCR[HALT].
+    After the last step listed above, FlexCAN attempts to synchronize to the CAN bus.
+*/
