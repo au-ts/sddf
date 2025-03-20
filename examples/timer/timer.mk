7463b5dcb8bedb6f59a95efba666976644a747da
Szymon Duchniewicz <s.duchniewicz@unsw.edu.au>
2025-03-20 17:01:09 +1100

    examples:timer: Refactor Makefiles
    * refactor makefiles to stick to the same format as serial example.
    
    Signed-off-by: Szymon Duchniewicz <s.duchniewicz@unsw.edu.au>

--- examples/timer/timer.mk
+++ examples/timer/timer.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2024, UNSW
+# Copyright 2025, UNSW
 #
 # SPDX-License-Identifier: BSD-2-Clause
 #
@@ -8,25 +8,71 @@ ifeq ($(strip $(MICROKIT_SDK)),)
 $(error MICROKIT_SDK must be specified)
 endif
 
-METAPROGRAM := $(TOP)/meta.py
+ifeq ($(strip $(SDDF)),)
+$(error SDDF must be specified)
+endif
 
 BUILD_DIR ?= build
 # By default we make a debug build so that the client debug prints can be seen.
 MICROKIT_CONFIG ?= debug
+IMAGE_FILE := loader.img
+REPORT_FILE := report.txt
 
+ifeq ($(strip $(TOOLCHAIN)),)
+	TOOLCHAIN := clang
+endif
+ifeq ($(strip $(TOOLCHAIN)), clang)
+	CC := clang
+	LD := ld.lld
+	AR := llvm-ar
+	RANLIB := llvm-ranlib
+	OBJCOPY := llvm-objcopy
+else
+	CC := $(TOOLCHAIN)-gcc
+	LD := $(TOOLCHAIN)-ld
+	AS := $(TOOLCHAIN)-as
+	AR := $(TOOLCHAIN)-ar
+	RANLIB := $(TOOLCHAIN)-ranlib
+	OBJCOPY := $(TOOLCHAIN)-objcopy
+endif
 DTC := dtc
 PYTHON ?= python3
 
-CC := clang
-LD := ld.lld
-AR := llvm-ar
-RANLIB := llvm-ranlib
-OBJCOPY := llvm-objcopy
-
 MICROKIT_TOOL ?= $(MICROKIT_SDK)/bin/microkit
 
-BOARD_DIR := $(MICROKIT_SDK)/board/$(MICROKIT_BOARD)/$(MICROKIT_CONFIG)
+ifeq ($(strip $(MICROKIT_BOARD)), odroidc4)
+	export TIMER_DRIVER_DIR := meson
+	export CPU := cortex-a55
+else ifeq ($(strip $(MICROKIT_BOARD)), odroidc2)
+	export TIMER_DRIVER_DIR := meson
+	export CPU := cortex-a53
+else ifeq ($(strip $(MICROKIT_BOARD)), qemu_virt_aarch64)
+	export TIMER_DRIVER_DIR := arm
+	export CPU := cortex-a53
+	export QEMU := qemu-system-aarch64
+	export QEMU_ARCH_ARGS := -machine virt,virtualization=on -cpu cortex-a53 -device loader,file=$(IMAGE_FILE),addr=0x70000000,cpu-num=0
+else ifeq ($(strip $(MICROKIT_BOARD)), qemu_virt_riscv64)
+	export TIMER_DRIVER_DIR := goldfish
+	export QEMU := qemu-system-riscv64
+	export QEMU_ARCH_ARGS := -machine virt -kernel $(IMAGE_FILE)
+else ifeq ($(strip $(MICROKIT_BOARD)), star64)
+	export TIMER_DRIVER_DIR := jh7110
+else ifneq ($(filter $(strip $(MICROKIT_BOARD)),imx8mm_evk imx8mp_evk imx8mq_evk maaxboard),)
+	export TIMER_DRIVER_DIR := imx
+	export CPU := cortex-a53
+else
+$(error Unsupported MICROKIT_BOARD given)
+endif
+
+TOP:= ${SDDF}/examples/timer
+METAPROGRAM := $(TOP)/meta.py
 UTIL := $(SDDF)/util
+TIMER_DRIVER := $(SDDF)/drivers/timer/$(TIMER_DRIVER_DIR)
+BOARD_DIR := $(MICROKIT_SDK)/board/$(MICROKIT_BOARD)/$(MICROKIT_CONFIG)
+SYSTEM_FILE := timer.system
+DTS := $(SDDF)/dts/$(MICROKIT_BOARD).dts
+DTB := $(MICROKIT_BOARD).dtb
+ARCH := ${shell grep 'CONFIG_SEL4_ARCH  ' $(BOARD_DIR)/include/kernel/gen_config.h | cut -d' ' -f4}
 
 IMAGES := timer_driver.elf client.elf
 
@@ -48,13 +94,6 @@ CFLAGS := -nostdlib \
 LDFLAGS := -L$(BOARD_DIR)/lib
 LIBS := --start-group -lmicrokit -Tmicrokit.ld libsddf_util_debug.a --end-group
 
-IMAGE_FILE := loader.img
-DTS := $(SDDF)/dts/$(MICROKIT_BOARD).dts
-DTB := $(MICROKIT_BOARD).dtb
-SYSTEM_FILE := timer.system
-REPORT_FILE := report.txt
-CLIENT_OBJS := client.o
-TIMER_DRIVER := $(SDDF)/drivers/timer/$(TIMER_DRIVER_DIR)
 
 all: $(IMAGE_FILE)
 CHECK_FLAGS_BOARD_MD5:=.board_cflags-$(shell echo -- ${CFLAGS} ${MICROKIT_BOARD} ${MICROKIT_CONFIG} | shasum | sed 's/ *-//')
