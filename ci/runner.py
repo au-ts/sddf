79ce9517defb2ef04c6134f6e7947ca323d4ac33
julia <git.ts@trainwit.ch>
2025-04-16 17:36:59 +1000

    ci: unittest running examples
    Signed-off-by: julia <git.ts@trainwit.ch>

--- ci/runner.py
+++ ci/runner.py
@@ -9,15 +9,9 @@ QEMU.
 """
 
 import asyncio
-from pathlib import Path
 import sys
 
-from .hardware_backend import (
-    HardwareBackend,
-    MachineQueueBackend,
-    QemuBackend,
-    TtyBackend,
-)
+from hardware_backend import HardwareBackend
 
 
 async def runner(test, backend: HardwareBackend):
@@ -34,49 +28,3 @@ async def runner(test, backend: HardwareBackend):
         quit(1)
     finally:
         await backend.stop()
-
-
-# TODO: Figure out how to get this to work with unittest so we have autodiscovery
-#       and listing.
-# TODO: Figure out how to structure this in a good way. cc @Ivan.
-def cli(test):
-    import argparse
-
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument(
-        "--file",
-        type=Path,
-        required=True,
-    )
-    parser.add_argument(
-        "--backend",
-        choices=["qemu", "machine_queue", "tty"],
-        required=True,
-    )
-    parser.add_argument(
-        "--board",
-        choices=["odroidc4_1", "qemu_virt_riscv64"],
-        required=True,
-    )
-    args = parser.parse_args()
-
-    if args.backend == "qemu":
-        backend = QemuBackend("make", "MICROKIT_BOARD=qemu_virt_riscv64", "qemu", "-C", "examples/serial")
-    elif args.backend == "machine_queue":
-        backend = MachineQueueBackend(args.file, "odroidc4_1")
-    elif args.backend == "tty":
-        backend = TtyBackend("/dev/ttyUSB0")
-    else:
-        raise Exception("Unknown backend %s" % (args.backend))
-
-    asyncio.run(runner(test, backend))
-
-
-if __name__ == "__main__":
-    # TODO:
-    ...
-
-
-# python -m ci.tests.examples.serial --backend machine_queue --file examples/serial/build/loader.img
-# python -m ci.tests.examples.serial --backend qemu --board qemu_virt_riscv64 --file examples/serial/build/loader.img
-# python -m ci.tests.examples.serial --backend machine_queue --board odroidc4_1 --file examples/serial/build/loader.img
