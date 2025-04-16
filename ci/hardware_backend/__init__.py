4dbb45449a197141ade71488d6bf5b560c16478f
julia <git.ts@trainwit.ch>
2025-04-16 12:56:33 +1000

    ci: add basic hardware backend tests
    Signed-off-by: julia <git.ts@trainwit.ch>

---
+++ ci/hardware_backend/__init__.py
@@ -0,0 +1,16 @@
+"""
+Set of "classes" (in the duck-typing way) that describe how we interact with the
+actual system that is running the test.
+
+In broad strokes, requirements of these backends are:
+
+- Can collect log outputs (at a line granularity, i.e. streaming)
+- Can enter test inputs
+- Can kill tests.
+"""
+
+# Re-Exports
+from .base import HardwareBackend, LockedBoardException
+from .streams import send_input, wait_for_output
+from .machine_queue import MachineQueueBackend
+from .qemu import QemuBackend
