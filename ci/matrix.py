a4f174d0e3db27dd710499facc5dc0fc32a6c7d2
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2025-05-05 13:27:15 +1000

    ci: add zcu102 serial driver
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- ci/matrix.py
+++ ci/matrix.py
@@ -15,6 +15,7 @@ MACHINE_QUEUE_BOARDS: dict[str, list[str]] = {
     "odroidc2": ["odroidc2"],
     "odroidc4": ["odroidc4_1", "odroidc4_2"],
     "star64": ["star64"],
+    "zcu102": ["zcu102"],
 }
 
 EXAMPLES: dict[str, _ExampleMatrixType] = {
@@ -70,6 +71,7 @@ EXAMPLES: dict[str, _ExampleMatrixType] = {
             "qemu_virt_aarch64",
             "qemu_virt_riscv64",
             "star64",
+            "zcu102",
         ],
         "boards_test": [
             "imx8mm_evk",
@@ -81,6 +83,7 @@ EXAMPLES: dict[str, _ExampleMatrixType] = {
             "qemu_virt_aarch64",
             "qemu_virt_riscv64",
             "star64",
+            "zcu102",
         ],
     },
     "timer": {
