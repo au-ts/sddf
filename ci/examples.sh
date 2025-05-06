9a8f0771c5730bef28268353ed1751973fd00885
Szymon Duchniewicz <s.duchniewicz@unsw.edu.au>
2025-05-06 11:21:53 +1000

    ci:add zcu102 timer driver
    Signed-off-by: Szymon Duchniewicz <s.duchniewicz@unsw.edu.au>

--- ci/examples.sh
+++ ci/examples.sh
@@ -196,7 +196,7 @@ i2c() {
 }
 
 timer() {
-    BOARDS=("imx8mm_evk" "imx8mp_evk" "imx8mq_evk" "maaxboard" "odroidc2" "odroidc4" "qemu_virt_aarch64" "qemu_virt_riscv64" "star64")
+    BOARDS=("imx8mm_evk" "imx8mp_evk" "imx8mq_evk" "maaxboard" "odroidc2" "odroidc4" "qemu_virt_aarch64" "qemu_virt_riscv64" "star64" "zcu102")
     CONFIGS=("debug" "release")
     for BOARD in "${BOARDS[@]}"
     do
