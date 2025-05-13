0899639ff1916917cc0cc1468861fa42c98dbe89
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2025-05-13 13:58:19 +1000

    drivers/pinctrl/imx: overhaul how pinctrl config data is encoded
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- drivers/pinctrl/imx/pinctrl.c
+++ drivers/pinctrl/imx/pinctrl.c
@@ -5,7 +5,6 @@
 
 /*
 Pinctrl driver for the iMX8 SoC based platforms.
-Tested on MaaXBoard (imx8mq), imx8mq-evk, imx8mm-evk and imx8mp-evk.
 
 Documents referenced:
 Linux: Documentation/devicetree/bindings/pinctrl/fsl,imx-pinctrl.txt
@@ -38,36 +37,46 @@ uintptr_t iomuxc_dev_base;
 // Indicate this pin does not need config
 #define NO_PAD_CTL (1 << 31)
 
-typedef struct __attribute__((packed)) iomuxc_config {
+typedef struct __attribute__((packed)) pinctrl_pin_register {
     const uint32_t mux_reg;         /* Contains offset of mux registers */
     const uint32_t conf_reg;        /* Offset of pad configuration register */
     const uint32_t input_reg;       /* Offset of select input register */
     const uint32_t mux_val;         /* Mux values to be applied */
     const uint32_t input_val;       /* Select input values to be applied */
     const uint32_t pad_setting;     /* Pad configuration values to be applied */
-    const uint32_t phandle;
-    const char *src_device_path;    /* Device tree path of the device that this config data is a part of */
-    const char *config_name;        /* Config name as referenced by the device in the `pinctrl-names` prop */
-    const char *pinctrl_group_name; /* Config name as referenced by the iomuxc */
-} iomuxc_config_t;
+} pinctrl_pin_register_t;
+
+typedef struct __attribute__((packed)) pinctrl_client_device_state {
+    const char *state_name;
+    const uint32_t num_pins;
+    const pinctrl_pin_register_t *pins_reg;
+} pinctrl_client_device_state_t;
+
+typedef struct __attribute__((packed)) pinctrl_client_device_data {
+    const char *dev_dt_path;                      /* Device tree path of this particular device that needs pinctrl configuration */
+    const uint32_t num_states;                    /* Number of pinctrl states that this device requires as defined in the `pinctrl-names` prop */
+    const pinctrl_client_device_state_t **states;
+} pinctrl_client_device_data_t;
 
 // Data from Device Tree that is linked during compile time.
-extern const iomuxc_config_t iomuxc_configs[];
-extern const uint32_t num_iomuxc_configs;
+#define CONFIG_MAGIC 0x696D70696E6D7578 // "impinmux"
+extern const uint64_t pinctrl_config_data_magic;
+extern const pinctrl_client_device_data_t pinctrl_client_devices_configs[];
+extern const uint32_t num_pinctrl_client_devices_configs;
 
 bool sanity_check_pinctrl_reg_offset(uint32_t offset)
 {
+    // @billn: should really check the bound of the offset. to fix
     if (offset % 4 == 0) {
         return true;
     } else {
-        LOG_DRIVER_ERR("offset is not 4 bytes aligned\n");
+        LOG_DRIVER_ERR("offset 0x%x is not 4 bytes aligned\n", offset);
         return false;
     }
 }
 
 bool read_mux(uint32_t offset, uint32_t *ret)
 {
-    // @billn: should really check the bound of the offset. to fix
     if (!sanity_check_pinctrl_reg_offset(offset)) {
         return false;
     }
@@ -79,7 +88,6 @@ bool read_mux(uint32_t offset, uint32_t *ret)
 
 bool set_mux(uint32_t offset, uint32_t val)
 {
-    // @billn: should really check the bound of the offset. to fix
     if (!sanity_check_pinctrl_reg_offset(offset)) {
         return false;
     }
@@ -87,7 +95,6 @@ bool set_mux(uint32_t offset, uint32_t val)
     volatile uint32_t *mux_reg_vaddr = (uint32_t *)(iomuxc_dev_base + (uintptr_t)offset);
     *mux_reg_vaddr = val;
 
-    // Make sure the register is programmed with the expected value
     if (*mux_reg_vaddr != val) {
         LOG_DRIVER_ERR("write was not completed, real != expected: %x != %x", *mux_reg_vaddr, val);
         return false;
@@ -95,35 +102,38 @@ bool set_mux(uint32_t offset, uint32_t val)
     return true;
 }
 
-void debug_dts_print()
+void debug_print_pinctrl_config_data(void)
 {
-    LOG_DRIVER("nums of config is %u\n", num_iomuxc_configs);
-    LOG_DRIVER("data dump begin...one pin per line\n");
-    for (uint32_t i = 0; i < num_iomuxc_configs; i += 1) {
-        LOG_DRIVER("mux reg: 0x%x = 0x%x, input reg: 0x%x = 0x%x, pad conf reg: 0x%x = 0x%x from phandle 0x%x of device `%s` as part of its `%s` configuration in group `%s`\n",
-                   iomuxc_configs[i].mux_reg,
-                   iomuxc_configs[i].mux_val, iomuxc_configs[i].input_reg, iomuxc_configs[i].input_val,
-                   iomuxc_configs[i].conf_reg, iomuxc_configs[i].pad_setting, iomuxc_configs[i].phandle, iomuxc_configs[i].src_device_path,
-                   iomuxc_configs[i].config_name, iomuxc_configs[i].pinctrl_group_name);
+    LOG_DRIVER("STARTING PINCTRL CONFIG DUMP\n");
+    LOG_DRIVER("Total %u devices need pinctrl configuration.\n", num_pinctrl_client_devices_configs);
+    for (int i = 0; i < num_pinctrl_client_devices_configs; i++) {
+        LOG_DRIVER("** %s have the following %d states:\n", pinctrl_client_devices_configs[i].dev_dt_path, pinctrl_client_devices_configs[i].num_states);
+        for (int j = 0; j < pinctrl_client_devices_configs[i].num_states; j++) {
+            LOG_DRIVER("* State '%s' at index %d have %u pins:\n", pinctrl_client_devices_configs[i].states[j]->state_name, j, pinctrl_client_devices_configs[i].states[j]->num_pins);
+            for (int k = 0; k < pinctrl_client_devices_configs[i].states[j]->num_pins; k++) {
+                LOG_DRIVER("mux reg: 0x%x = 0x%x, input reg: 0x%x = 0x%x, pad conf reg: 0x%x = 0x%x\n",
+                    pinctrl_client_devices_configs[i].states[j]->pins_reg[k].mux_reg,
+                    pinctrl_client_devices_configs[i].states[j]->pins_reg[k].mux_val,
+                    pinctrl_client_devices_configs[i].states[j]->pins_reg[k].input_reg,
+                    pinctrl_client_devices_configs[i].states[j]->pins_reg[k].input_val,
+                    pinctrl_client_devices_configs[i].states[j]->pins_reg[k].conf_reg,
+                    pinctrl_client_devices_configs[i].states[j]->pins_reg[k].pad_setting
+                );
+            }
+        }
     }
+    LOG_DRIVER("---------------------------------------------------------------\n");
 }
 
-void reset_pinmux_to_default()
-{
-    int nums_pin_set = 0;
-    for (uint32_t i = 0; i < num_iomuxc_configs; i += 1) {
-        if (sddf_strcmp(iomuxc_configs[i].config_name, "default") != 0) {
-            continue;
-        }
-        nums_pin_set += 1;
-
+void pinctrl_set_state(pinctrl_client_device_state_t state) {
+    for (int j = 0; j < state.num_pins; j++) {
         // Write mux settings
-        if (!set_mux(iomuxc_configs[i].mux_reg, iomuxc_configs[i].mux_val)) {
+        if (!set_mux(state.pins_reg[j].mux_reg, state.pins_reg[j].mux_val)) {
             while (true) {};
         }
 
         // Write input settings
-        if (iomuxc_configs[i].input_reg) {
+        if (state.pins_reg[j].input_reg) {
             /*
                 * If the select input value begins with 0xff,
                 * it's a quirky select input and the value should
@@ -139,7 +149,7 @@ void reset_pinmux_to_default()
                 * and then decode them here for setting up the select
                 * input bits in general purpose register.
             */
-            if (iomuxc_configs[i].input_val >> 24 == 0xff) {
+            if (state.pins_reg[j].input_val >> 24 == 0xff) {
                 // uint32_t val = iomuxc_configs[i].input_val;
                 // uint8_t select = val & 0xff;
                 // uint8_t width = (val >> 8) & 0xff;
@@ -164,20 +174,31 @@ void reset_pinmux_to_default()
                 while (1);
             }
 
-            if (!set_mux(iomuxc_configs[i].input_reg, iomuxc_configs[i].input_val)) {
+            if (!set_mux(state.pins_reg[j].input_reg, state.pins_reg[j].input_val)) {
                 while (true) {};
             }
         }
 
         // Write pad settings
-        if (!(iomuxc_configs[i].pad_setting & NO_PAD_CTL)) {
-            if (!set_mux(iomuxc_configs[i].conf_reg, iomuxc_configs[i].pad_setting)) {
+        if (!(state.pins_reg[j].pad_setting & NO_PAD_CTL)) {
+            if (!set_mux(state.pins_reg[j].conf_reg, state.pins_reg[j].pad_setting)) {
                 while (true) {};
             }
         }
     }
+}
 
-    LOG_DRIVER("reset_pinmux_to_default(): set %d pins\n", nums_pin_set);
+void pinctrl_reset_all_default(void)
+{
+    for (int i = 0; i < num_pinctrl_client_devices_configs; i++) {
+        pinctrl_client_device_data_t client_device = pinctrl_client_devices_configs[i];
+        assert(client_device.num_states > 0);
+        pinctrl_client_device_state_t default_state = *(pinctrl_client_devices_configs[i].states[0]);
+        assert(sddf_strcmp(default_state.state_name, "default") == 0);
+
+        LOG_DRIVER("Setting dev %s to default pinctrl config with total %u pins.\n", client_device.dev_dt_path, default_state.num_pins);
+        pinctrl_set_state(default_state);
+    }
 }
 
 void init(void)
@@ -186,12 +207,15 @@ void init(void)
     assert(device_resources.num_irqs == 0);
     assert(device_resources.num_regions == 1);
 
+    assert(pinctrl_config_data_magic == CONFIG_MAGIC);
+    assert(num_pinctrl_client_devices_configs >= 1);
+
     iomuxc_dev_base = (uintptr_t) device_resources.regions[0].region.vaddr;
 
-    debug_dts_print();
-    reset_pinmux_to_default();
+    debug_print_pinctrl_config_data();
+    pinctrl_reset_all_default();
 
-    LOG_DRIVER("pinctrl device initialisation done\n");
+    LOG_DRIVER("INIT OK\n");
 }
 
 void notified(microkit_channel ch)
