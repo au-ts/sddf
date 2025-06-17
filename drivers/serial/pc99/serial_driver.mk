30439e24bcac2186f7e976e5275eef0d24a0f5d4
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2025-06-17 23:33:17 +1000

    drivers/serial/pc99: init
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

---
+++ drivers/serial/pc99/serial_driver.mk
@@ -0,0 +1,29 @@
+#
+# Copyright 2024, UNSW
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Include this snippet in your project Makefile to build
+# the PC99 UART driver.
+#
+# NOTES:
+#   Builds serial_driver.elf
+
+SERIAL_DRIVER_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
+serial_driver.elf: serial/pc99/serial_driver.o libsddf_util_debug.a
+	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
+
+serial/pc99/serial_driver.o: ${SERIAL_DRIVER_DIR}/uart.c |serial/pc99
+	$(CC) -c $(CFLAGS) -I${SERIAL_DRIVER_DIR}/include -o $@ $<
+
+serial/pc99:
+	mkdir -p $@
+
+-include serial/pc99/serial_driver.d
+
+clean::
+	rm -f serial/pc99/serial_driver.[do]
+
+clobber::
+	rm -rf serial
---
+++ drivers/serial/pc99/uart.c
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2025, UNSW
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <microkit.h>
+#include <sddf/util/printf.h>
+#include <sddf/util/util.h>
+#include <sddf/resources/device.h>
+#include <sddf/serial/config.h>
+#include <sddf/serial/queue.h>
+
+__attribute__((__section__(".device_resources"))) device_resources_t device_resources;
+__attribute__((__section__(".serial_driver_config"))) serial_driver_config_t config;
+
+#define IOPORT_ID 0
+#define IOPORT_BASE 0x3f8
+
+/*
+ * Port offsets
+ * W    - write
+ * R    - read
+ * RW   - read and write
+ * DLAB - Alternate register function bit
+ */
+
+#define SERIAL_THR  0 /* Transmitter Holding Buffer (W ) DLAB = 0 */
+#define SERIAL_RBR  0 /* Receiver Buffer            (R ) DLAB = 0 */
+#define SERIAL_DLL  0 /* Divisor Latch Low Byte     (RW) DLAB = 1 */
+#define SERIAL_IER  1 /* Interrupt Enable Register  (RW) DLAB = 0 */
+#define SERIAL_DLH  1 /* Divisor Latch High Byte    (RW) DLAB = 1 */
+#define SERIAL_IIR  2 /* Interrupt Identification   (R ) */
+#define SERIAL_FCR  2 /* FIFO Control Register      (W ) */
+#define SERIAL_LCR  3 /* Line Control Register      (RW) */
+#define SERIAL_MCR  4 /* Modem Control Register     (RW) */
+#define SERIAL_LSR  5 /* Line Status Register       (R ) */
+#define SERIAL_MSR  6 /* Modem Status Register      (R ) */
+#define SERIAL_SR   7 /* Scratch Register           (RW) */
+#define SERIAL_DLAB BIT(7)
+#define SERIAL_LSR_DATA_READY BIT(0)
+#define SERIAL_LSR_TRANSMITTER_EMPTY BIT(5)
+
+#define TX_CH  1
+
+serial_queue_handle_t tx_queue_handle;
+
+void write(uint16_t port_offset, uint8_t v)
+{
+    seL4_X86_IOPort_Out8(microkit_ioport_cap(IOPORT_ID), IOPORT_BASE + port_offset, v);
+}
+
+uint8_t read(uint16_t port_offset)
+{
+    return seL4_X86_IOPort_In8(microkit_ioport_cap(IOPORT_ID), IOPORT_BASE + port_offset).result;
+}
+
+int ready(void)
+{
+    return read(SERIAL_LSR) & SERIAL_LSR_TRANSMITTER_EMPTY;
+}
+
+void init(void)
+{
+    assert(serial_config_check_magic(&config));
+    assert(device_resources_check_magic(&device_resources));
+
+    serial_queue_init(&tx_queue_handle, config.tx.queue.vaddr, config.tx.data.size, config.tx.data.vaddr);
+
+    while (!(read(SERIAL_LSR) & 0x60)); /* wait until not busy */
+
+    write(SERIAL_LCR, 0x00);
+    write(SERIAL_IER, 0x00); /* disable generating interrupts */
+    write(SERIAL_LCR, 0x80); /* line control register: command: set divisor */
+    write(SERIAL_DLL, 0x01); /* set low byte of divisor to 0x01 = 115200 baud */
+    write(SERIAL_DLH, 0x00); /* set high byte of divisor to 0x00 */
+    write(SERIAL_LCR, 0x03); /* line control register: set 8 bit, no parity, 1 stop bit */
+    write(SERIAL_MCR, 0x0b); /* modem control register: set DTR/RTS/OUT2 */
+
+    read(SERIAL_RBR); /* clear receiver port */
+    read(SERIAL_LSR); /* clear line status port */
+    read(SERIAL_MSR); /* clear modem status port */
+}
+
+static void tx_provide(void)
+{
+    bool transferred = false;
+    char c;
+    while (!serial_queue_empty(&tx_queue_handle, tx_queue_handle.queue->head)) {
+        serial_dequeue(&tx_queue_handle, &c);
+        while (!ready());
+        write(SERIAL_THR, c);
+        transferred = true;
+    }
+
+    if (transferred && serial_require_consumer_signal(&tx_queue_handle)) {
+        serial_cancel_consumer_signal(&tx_queue_handle);
+        microkit_notify(TX_CH);
+    }
+}
+
+void notified(microkit_channel ch)
+{
+    switch (ch) {
+    case TX_CH:
+        tx_provide();
+        break;
+    default:
+        sddf_dprintf("UART|LOG: received notification on unexpected channel: %u\n", ch);
+        break;
+    }
+}
