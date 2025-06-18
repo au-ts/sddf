d405eb906b829746bab2920b6ca3dc40b096dda7
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2025-06-18 15:44:52 +1000

    drivers/network/virtio: implement transport abstraction
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- drivers/network/virtio/mmio/eth_driver.mk
+++ drivers/network/virtio/mmio/eth_driver.mk
@@ -1,16 +1,19 @@
 #
-# Copyright 2024, UNSW
+# Copyright 2025, UNSW
 #
 # SPDX-License-Identifier: BSD-2-Clause
 #
 # Include this snippet in your project Makefile to build
-# the VirtIO driver
+# the VirtIO network driver
 #
 # NOTES:
 #   Generates eth_driver.elf
 #   Assumes libsddf_util_debug.a is in LIBS
 
 ETHERNET_DRIVER_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+ETHERNET_COMMON_DIR := $(dir $(lastword $(MAKEFILE_LIST)))/../common
+# @billn this is not really elegant, should be going from sddf root
+VIRTIO_TRANSPORT_DIR := $(dir $(lastword $(MAKEFILE_LIST)))/../../../../virtio/transport/
 
 CHECK_NETDRV_FLAGS_MD5:=.netdrv_cflags-$(shell echo -- ${CFLAGS} ${CFLAGS_network} | shasum | sed 's/ *-//')
 
@@ -18,11 +21,15 @@ ${CHECK_NETDRV_FLAGS_MD5}:
 	-rm -f .netdrv_cflags-*
 	touch $@
 
-eth_driver.elf: network/virtio/mmio/ethernet.o
-	$(LD) $(LDFLAGS) $< $(LIBS) -o $@
+eth_driver.elf: network/virtio/mmio/ethernet.o network/virtio/mmio/transport.o
+	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
 
-network/virtio/mmio/ethernet.o: ${ETHERNET_DRIVER_DIR}/ethernet.c ${CHECK_NETDRV_FLAGS}
+network/virtio/mmio/ethernet.o: ${ETHERNET_COMMON_DIR}/ethernet.c ${CHECK_NETDRV_FLAGS}
 	mkdir -p network/virtio/mmio
 	${CC} -c ${CFLAGS} ${CFLAGS_network} -I ${ETHERNET_DRIVER_DIR} -o $@ $<
+
+network/virtio/mmio/transport.o: ${VIRTIO_TRANSPORT_DIR}/mmio.c ${CHECK_NETDRV_FLAGS}
+	mkdir -p network/virtio/mmio
+	${CC} -c ${CFLAGS} -o $@ $<
 
 -include virtio/mmio/ethernet.d
