ee0e00cdc68929b3ca16ecdbe28d2ea33ac0c6b8
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2025-06-19 16:20:08 +1000

    virtio/transport: implement PCI layer
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- include/sddf/virtio/transport/pci.h
+++ include/sddf/virtio/transport/pci.h
@@ -6,7 +6,7 @@
 
 #include <stdint.h>
 
-struct virtio_pci_common_cfg {
+typedef volatile struct __attribute__((packed)) virtio_pci_common_cfg {
     /* About the whole device. */
     uint32_t device_feature_select; /* read-write */
     uint32_t device_feature;        /* read-only for driver */
@@ -28,4 +28,40 @@ struct virtio_pci_common_cfg {
     uint64_t queue_device;      /* read-write */
     uint16_t queue_notify_data; /* read-only for driver */
     uint16_t queue_reset;       /* read-write */
+} virtio_pci_common_cfg_t;
+
+/* Possible values for cfg_type */
+/* Common configuration */
+#define VIRTIO_PCI_CAP_COMMON_CFG 1
+/* Notifications */
+#define VIRTIO_PCI_CAP_NOTIFY_CFG 2
+/* ISR Status */
+#define VIRTIO_PCI_CAP_ISR_CFG 3
+/* Device specific configuration */
+#define VIRTIO_PCI_CAP_DEVICE_CFG 4
+/* PCI configuration access */
+#define VIRTIO_PCI_CAP_PCI_CFG 5
+/* Shared memory region */
+#define VIRTIO_PCI_CAP_SHARED_MEMORY_CFG 8
+/* Vendor-specific data */
+#define VIRTIO_PCI_CAP_VENDOR_CFG 9
+
+#define PCI_CAP_ID_VNDR   0x09
+
+typedef volatile struct __attribute__((packed)) virtio_pci_cap {
+    uint8_t cap_vndr;   /* Generic PCI field: PCI_CAP_ID_VNDR */
+    uint8_t cap_next;   /* Generic PCI field: next ptr. */
+    uint8_t cap_len;    /* Generic PCI field: capability length */
+    uint8_t cfg_type;   /* Identifies the structure. */
+    uint8_t bar;        /* Where to find it. */
+    uint8_t id;         /* Multiple capabilities of the same type */
+    uint8_t padding[2]; /* Pad to full dword. */
+    uint32_t offset;    /* Offset within bar. */
+    uint32_t length;    /* Length of the structure, in bytes. */
+} virtio_pci_cap_t;
+
+typedef volatile struct __attribute__((packed)) virtio_pci_cap64 {
+    virtio_pci_cap_t cap;
+    uint32_t offset_hi;
+    uint32_t length_hi;
 };
