49368289476724c7772076662497f3f9ad5886b7
julia <git.ts@trainwit.ch>
2025-07-09 14:34:38 +1000

    lwip: add update lwip script with explicit patches
    Now LWIP is of a known version with known changes.
    
    Signed-off-by: julia <git.ts@trainwit.ch>

---
+++ network/ipstacks/update_lwip.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Copyright 2025, UNSW
+# SPDX-License-Identifier: BSD-2-Clause
+
+set -euo pipefail
+
+LWIP_VERSION="2.1.3"
+
+wget --continue "https://download-mirror.savannah.gnu.org/releases/lwip/lwip-${LWIP_VERSION}.zip" -O "lwip.zip"
+
+rm -rf lwip/
+unzip lwip.zip
+mv "lwip-${LWIP_VERSION}/" lwip/
+
+find lwip/ -type f -print0 | xargs -0 dos2unix
+rm -rf lwip/doc/doxygen/output/html/
+rm lwip/pax_global_header
+
+patch -p 1 < lwip-optimised-checksum.patch
+patch -p 1 < no-ssize-t-typedef.patch
+patch -p 1 < no-dhcp-arp-check.patch
