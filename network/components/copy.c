11ac779b3f84663ed54300e91c0941fdf6efe3c5
Courtney Darville <courtneydarville94@outlook.com>
2025-07-18 12:26:51 +1000

    Update copy component to drop active Rx DMA buffers if client buffer queue is empty
    Signed-off-by: Courtney Darville <courtneydarville94@outlook.com>

--- network/components/copy.c
+++ network/components/copy.c
@@ -18,65 +18,69 @@ net_queue_handle_t rx_queue_cli;
 
 void rx_return(void)
 {
-    bool enqueued = false;
+    bool client_enqueued = false;
+    bool virt_enqueued = false;
     bool reprocess = true;
 
     while (reprocess) {
-        while (!net_queue_empty_active(&rx_queue_virt) && !net_queue_empty_free(&rx_queue_cli)) {
-            net_buff_desc_t cli_buffer, virt_buffer = {0};
-            int err = net_dequeue_free(&rx_queue_cli, &cli_buffer);
-            assert(!err);
+        while (!net_queue_empty_active(&rx_queue_virt)) {
+            /* Copy into client buffer if available, else return to rx virt free queue */
+            if (!net_queue_empty_free(&rx_queue_cli)) {
+                net_buff_desc_t cli_buffer, virt_buffer = { 0 };
+                int err = net_dequeue_free(&rx_queue_cli, &cli_buffer);
+                assert(!err);
 
-            if (cli_buffer.io_or_offset % NET_BUFFER_SIZE
-                || cli_buffer.io_or_offset >= NET_BUFFER_SIZE * rx_queue_cli.capacity) {
-                sddf_dprintf("COPY|LOG: Client provided offset %lx which is not buffer aligned or outside of buffer region\n",
-                             cli_buffer.io_or_offset);
-                continue;
+                if (cli_buffer.io_or_offset % NET_BUFFER_SIZE
+                    || cli_buffer.io_or_offset >= NET_BUFFER_SIZE * rx_queue_cli.capacity) {
+                    sddf_dprintf("COPY|LOG: Client provided offset %lx which is not buffer aligned or outside of "
+                                 "buffer region\n",
+                                 cli_buffer.io_or_offset);
+                    continue;
+                }
+
+                err = net_dequeue_active(&rx_queue_virt, &virt_buffer);
+                assert(!err);
+
+                void *cli_addr = config.client_data.vaddr + cli_buffer.io_or_offset;
+                void *virt_addr = config.device_data.vaddr + virt_buffer.io_or_offset;
+
+                sddf_memcpy(cli_addr, virt_addr, virt_buffer.len);
+                cli_buffer.len = virt_buffer.len;
+                virt_buffer.len = 0;
+
+                err = net_enqueue_active(&rx_queue_cli, cli_buffer);
+                assert(!err);
+
+                err = net_enqueue_free(&rx_queue_virt, virt_buffer);
+                assert(!err);
+
+                client_enqueued = true;
+            } else {
+                net_buff_desc_t virt_buffer = { 0 };
+                int err = net_dequeue_active(&rx_queue_virt, &virt_buffer);
+                assert(!err);
+
+                err = net_enqueue_free(&rx_queue_virt, virt_buffer);
+                assert(!err);
             }
-
-            err = net_dequeue_active(&rx_queue_virt, &virt_buffer);
-            assert(!err);
-
-            void *cli_addr = config.client_data.vaddr + cli_buffer.io_or_offset;
-            void *virt_addr = config.device_data.vaddr + virt_buffer.io_or_offset;
-
-            sddf_memcpy(cli_addr, virt_addr, virt_buffer.len);
-            cli_buffer.len = virt_buffer.len;
-            virt_buffer.len = 0;
-
-            err = net_enqueue_active(&rx_queue_cli, cli_buffer);
-            assert(!err);
-
-            err = net_enqueue_free(&rx_queue_virt, virt_buffer);
-            assert(!err);
-
-            enqueued = true;
+            virt_enqueued = true;
         }
 
         net_request_signal_active(&rx_queue_virt);
-
-        /* Only request signal from client if incoming packets from multiplexer are awaiting free buffers */
-        if (!net_queue_empty_active(&rx_queue_virt)) {
-            net_request_signal_free(&rx_queue_cli);
-        } else {
-            net_cancel_signal_free(&rx_queue_cli);
-        }
-
         reprocess = false;
 
-        if (!net_queue_empty_active(&rx_queue_virt) && !net_queue_empty_free(&rx_queue_cli)) {
+        if (!net_queue_empty_active(&rx_queue_virt)) {
             net_cancel_signal_active(&rx_queue_virt);
-            net_cancel_signal_free(&rx_queue_cli);
             reprocess = true;
         }
     }
 
-    if (enqueued && net_require_signal_active(&rx_queue_cli)) {
+    if (client_enqueued && net_require_signal_active(&rx_queue_cli)) {
         net_cancel_signal_active(&rx_queue_cli);
         sddf_notify(config.client.id);
     }
 
-    if (enqueued && net_require_signal_free(&rx_queue_virt)) {
+    if (virt_enqueued && net_require_signal_free(&rx_queue_virt)) {
         net_cancel_signal_free(&rx_queue_virt);
         sddf_deferred_notify(config.virt_rx.id);
     }
