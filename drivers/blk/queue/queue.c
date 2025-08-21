6d3af04613cc5c816683b033cad5e6190b815fb1
sawlani63 <reachchiragsawlani@gmail.com>
2025-08-21 14:26:39 +1000

    minor change

--- drivers/blk/queue/queue.c
+++ drivers/blk/queue/queue.c
@@ -59,6 +59,63 @@ typedef struct FS_request{
 
 fs_request request_pool[4];
 
+typedef struct ext4_fs_cmd_params_file_open {
+    fs_buffer_t path;
+    uint32_t parent_inode_num;
+    bool create;
+    uint16_t ftype;
+    uint32_t name_off;
+    uint8_t _padding[4];
+    uint64_t inode;
+} ext4_fs_cmd_params_file_open_t;
+
+typedef struct ext4_fs_cmd_params_file_read {
+    uint32_t inode;
+    uint64_t _padding;
+    uint64_t offset;
+    fs_buffer_t read_buf;  
+    uint64_t n;             
+} ext4_fs_cmd_params_file_read_t;
+
+typedef struct ext4_fs_cmd_params_file_write {
+    uint32_t inode;
+    uint64_t _padding;   
+    uint64_t offset;
+    fs_buffer_t write_buf;   
+    uint64_t n;                
+} ext4_fs_cmd_params_file_write_t;
+
+typedef union ext4_fs_cmd_params {
+    ext4_fs_cmd_params_file_open_t file_open;
+    ext4_fs_cmd_params_file_read_t file_read;
+    ext4_fs_cmd_params_file_write_t file_write;
+    uint8_t min_size[48];
+} ext4_fs_cmd_params_t;
+
+typedef struct ext4_fs_cmd {
+    uint64_t id;
+    uint64_t type;
+    ext4_fs_cmd_params_t params;
+} ext4_fs_cmd_t;
+
+typedef struct ext4_fs_cmpl {
+    uint64_t id;
+    uint64_t status;
+    ext4_fs_cmd_params_t data;
+} ext4_fs_cmpl_t;
+
+typedef union ext4_fs_msg {
+    ext4_fs_cmd_t cmd;
+    ext4_fs_cmpl_t cmpl;
+} ext4_fs_msg_t;
+
+typedef struct ext4_fs_queue {
+    uint64_t head;
+    uint64_t tail;
+    uint8_t padding[48];
+    ext4_fs_msg_t buffer[FS_QUEUE_CAPACITY];
+} ext4_fs_queue_t;
+
 /* Wrapper functions to get around inline for FFI */
 
 bool blk_storage_is_ready_wrapper(blk_storage_info_t *storage_info)
@@ -130,4 +187,42 @@ void fs_queue_publish_consumption_wrapper(fs_queue_t *queue, uint64_t amount_con
 void fs_queue_publish_production_wrapper(fs_queue_t *queue, uint64_t amount_produced)
 {
     fs_queue_publish_production(queue, amount_produced);
+}
+
+void init(void) {
+    assert(fs_config_check_magic(&fs_config));
+    assert(blk_config_check_magic(&blk_config));
+
+    assert(blk_config.virt.num_buffers >= FAT_WORKER_THREAD_NUM);
+
+    max_cluster_size = blk_config.data.size / FAT_WORKER_THREAD_NUM;
+    fs_command_queue = fs_config.client.command_queue.vaddr;
+    fs_completion_queue = fs_config.client.completion_queue.vaddr;
+    fs_share = fs_config.client.share.vaddr;
+
+    blk_data = blk_config.data.vaddr;
+
+    blk_queue_init(&blk_queue, blk_config.virt.req_queue.vaddr, blk_config.virt.resp_queue.vaddr, blk_config.virt.num_buffers);
+
+    blk_storage_info = blk_config.virt.storage_info.vaddr;
+
+    /* Wait for the the block device before doing anything else */
+    while (!blk_storage_is_ready(blk_storage_info));
+
+    /*
+       This part of the code is for setting up the thread pool by
+       assign stacks and size of the stack to the pool
+    */
+    stack_ptrs_arg_array_t costacks = {
+        (uintptr_t) &worker_thread_stack_one,
+        (uintptr_t) &worker_thread_stack_two,
+        (uintptr_t) &worker_thread_stack_three,
+        (uintptr_t) &worker_thread_stack_four
+    };
+
+    // Init thread pool
+    microkit_cothread_init(&co_controller_mem, FAT_WORKER_THREAD_STACKSIZE, costacks);
+    for (uint32_t i = 0; i < (FAT_WORKER_THREAD_NUM + 1); i++) {
+        microkit_cothread_semaphore_init(&sem[i]);
+    }
 }
