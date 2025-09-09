afc0512eec055901919bfbbb25036db949a848b4
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2025-09-09 21:12:17 +1000

    drivers/blk/virtio: updates for new virtio transport abstraction
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- drivers/blk/virtio/blk_driver.mk
+++ drivers/blk/virtio/blk_driver.mk
@@ -7,12 +7,16 @@
 # Assumes libsddf_util_debug.a is in ${LIBS}.
 
 VIRTIO_BLK_DRIVER_DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
+VIRTIO_TRANSPORT_DIR := $(SDDF)/virtio/transport/
 
-blk_driver.elf: blk/virtio/blk_driver.o
+blk_driver.elf: blk/virtio/blk_driver.o blk/virtio/transport.o
 	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
 
 blk/virtio/blk_driver.o: ${VIRTIO_BLK_DRIVER_DIR}/block.c |blk/virtio
 	$(CC) -c $(CFLAGS) -I${VIRTIO_BLK_DRIVER_DIR}/include -o $@ $<
+
+blk/virtio/transport.o: ${VIRTIO_TRANSPORT_DIR}/mmio.c
+	${CC} -c ${CFLAGS} -o $@ $<
 
 -include blk_driver.d
 
