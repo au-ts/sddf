2cb4830eda50254a7617b393d2a1e8371d486bfc
Ivan Velickovic <i.velickovic@unsw.edu.au>
2025-09-24 19:15:46 +1000

    Update to sdfgen 0.26.3
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     zig-overlay.url = "github:mitchellh/zig-overlay";
     zig-overlay.inputs.nixpkgs.follows = "nixpkgs";
-    sdfgen.url = "github:au-ts/microkit_sdf_gen/0.26.1";
+    sdfgen.url = "github:au-ts/microkit_sdf_gen/0.26.3";
     sdfgen.inputs.nixpkgs.follows = "nixpkgs";
   };
 
