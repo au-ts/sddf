6dd67ae8d26a68ecea0752191913800bde473e69
Peter Chubb <Peter.Chubb@unsw.edu.au>
2025-09-24 10:27:29 +1000

    Add ETH_DRIV definition to maaxboard
    Somehome I missed this on the last pass.
    
    Signed-off-by: Peter Chubb <Peter.Chubb@unsw.edu.au>

--- tools/Make/board/maaxboard.mk
+++ tools/Make/board/maaxboard.mk
@@ -6,8 +6,11 @@
 # Set up variables for the MAAXboard
 # Should be included _before_ toolchain makefile.
 PLATFORM := imx
+
 NET_DRIV_DIR := ${PLATFORM}
+ETH_DRIV := eth_driver_${PLATFORM}.elf
 UART_DRIV_DIR := ${PLATFORM}
 TIMER_DRIV_DIR := ${PLATFORM}
 I2C_DRIV_DIR := ${PLATFORM}
+
 CPU := cortex-a53
