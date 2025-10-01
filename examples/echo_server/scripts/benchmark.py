ec46cede371084bbdf713587c26f81affdf6bb71
Ivan Velickovic <i.velickovic@unsw.edu.au>
2025-10-01 14:11:54 +1000

    examples/echo_server: improve documentation
    * add diagram of architecture (thank you Krishnan)
    * explain basic architecture
    * add details on how the benchmarking setup works and how to
      reproduce it
    * move our private benchmark.py ipbench helper script into
      this repo since it is actually already generic and so anyone
      can make use of it assuming they have their own ipbench setup
    * add basic connect.py script to test TCP/UDP connections
    
    Co-authored-by: Krishnan Winter <krishnan-git@wintermail.me>
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

---
+++ examples/echo_server/scripts/benchmark.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python3
+
+# Copyright 2025, UNSW
+# SPDX-License-Identifier: BSD-2-Clause
+
+import argparse
+import datetime as dt
+import socket
+import subprocess
+from pathlib import Path
+
+IPBENCHD_PORT = 8036
+udp_packet_size_default = 1472
+tcp_packet_size_default = 1460
+
+
+def abort_clients(clients: list[str]):
+    for client in clients:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((client, IPBENCHD_PORT))
+        sock.send(b"ABORT\n")
+        sock.shutdown(socket.SHUT_WR)
+        while True:
+            data = sock.recv(4096)
+            if not data:
+                break
+            print(data)
+        sock.close()
+
+
+def run_benchmark(args, throughput: int, packet_size: int):
+    argv = [
+        "ipbench",
+        "--debug",
+    ]
+
+    make_arg_list = lambda **opts: ",".join(f"{k}={v}" for k, v in opts.items())
+
+    # test mode
+    argv += [
+        "--test",
+        "latency",
+        "--test-args",
+        make_arg_list(
+            socktype=args.protocol,
+            bps=throughput // len(args.clients),
+            size=packet_size,
+            warmup=args.warmup_seconds,
+            cooldown=args.cooldown_seconds,
+            samples=args.samples // len(args.clients),
+        ),
+    ]
+
+    # remote clients
+    argv += ["--port", str(IPBENCHD_PORT)]
+    for client in args.clients:
+        argv += ["--client", client]
+
+    # device under test
+    argv += [
+        # echo server port
+        "--test-target",
+        args.ip,
+        "--test-port",
+        {
+            "tcp": "1237",
+            "udp": "1235",
+        }[args.protocol],
+        # benchmark stuff
+        "--target-test",
+        "cpu_target_lukem",
+        "--target-test-hostname",
+        args.ip,
+        "--target-test-port",
+        "1236",
+    ]
+
+    p = subprocess.run(argv, stdout=subprocess.PIPE)
+    p.check_returncode()
+
+    return p.stdout.decode("utf-8").replace("\n", "")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("ip", help="IP address of machine to benchmark")
+
+    parser.add_argument(
+        "--clients",
+        nargs="+",
+        required=True,
+    )
+    parser.add_argument(
+        "--udp",
+        action="store_const",
+        dest="protocol",
+        const="udp",
+        default="udp",
+    )
+    parser.add_argument(
+        "--tcp",
+        action="store_const",
+        dest="protocol",
+        const="tcp",
+    )
+    parser.add_argument(
+        "--warmup-seconds",
+        type=int,
+        default=10,
+    )
+    parser.add_argument(
+        "--cooldown-seconds",
+        type=int,
+        default=10,
+    )
+    parser.add_argument(
+        "--samples",
+        type=int,
+        default=200_000,
+    )
+    parser.add_argument(
+        "--packet-sizes",
+        nargs="+",
+        type=int,
+    )
+    # Note that by default we stop at 970Mb/s as the default packet size of 1470
+    # means that the actual achievable throughput is 1470/(1470 + 40) as there is about
+    # 40 octets of overhead for each packet due to 20 octets of overhead for each
+    # ethernet packet plus 12 octets of IP, plus eight octets of UDP overhead.
+    default_throughputs_mbs = [
+        10,
+        20,
+        50,
+        100,
+        200,
+        300,
+        400,
+        500,
+        600,
+        700,
+        800,
+        900,
+        970,
+    ]
+    default_throughputs = [t * 1000 * 1000 for t in default_throughputs_mbs]
+    parser.add_argument(
+        "--throughputs", nargs="+", type=int, default=default_throughputs
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    packet_sizes = args.packet_sizes
+    if not packet_sizes:
+        default = udp_packet_size_default
+        if args.protocol == "tcp":
+            default = tcp_packet_size_default
+        packet_sizes = [default]
+
+    abort_clients(args.clients)
+    abort_clients(args.clients)
+
+    file = dt.datetime.now().strftime("output-%Y-%m-%dT%H:%M:%S.csv")
+
+    output_link = Path("output.csv")
+    if output_link.is_symlink():
+        output_link.unlink()
+        output_link.symlink_to(file)
+
+    heading = "Requested_Throughput,Receive_Throughput,Send_Throughput,Packet_Size,Minimum_RTT,Average_RTT,Maximum_RTT,Stdev_RTT,Median_RTT,Bad_Packets,Idle_Cycles,Total_Cycles\n"
+
+    data = ""
+    with open(file, "w") as data_out:
+        data_out.write(heading)
+        data += heading
+
+        try:
+            for packet_size in packet_sizes:
+                for throughput in args.throughputs:
+                    row = run_benchmark(args, throughput, packet_size) + "\n"
+
+                    data_out.write(row)
+                    data += row
+                    print(heading, row)
+
+        finally:
+            abort_clients(args.clients)
+
+        print("Result Summary:")
+        print(data)
