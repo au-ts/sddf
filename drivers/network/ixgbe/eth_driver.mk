a9ca6c7cca098552e7d631c861d043b131039bbf
Terry Bai <terry.z.bai@gmail.com>
2025-10-01 13:40:24 +1000

    add ixgbe
    Signed-off-by: Terry Bai <terry.z.bai@gmail.com>

---
+++ drivers/network/ixgbe/eth_driver.mk
@@ -0,0 +1,28 @@
+#
+# Copyright 2024, UNSW
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Include this snippet in your project Makefile to build
+# the IXGBE NIC driver
+#
+# NOTES
+#  Generates eth_driver.elf (alternative unique name eth_driver_ixgbe.elf)
+#  Expects libsddf_util_debug.a to be in LIBS
+
+ETHERNET_DRIVER_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+CHECK_NETDRV_FLAGS_MD5:=.netdrv_cflags-$(shell echo -- ${CFLAGS} ${CFLAGS_network} | shasum | sed 's/ *-//')
+
+${CHECK_NETDRV_FLAGS_MD5}:
+	-rm -f .netdrv_cflags-*
+	touch $@
+
+eth_driver.elf eth_driver_ixgbe.elf: network/ixgbe/ethernet.o
+	$(LD) $(LDFLAGS) $< $(LIBS) -o $@
+
+network/ixgbe/ethernet.o: ${ETHERNET_DRIVER_DIR}/ethernet.c ${CHECK_NETDRV_FLAGS_MD5}
+	mkdir -p network/ixgbe
+	${CC} -c ${CFLAGS} ${CFLAGS_network} -I ${ETHERNET_DRIVER_DIR} -o $@ $<
+
+
+-include ixgbe/ethernet.d
