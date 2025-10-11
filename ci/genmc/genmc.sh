b3b954d426c9a65e90adef21eba2fd7beb8bf446
Kurt Wu <rihui.wu@unsw.edu.au>
2025-10-11 09:50:35 +0000

    ci: test SMP implementation
    Signed-off-by: Kurt Wu <rihui.wu@unsw.edu.au>

--- ci/genmc/genmc.sh
+++ ci/genmc/genmc.sh
@@ -6,26 +6,32 @@ cd "$(dirname "$0")/../.."
 
 set -ev
 
-export CMD="genmc --disable-estimation --disable-mm-detector --v1 -- -I./ci/genmc -I./include"
+for i in `seq 0 1`; do
+  export CMD="genmc --disable-estimation --disable-mm-detector --v1 -- -I./ci/genmc -I./include"
 
-$CMD ./ci/genmc/network/test1.c
+  if [ "$i" -eq 1 ]; then
+    export CMD="$CMD -DCONFIG_ENABLE_SMP_SUPPORT=1"
+  fi
 
-$CMD -DQUEUE_SIZE=2 -DPRODUCER=2 -DCONSUMER=2 ./ci/genmc/network/test1.c
+  $CMD ./ci/genmc/network/test1.c
 
-$CMD -DQUEUE_SIZE=2 -DPRODUCER=4 -DCONSUMER=4 ./ci/genmc/network/test1.c
+  $CMD -DQUEUE_SIZE=2 -DPRODUCER=2 -DCONSUMER=2 ./ci/genmc/network/test1.c
 
-$CMD -DQUEUE_SIZE=2 -DPRODUCER=6 -DCONSUMER=6 ./ci/genmc/network/test1.c
+  $CMD -DQUEUE_SIZE=2 -DPRODUCER=4 -DCONSUMER=4 ./ci/genmc/network/test1.c
 
-$CMD -DQUEUE_SIZE=2 -DPRODUCER=8 -DCONSUMER=8 ./ci/genmc/network/test1.c
+  $CMD -DQUEUE_SIZE=2 -DPRODUCER=6 -DCONSUMER=6 ./ci/genmc/network/test1.c
 
-$CMD -DQUEUE_SIZE=3 -DPRODUCER=4 -DCONSUMER=4 ./ci/genmc/network/test1.c
+  $CMD -DQUEUE_SIZE=2 -DPRODUCER=8 -DCONSUMER=8 ./ci/genmc/network/test1.c
 
-$CMD -DQUEUE_SIZE=3 -DPRODUCER=6 -DCONSUMER=6 ./ci/genmc/network/test1.c
+  $CMD -DQUEUE_SIZE=3 -DPRODUCER=4 -DCONSUMER=4 ./ci/genmc/network/test1.c
 
-$CMD -DQUEUE_SIZE=3 -DPRODUCER=7 -DCONSUMER=7 ./ci/genmc/network/test1.c
+  $CMD -DQUEUE_SIZE=3 -DPRODUCER=6 -DCONSUMER=6 ./ci/genmc/network/test1.c
 
-$CMD -DQUEUE_SIZE=4 -DPRODUCER=4 -DCONSUMER=4 ./ci/genmc/network/test1.c
+  $CMD -DQUEUE_SIZE=3 -DPRODUCER=7 -DCONSUMER=7 ./ci/genmc/network/test1.c
 
-$CMD -DQUEUE_SIZE=4 -DPRODUCER=6 -DCONSUMER=6 ./ci/genmc/network/test1.c
+  $CMD -DQUEUE_SIZE=4 -DPRODUCER=4 -DCONSUMER=4 ./ci/genmc/network/test1.c
 
-$CMD -DQUEUE_SIZE=4 -DPRODUCER=7 -DCONSUMER=6 ./ci/genmc/network/test1.c
+  $CMD -DQUEUE_SIZE=4 -DPRODUCER=6 -DCONSUMER=6 ./ci/genmc/network/test1.c
+
+  $CMD -DQUEUE_SIZE=4 -DPRODUCER=7 -DCONSUMER=6 ./ci/genmc/network/test1.c
+done
