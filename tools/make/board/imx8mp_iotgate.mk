au-ts/sddf

commit 0911c90aa7c39a26de52f566b422cc530605bd4e

Abstract away board and toolchain details
Put the common bits in a makefile snippet.

Signed-off-by: Peter Chubb <Peter.Chubb@unsw.edu.au>

tools/make/board/imx8mp_iotgate.mk
@@ -1,0 +1,18 @@
+#
+# Copyright 2023, UNSW
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Set up variables for IMX8MQ_evk
+# Should be included _before_ toolchain makefile.
+PLATFORM := imx
+
+I2C_DRIV_DIR := ${PLATFORM}
+NET_DRIV_DIR := ${PLATFORM}
+ETH_DRIV := eth_driver_${PLATFORM}.elf
+ETH_DRIV_DIR1 := dwmac-5.10a
+ETH_DRIV_1 := eth_driver_dwmac-5.10a.elf
+TIMER_DRIV_DIR := ${PLATFORM}
+UART_DRIV_DIR := ${PLATFORM}
+
+CPU := cortex-a53

drivers/blk/mmc/imx/blk_driver.mk
@@ -7,6 +7,8 @@
 # Assumes libsddf_util_debug.a is in ${LIBS}.
 
 USDHC_DRIVER_DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
+# This block driver needs a configured timer driver
+BLK_NEED_TIMER := 1
 
 blk_driver.elf: blk/mmc/imx/blk_driver.o
 	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@

drivers/network/dwmac-5.10a/eth_driver.mk
@@ -18,7 +18,7 @@ ${CHECK_NETDRV_FLAGS_MD5}:
 	-rm -f .netdrv_cflags-*
 	touch $@
 
-eth_driver.elf eth_driver_dwmac-5.10a.elf: network/dwmac-5.10a/ethernet.o
+eth_driver_dwmac-5.10a.elf: network/dwmac-5.10a/ethernet.o
 	$(LD) $(LDFLAGS) $< $(LIBS) -o $@
 
 network/dwmac-5.10a/ethernet.o: ${ETHERNET_DRIVER_DIR}ethernet.c ${CHECK_NETDRV_FLAGS} | $(SDDF_LIBC_INCLUDE)

drivers/network/imx/eth_driver.mk
@@ -17,7 +17,7 @@ ${CHECK_NETDRV_FLAGS_MD5}:
 	-rm -f .netdrv_cflags-*
 	touch $@
 
-eth_driver.elf eth_driver_imx.elf: network/imx/ethernet.o
+eth_driver_imx.elf: network/imx/ethernet.o
 	$(LD) $(LDFLAGS) $< $(LIBS) -o $@
 
 network/imx/ethernet.o: ${ETHERNET_DRIVER_DIR}/ethernet.c ${CHECK_NETDRV_FLAGS_MD5} | $(SDDF_LIBC_INCLUDE)

drivers/network/meson/eth_driver.mk
@@ -18,7 +18,7 @@ ${CHECK_NETDRV_FLAGS_MD5}:
 	-rm -f .netdrv_cflags-*
 	touch $@
 
-eth_driver.elf eth_driver_meson.elf: network/meson/ethernet.o
+eth_driver_meson.elf: network/meson/ethernet.o
 	$(LD) $(LDFLAGS) $< $(LIBS) -o $@
 
 network/meson/ethernet.o: ${ETHERNET_DRIVER_DIR}/ethernet.c ${CHECK_NETDRV_FLAGS_MD5} | $(SDDF_LIBC_INCLUDE)

drivers/network/virtio/eth_driver.mk
@@ -18,7 +18,7 @@ ${CHECK_NETDRV_FLAGS_MD5}:
 	-rm -f .netdrv_cflags-*
 	touch $@
 
-eth_driver.elf eth_driver_virtio.elf: network/virtio/ethernet.o
+eth_driver_virtio.elf: network/virtio/ethernet.o
 	$(LD) $(LDFLAGS) $< $(LIBS) -o $@
 
 network/virtio/ethernet.o: ${ETHERNET_DRIVER_DIR}/ethernet.c ${CHECK_NETDRV_FLAGS} | $(SDDF_LIBC_INCLUDE)

examples/blk/blk.mk
@@ -19,20 +19,8 @@ ifeq ($(strip $(TOOLCHAIN)),)
 	TOOLCHAIN := clang
 endif
 
-ifeq ($(strip $(TOOLCHAIN)), clang)
-	CC := clang
-	LD := ld.lld
-	AR := llvm-ar
-	RANLIB := llvm-ranlib
-	OBJCOPY := llvm-objcopy
-else
-	CC := $(TOOLCHAIN)-gcc
-	LD := $(TOOLCHAIN)-ld
-	AS := $(TOOLCHAIN)-as
-	AR := $(TOOLCHAIN)-ar
-	RANLIB := $(TOOLCHAIN)-ranlib
-	OBJCOPY := $(TOOLCHAIN)-objcopy
-endif
+BUILD_DIR ?= build
+MICROKIT_CONFIG ?= debug
 
 # Allow to user to specify a custom partition
 PARTITION :=
@@ -44,75 +32,40 @@ IMAGE_FILE := loader.img
 REPORT_FILE  := report.txt
 SYSTEM_FILE := blk.system
 
-ifeq ($(strip $(MICROKIT_BOARD)), qemu_virt_aarch64)
-	BLK_DRIVER_DIR := virtio
-	SERIAL_DRIVER_DIR := arm
-	CPU := cortex-a53
-	QEMU := qemu-system-aarch64
-	QEMU_ARCH_ARGS := -machine virt,virtualization=on -cpu cortex-a53 -device loader,file=$(IMAGE_FILE),addr=0x70000000,cpu-num=0
-else ifeq ($(strip $(MICROKIT_BOARD)), qemu_virt_riscv64)
-	BLK_DRIVER_DIR := virtio
-	SERIAL_DRIVER_DIR := ns16550a
-	QEMU := qemu-system-riscv64
-	QEMU_ARCH_ARGS := -machine virt -kernel $(IMAGE_FILE)
-else ifeq ($(strip $(MICROKIT_BOARD)), maaxboard)
-	CPU := cortex-a53
-	SERIAL_DRIVER_DIR := imx
-	BLK_DRIVER_DIR := mmc/imx
-	TIMER_DRIVER_DIR := imx
-else
-$(error Unsupported MICROKIT_BOARD given)
-endif
-
-DTC := dtc
-PYTHON ?= python3
-
-BUILD_DIR ?= build
-MICROKIT_CONFIG ?= debug
+SUPPORTED_BOARDS := qemu_virt_aarch64 \
+		    qemu_virt_riscv64 \
+		    maaxboard \
 
 TOP := ${SDDF}/examples/blk
 CONFIGS_INCLUDE := ${TOP}
-
-MICROKIT_TOOL ?= $(MICROKIT_SDK)/bin/microkit
-
-BOARD_DIR := $(MICROKIT_SDK)/board/$(MICROKIT_BOARD)/$(MICROKIT_CONFIG)
-ARCH := ${shell grep 'CONFIG_SEL4_ARCH  ' $(BOARD_DIR)/include/kernel/gen_config.h | cut -d' ' -f4}
 SDDF_CUSTOM_LIBC := 1
 
+include ${SDDF}/tools/make/board/common.mk
+
+
 IMAGES := blk_driver.elf client.elf blk_virt.elf serial_virt_tx.elf serial_driver.elf
-CFLAGS := -nostdlib \
-		  -ffreestanding \
-		  -g3 \
-		  -O3 \
-		  -Wall -Wno-unused-function -Werror -Wno-unused-command-line-argument \
-		  -I$(BOARD_DIR)/include \
+CFLAGS +=  -Wall -Wno-unused-function -Werror -Wno-unused-command-line-argument \
 		  -I$(SDDF)/include \
 		  -I$(SDDF)/include/microkit \
 		  -I$(CONFIGS_INCLUDE)
+
 LDFLAGS := -L$(BOARD_DIR)/lib
 LIBS := --start-group -lmicrokit -Tmicrokit.ld libsddf_util_debug.a --end-group
 
-ifeq ($(ARCH),aarch64)
-	CFLAGS += -mcpu=$(CPU) -target aarch64-none-elf
-else ifeq ($(ARCH),riscv64)
-	CFLAGS += -march=rv64imafdc -target riscv64-none-elf
-endif
-
-DTS := $(SDDF)/dts/$(MICROKIT_BOARD).dts
-DTB := $(MICROKIT_BOARD).dtb
 METAPROGRAM := $(TOP)/meta.py
 
-BLK_DRIVER := $(SDDF)/drivers/blk/${BLK_DRIVER_DIR}
-SERIAL_DRIVER := $(SDDF)/drivers/serial/${SERIAL_DRIVER_DIR}
+BLK_DRIVER := $(SDDF)/drivers/blk/${BLK_DRIV_DIR}
+SERIAL_DRIVER := $(SDDF)/drivers/serial/${UART_DRIV_DIR}
 
 all: $(IMAGE_FILE)
 
-include ${SDDF}/drivers/blk/${BLK_DRIVER_DIR}/blk_driver.mk
-include ${SDDF}/drivers/serial/${SERIAL_DRIVER_DIR}/serial_driver.mk
+include ${SDDF}/drivers/blk/${BLK_DRIV_DIR}/blk_driver.mk
+include ${SDDF}/drivers/serial/${UART_DRIV_DIR}/serial_driver.mk
 
-ifdef TIMER_DRIVER_DIR
-include ${SDDF}/drivers/timer/${TIMER_DRIVER_DIR}/timer_driver.mk
+ifdef BLK_NEED_TIMER
+include ${SDDF}/drivers/timer/${TIMER_DRIV_DIR}/timer_driver.mk
 IMAGES += timer_driver.elf
+export BLK_NEED_TIMER
 endif
 
 include ${SDDF}/util/util.mk
@@ -126,12 +79,12 @@ client.o: ${TOP}/client.c ${TOP}/basic_data.h
 client.elf: client.o libsddf_util.a
 	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
 
-$(DTB): $(DTS)
-	dtc -q -I dts -O dtb $(DTS) > $(DTB)
-
 $(SYSTEM_FILE): $(METAPROGRAM) $(IMAGES) $(DTB)
-	$(PYTHON) $(METAPROGRAM) --sddf $(SDDF) --board $(MICROKIT_BOARD) --dtb $(DTB) --output . --sdf $(SYSTEM_FILE) $(PARTITION_ARG)
-ifdef TIMER_DRIVER_DIR
+	$(PYTHON) \
+	$(METAPROGRAM) --sddf $(SDDF) --board $(MICROKIT_BOARD) \
+	--dtb $(DTB) --output . --sdf $(SYSTEM_FILE) $(PARTITION_ARG) \
+	$${BLK_NEED_TIMER:+--need_timer}
+ifdef BLK_NEED_TIMER
 	$(OBJCOPY) --update-section .device_resources=timer_driver_device_resources.data timer_driver.elf
 	$(OBJCOPY) --update-section .timer_client_config=timer_client_blk_driver.data blk_driver.elf
 endif
@@ -143,6 +96,7 @@ endif
 	$(OBJCOPY) --update-section .serial_driver_config=serial_driver_config.data serial_driver.elf
 	$(OBJCOPY) --update-section .serial_virt_tx_config=serial_virt_tx.data serial_virt_tx.elf
 	$(OBJCOPY) --update-section .serial_client_config=serial_client_client.data client.elf
+	touch $@
 
 $(IMAGE_FILE) $(REPORT_FILE): $(IMAGES) $(SYSTEM_FILE)
 	$(MICROKIT_TOOL) $(SYSTEM_FILE) --search-path $(BUILD_DIR) --board $(MICROKIT_BOARD) --config $(MICROKIT_CONFIG) -o $(IMAGE_FILE) -r $(REPORT_FILE)
@@ -152,13 +106,11 @@ qemu_disk:
 
 qemu: ${IMAGE_FILE} qemu_disk
 	$(QEMU) $(QEMU_ARCH_ARGS) \
-			-serial mon:stdio \
-			-m size=2G \
-			-nographic \
-            -d guest_errors \
-            -global virtio-mmio.force-legacy=false \
-            -drive file=disk,if=none,format=raw,id=hd \
-            -device virtio-blk-device,drive=hd
+	    -nographic \
+	    -d guest_errors \
+	    -global virtio-mmio.force-legacy=false \
+	    -drive file=disk,if=none,format=raw,id=hd \
+	    -device virtio-blk-device,drive=hd
 
 clean::
 	rm -f client.o

examples/blk/build.zig
@@ -191,6 +191,9 @@ pub fn build(b: *std.Build) !void {
     run_metaprogram.addArg(microkit_board);
     run_metaprogram.addArg("--sdf");
     run_metaprogram.addArg("blk.system");
+    if (timer_driver_install != null) {
+        run_metaprogram.addArg("--need_timer");
+    }
     if (partition) |p| {
         run_metaprogram.addArg("--partition");
         run_metaprogram.addArg(b.fmt("{}", .{p}));

commit 9551f7e9181acd7e91d5c5d5dd67673df49ee801

Fix selected ethernet driver for imx8mp_iotgate
Regression from 0911c90aa7c39a26de52f566b422cc530605bd4e.

Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

tools/make/board/imx8mp_iotgate.mk
@@ -3,15 +3,15 @@
 #
 # SPDX-License-Identifier: BSD-2-Clause
 #
-# Set up variables for IMX8MQ_evk
+# Set up variables for i.MX8MP IOTGATE
 # Should be included _before_ toolchain makefile.
 PLATFORM := imx
 
 I2C_DRIV_DIR := ${PLATFORM}
-NET_DRIV_DIR := ${PLATFORM}
-ETH_DRIV := eth_driver_${PLATFORM}.elf
-ETH_DRIV_DIR1 := dwmac-5.10a
-ETH_DRIV_1 := eth_driver_dwmac-5.10a.elf
+NET_DRIV_DIR := dwmac-5.10a
+ETH_DRIV := eth_driver_${NET_DRIV_DIR}.elf
+ETH_DRIV_DIR1 := imx
+ETH_DRIV_1 := eth_driver_${ETH_DRIV_DIR1}.elf
 TIMER_DRIV_DIR := ${PLATFORM}
 UART_DRIV_DIR := ${PLATFORM}
 