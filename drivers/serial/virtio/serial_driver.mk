f00d0303978a7faa638444f4b4a75e7892eb99f9
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2025-11-18 15:07:42 +1100

    drivers/serial: add pc99
    Signed-off-by: Terry Bai <tianyi.bai@unsw.edu.au>

--- drivers/serial/virtio/serial_driver.mk
+++ drivers/serial/virtio/serial_driver.mk
@@ -7,12 +7,16 @@
 # the virtio console driver.
 
 SERIAL_DRIVER_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+VIRTIO_TRANSPORT_DIR := $(SDDF)/virtio/transport/
 
-serial_driver.elf: serial/virtio/serial_driver.o libsddf_util_debug.a
+serial_driver.elf: serial/virtio/serial_driver.o serial/virtio/mmio_transport.o libsddf_util_debug.a
 	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
 
 serial/virtio/serial_driver.o: ${SERIAL_DRIVER_DIR}/console.c |serial/virtio $(SDDF_LIBC_INCLUDE)
 	$(CC) -c $(CFLAGS) -I${SERIAL_DRIVER_DIR}/include -o $@ $<
+
+serial/virtio/mmio_transport.o: ${VIRTIO_TRANSPORT_DIR}/mmio.c
+	${CC} -c ${CFLAGS} -DVIRTIO_MMIO_TRANSPORT_FOR_CONSOLE -o $@ $<
 
 serial/virtio:
 	mkdir -p $@
