3b33a9658d45116464fbd378bc40c4f79892e2f9
Courtney Darville <courtneydarville94@outlook.com>
2025-11-19 17:53:57 +1100

    examples/echo_server: add support for SMP configurations
    Signed-off-by: Courtney Darville <courtneydarville94@outlook.com>

--- include/sddf/benchmark/config.h
+++ include/sddf/benchmark/config.h
@@ -7,6 +7,7 @@
 
 #include <os/sddf.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 /* At the moment we run systems that contain this benchmarking code on architectures
  * where we cannot properly do benchmarking. We also include the benchmarking PD
@@ -35,20 +36,50 @@ typedef struct benchmark_child_config {
 } benchmark_child_config_t;
 
 typedef struct benchmark_config {
-    uint8_t start_ch;
-    uint8_t stop_ch;
+    /* Channel a benchmark PD receives the benchmark start notification on. */
+    uint8_t rx_start_ch;
+    /* Channel a benchmark PD transmits the benchmark start notification on
+    (start signal is propagated to benchmark PDs on each core). */
+    uint8_t tx_start_ch;
+    /* Channel a benchmark PD receives the benchmark stop notification on. */
+    uint8_t rx_stop_ch;
+    /* Channel a benchmark PD transmits the benchmark stop notification on
+    (stop signal is propagated to benchmark PDs on each core). */
+    uint8_t tx_stop_ch;
+    /* Initialisation channel shared with the idle thread on the same core. */
     uint8_t init_ch;
+    /* Core a benchmark PD resides on. Used for displaying benchmark results. */
+    uint8_t core;
+    /* Whether a benchmark PD is on the last core to receive start and stop
+    signals. If last_core is set to true, the above tx channels are invalid. */
+    bool last_core;
+    /* Number of PDs requiring cycle counts sharing a core with this benchmark
+    PD. */
     uint8_t num_children;
+    /* PDs requiring cycle counts sharing a core with this benchmark PD. */
     benchmark_child_config_t children[BENCHMARK_MAX_CHILDREN];
 } benchmark_config_t;
 
 typedef struct benchmark_idle_config {
+    /* Address to store the core's cycle counts. Shared with the benchmarking
+    client PD. */
     void *cycle_counters;
+    /* Initialisation channel shared with the benchmark PD on the same core. */
     uint8_t init_channel;
 } benchmark_idle_config_t;
 
 typedef struct benchmark_client_config {
-    void *cycle_counters;
+    /* Channel to notify the first benchmark PD that a benchmark has started.
+    Each benchmark PD propagates this notification to the benchmark PD on the
+    next core. */
     uint8_t start_ch;
+    /* Channel to notify the first benchmark PD that a benchmark has finished.
+    Each benchmark PD propagates this notification to the benchmark PD on the
+    next core. */
     uint8_t stop_ch;
+    /* Number of active cores in the system. */
+    uint8_t num_cores;
+    /* Addresses of each active core's cycle counts maintained by and shared
+    with the idle thread on that core. */
+    void *core_ccounts[CONFIG_MAX_NUM_NODES];
 } benchmark_client_config_t;
