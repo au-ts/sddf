161522afac9cf51e138c45868af6f3eda099bcc8
Kurt Wu <rihui.wu@unsw.edu.au>
2025-11-21 14:05:11 +1100

    ci/genmc: test debug-only assertions
    Signed-off-by: Kurt Wu <rihui.wu@unsw.edu.au>

--- ci/genmc/genmc.sh
+++ ci/genmc/genmc.sh
@@ -6,11 +6,15 @@ cd "$(dirname "$0")/../.."
 
 set -ev
 
-for i in `seq 0 1`; do
+for i in `seq 0 3`; do
   export CMD="genmc --disable-estimation --disable-mm-detector --v1 -- -I./ci/genmc -I./include"
 
-  if [ "$i" -eq 1 ]; then
+  if [ $(( i % 2 )) -eq 1 ]; then
     export CMD="$CMD -DCONFIG_ENABLE_SMP_SUPPORT=1"
+  fi
+
+  if [ $(( i / 2 )) -eq 1 ]; then
+    export CMD="$CMD -DCONFIG_DEBUG_BUILD=1"
   fi
 
   $CMD ./ci/genmc/serial/test1.c
@@ -30,11 +34,15 @@ for i in `seq 0 1`; do
   $CMD -DQUEUE_SIZE=4 -DPRODUCER=7 -DCONSUMER=6 ./ci/genmc/serial/test1.c
 done
 
-for i in `seq 0 1`; do
+for i in `seq 0 3`; do
   export CMD="genmc --disable-estimation --disable-mm-detector --v1 -- -I./ci/genmc -I./include"
 
-  if [ "$i" -eq 1 ]; then
+  if [ $(( i % 2 )) -eq 1 ]; then
     export CMD="$CMD -DCONFIG_ENABLE_SMP_SUPPORT=1"
+  fi
+
+  if [ $(( i / 2 )) -eq 1 ]; then
+    export CMD="$CMD -DCONFIG_DEBUG_BUILD=1"
   fi
 
   $CMD ./ci/genmc/serial/test2.c
@@ -72,11 +80,15 @@ for i in `seq 0 1`; do
   $CMD -DQUEUE_SIZE=4 -DPRODUCER=7 -DCONSUMER=6 -DBATCH_SIZE=4 ./ci/genmc/serial/test2.c
 done
 
-for i in `seq 0 1`; do
+for i in `seq 0 3`; do
   export CMD="genmc --disable-estimation --disable-mm-detector --v1 -- -I./ci/genmc -I./include"
 
-  if [ "$i" -eq 1 ]; then
+  if [ $(( i % 2 )) -eq 1 ]; then
     export CMD="$CMD -DCONFIG_ENABLE_SMP_SUPPORT=1"
+  fi
+
+  if [ $(( i / 2 )) -eq 1 ]; then
+    export CMD="$CMD -DCONFIG_DEBUG_BUILD=1"
   fi
 
   $CMD ./ci/genmc/serial/test3.c
