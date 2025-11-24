45d59c7024167d914c1f4e2a9ebac5124c397b1a
Terry Bai <tianyi.bai@unsw.edu.au>
2025-11-24 15:09:11 +1100

    makefiles: add QEMU device arguments
    Signed-off-by: Terry Bai <tianyi.bai@unsw.edu.au>

--- tools/make/board/qemu_virt_riscv64.mk
+++ tools/make/board/qemu_virt_riscv64.mk
@@ -11,4 +11,11 @@ TIMER_DRIV_DIR := goldfish
 UART_DRIV_DIR := ns16550a
 
 QEMU := qemu-system-riscv64
-QEMU_ARCH_ARGS := -machine virt -kernel $(IMAGE_FILE) -m size=2G -serial mon:stdio
+QEMU_ARCH_ARGS := -machine virt \
+					-kernel $(IMAGE_FILE) \
+					-m size=2G \
+					-serial mon:stdio
+
+QEMU_NET_ARGS := -device virtio-net-device,netdev=netdev0
+
+QEMU_BLK_ARGS := -device virtio-blk-device,drive=hd \
