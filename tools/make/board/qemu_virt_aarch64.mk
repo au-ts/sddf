f21e5dc536a9dc79e41a7369031ad15bd11e7a69
Terry Bai <tianyi.bai@unsw.edu.au>
2025-11-24 15:09:11 +1100

    makefiles: add QEMU device arguments
    Signed-off-by: Terry Bai <tianyi.bai@unsw.edu.au>

--- tools/make/board/qemu_virt_aarch64.mk
+++ tools/make/board/qemu_virt_aarch64.mk
@@ -17,6 +17,11 @@ UART_DRIV_DIR := ${PLATFORM}
 CPU := cortex-a53
 QEMU := qemu-system-aarch64
 QEMU_ARCH_ARGS := -machine virt,virtualization=on -cpu cortex-a53 \
-		  -m size=2G \
-		  -device loader,file=\$(IMAGE_FILE),addr=0x70000000,cpu-num=0 \
-		  -serial mon:stdio
+			-m size=2G \
+			-serial mon:stdio \
+			-global virtio-mmio.force-legacy=false \
+			-device loader,file=$(IMAGE_FILE),addr=0x70000000,cpu-num=0
+
+QEMU_NET_ARGS := -device virtio-net-device,netdev=netdev0
+
+QEMU_BLK_ARGS := -device virtio-blk-device,drive=hd
