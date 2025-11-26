727c9cd99977543995853ab09afd1914fc45555b
Lesley Rossouw <lesley.rossouw@unsw.edu.au>
2025-11-26 17:32:38 +1100

    Added I2C bus scan example
    Signed-off-by: Lesley Rossouw <lesley.rossouw@unsw.edu.au>

---
+++ examples/i2c_bus_scan/build.zig
@@ -0,0 +1,246 @@
+//
+// Copyright 2024, UNSW
+// SPDX-License-Identifier: BSD-2-Clause
+//
+const std = @import("std");
+const LazyPath = std.Build.LazyPath;
+const Step = std.Build.Step;
+
+const MicrokitBoard = enum { odroidc4, serengeti };
+
+const Target = struct {
+    board: MicrokitBoard,
+    zig_target: std.Target.Query,
+};
+
+const targets = [_]Target{
+    .{
+        .board = MicrokitBoard.odroidc4,
+        .zig_target = std.Target.Query{
+            .cpu_arch = .aarch64,
+            .cpu_model = .{ .explicit = &std.Target.aarch64.cpu.cortex_a55 },
+            .cpu_features_add = std.Target.aarch64.featureSet(&[_]std.Target.aarch64.Feature{.strict_align}),
+            .os_tag = .freestanding,
+            .abi = .none,
+        },
+    },
+    .{
+        .board = MicrokitBoard.serengeti,
+        .zig_target = std.Target.Query{
+            .cpu_arch = .riscv64,
+            .cpu_model = .{ .explicit = &std.Target.riscv.cpu.generic_rv64 },
+            .os_tag = .freestanding,
+            .abi = .none,
+        },
+    },
+};
+
+fn findTarget(board: MicrokitBoard) std.Target.Query {
+    for (targets) |target| {
+        if (board == target.board) {
+            return target.zig_target;
+        }
+    }
+
+    std.log.err("Board '{}' is not supported\n", .{board});
+    std.posix.exit(1);
+}
+
+const ConfigOptions = enum { debug, release, benchmark };
+
+fn updateSectionObjcopy(b: *std.Build, section: []const u8, data_output: std.Build.LazyPath, data: []const u8, elf: []const u8) *Step.Run {
+    const run_objcopy = b.addSystemCommand(&[_][]const u8{
+        "llvm-objcopy",
+    });
+    run_objcopy.addArg("--update-section");
+    const data_full_path = data_output.join(b.allocator, data) catch @panic("OOM");
+    run_objcopy.addPrefixedFileArg(b.fmt("{s}=", .{section}), data_full_path);
+    run_objcopy.addFileArg(.{ .cwd_relative = b.getInstallPath(.bin, elf) });
+
+    // We need the ELFs we talk about to be in the install directory first.
+    run_objcopy.step.dependOn(b.getInstallStep());
+
+    return run_objcopy;
+}
+
+pub fn build(b: *std.Build) !void {
+    const optimize = b.standardOptimizeOption(.{});
+
+    const default_python = if (std.posix.getenv("PYTHON")) |p| p else "python3";
+    const python = b.option([]const u8, "python", "Path to Python to use") orelse default_python;
+
+    const microkit_sdk = b.option(LazyPath, "sdk", "Path to Microkit SDK") orelse {
+        std.log.err("Missing -Dsdk=<sdk> argument", .{});
+        return error.MissingSdkPath;
+    };
+
+    const microkit_config_option = b.option(ConfigOptions, "config", "Microkit config to build for") orelse .debug;
+    const microkit_config = @tagName(microkit_config_option);
+
+    const microkit_board_option = b.option(MicrokitBoard, "board", "Microkit board to target") orelse {
+        std.log.err("Missing -Dboard=<board> argument", .{});
+        return error.MissingBoard;
+    };
+
+    const target = b.resolveTargetQuery(findTarget(microkit_board_option));
+    const microkit_board = @tagName(microkit_board_option);
+
+    const microkit_board_dir = microkit_sdk.path(b, "board").path(b, microkit_board).path(b, microkit_config);
+    const microkit_tool = microkit_sdk.path(b, "bin/microkit");
+    const libmicrokit = microkit_board_dir.path(b, "lib/libmicrokit.a");
+    const libmicrokit_include = microkit_board_dir.path(b, "include");
+    const libmicrokit_linker_script = microkit_board_dir.path(b, "lib/microkit.ld");
+
+    const sddf_dep = b.dependency("sddf", .{
+        .target = target,
+        .optimize = optimize,
+        .microkit_board_dir = microkit_board_dir,
+    });
+
+    const i2c_driver_class = switch (microkit_board_option) {
+        .odroidc4 => "meson",
+        .serengeti => "opentitan",
+    };
+
+    const serial_driver_class = switch (microkit_board_option) {
+        .odroidc4 => "meson",
+        .serengeti => "ns16550a",
+    };
+
+    const timer_driver_class = switch (microkit_board_option) {
+        .odroidc4 => "meson",
+        .serengeti => "apb_timer",
+    };
+
+    const timer_driver = sddf_dep.artifact(b.fmt("driver_timer_{s}.elf", .{timer_driver_class}));
+    // This is required because the SDF file is expecting a different name to the artifact we
+    // are dealing with.
+    const timer_driver_install = b.addInstallArtifact(timer_driver, .{ .dest_sub_path = "timer_driver.elf" });
+    const serial_driver = sddf_dep.artifact(b.fmt("driver_serial_{s}.elf", .{serial_driver_class}));
+    // This is required because the SDF file is expecting a different name to the artifact we
+    // are dealing with.
+    const serial_driver_install = b.addInstallArtifact(serial_driver, .{ .dest_sub_path = "serial_driver.elf" });
+
+    const scan_driver = sddf_dep.artifact("driver_i2c_device_scan");
+
+    const i2c_driver = sddf_dep.artifact(b.fmt("driver_i2c_{s}.elf", .{i2c_driver_class}));
+    // This is required because the SDF file is expecting a different name to the artifact we
+    // are dealing with.
+    const i2c_driver_install = b.addInstallArtifact(i2c_driver, .{ .dest_sub_path = "i2c_driver.elf" });
+
+    const client_scan = b.addExecutable(.{
+        .name = "client_scan.elf",
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+            .strip = false,
+        }),
+    });
+
+    client_scan.addCSourceFiles(.{
+        .files = &.{"client_scan.c"},
+        .flags = &.{"-DLIBI2C_RAW"},
+    });
+    client_scan.addIncludePath(sddf_dep.path("include"));
+    client_scan.addIncludePath(sddf_dep.path("include/microkit"));
+    client_scan.linkLibrary(sddf_dep.artifact("util"));
+    client_scan.linkLibrary(sddf_dep.artifact("util_putchar_serial"));
+    client_scan.linkLibrary(scan_driver);
+    client_scan.linkLibrary(sddf_dep.artifact("libi2c_raw"));
+
+    client_scan.addIncludePath(sddf_dep.path("libco"));
+    client_scan.addCSourceFile(.{ .file = sddf_dep.path("libco/libco.c") });
+
+    client_scan.addIncludePath(libmicrokit_include);
+    client_scan.addObjectFile(libmicrokit);
+    client_scan.setLinkerScript(libmicrokit_linker_script);
+
+    b.installArtifact(client_scan);
+    b.installArtifact(sddf_dep.artifact("i2c_virt.elf"));
+    b.installArtifact(sddf_dep.artifact("serial_virt_tx.elf"));
+
+    // For compiling the DTS into a DTB
+    const dts = sddf_dep.path(b.fmt("dts/{s}.dts", .{microkit_board}));
+    const dtc_cmd = b.addSystemCommand(&[_][]const u8{ "dtc", "-q", "-I", "dts", "-O", "dtb" });
+    dtc_cmd.addFileInput(dts);
+    dtc_cmd.addFileArg(dts);
+    const dtb = dtc_cmd.captureStdOut();
+
+    // Run the metaprogram to get sDDF configuration binary files and the SDF file.
+    const metaprogram = b.path("meta.py");
+    const run_metaprogram = b.addSystemCommand(&[_][]const u8{
+        python,
+    });
+    run_metaprogram.addFileArg(metaprogram);
+    run_metaprogram.addFileInput(metaprogram);
+    run_metaprogram.addPrefixedDirectoryArg("--sddf=", sddf_dep.path(""));
+    run_metaprogram.addPrefixedDirectoryArg("--dtb=", dtb);
+    const meta_output = run_metaprogram.addPrefixedOutputDirectoryArg("--output=", "meta_output");
+    run_metaprogram.addArg("--board");
+    run_metaprogram.addArg(microkit_board);
+    run_metaprogram.addArg("--sdf");
+    run_metaprogram.addArg("i2c.system");
+
+    const meta_output_install = b.addInstallDirectory(.{
+        .source_dir = meta_output,
+        .install_dir = .prefix,
+        .install_subdir = "meta_output",
+    });
+
+    const serial_driver_resources_objcopy = updateSectionObjcopy(b, ".device_resources", meta_output, "serial_driver_device_resources.data", "serial_driver.elf");
+    const serial_driver_config_objcopy = updateSectionObjcopy(b, ".serial_driver_config", meta_output, "serial_driver_config.data", "serial_driver.elf");
+    const serial_virt_objcopy = updateSectionObjcopy(b, ".serial_virt_tx_config", meta_output, "serial_virt_tx.data", "serial_virt_tx.elf");
+    serial_driver_resources_objcopy.step.dependOn(&serial_driver_install.step);
+    serial_driver_config_objcopy.step.dependOn(&serial_driver_install.step);
+    const client_scan_serial_objcopy = updateSectionObjcopy(b, ".serial_client_config", meta_output, "serial_client_client_scan.data", "client_scan.elf");
+
+    const timer_driver_objcopy = updateSectionObjcopy(b, ".device_resources", meta_output, "timer_driver_device_resources.data", "timer_driver.elf");
+    timer_driver_objcopy.step.dependOn(&timer_driver_install.step);
+    const client_scan_timer_objcopy = updateSectionObjcopy(b, ".timer_client_config", meta_output, "timer_client_client_scan.data", "client_scan.elf");
+
+    const i2c_driver_device_objcopy = updateSectionObjcopy(b, ".device_resources", meta_output, "i2c_driver_device_resources.data", "i2c_driver.elf");
+    i2c_driver_device_objcopy.step.dependOn(&i2c_driver_install.step);
+    const i2c_driver_config_objcopy = updateSectionObjcopy(b, ".i2c_driver_config", meta_output, "i2c_driver.data", "i2c_driver.elf");
+    i2c_driver_config_objcopy.step.dependOn(&i2c_driver_install.step);
+    const i2c_virt_objcopy = updateSectionObjcopy(b, ".i2c_virt_config", meta_output, "i2c_virt.data", "i2c_virt.elf");
+    const client_scan_i2c_objcopy = updateSectionObjcopy(b, ".i2c_client_config", meta_output, "i2c_client_client_scan.data", "client_scan.elf");
+
+    const objcopys = &.{
+        serial_driver_resources_objcopy,
+        serial_driver_config_objcopy,
+        serial_virt_objcopy,
+        client_scan_serial_objcopy,
+        client_scan_i2c_objcopy,
+        client_scan_timer_objcopy,
+        i2c_virt_objcopy,
+        i2c_driver_device_objcopy,
+        i2c_driver_config_objcopy,
+        timer_driver_objcopy,
+    };
+
+    const final_image_dest = b.getInstallPath(.bin, "./loader.img");
+    const microkit_tool_cmd = Step.Run.create(b, "run microkit tool");
+    microkit_tool_cmd.addFileArg(microkit_tool);
+    microkit_tool_cmd.addArgs(&[_][]const u8{
+        b.getInstallPath(.{ .custom = "meta_output" }, "i2c.system"),
+        "--search-path",
+        b.getInstallPath(.bin, ""),
+        "--board",
+        microkit_board,
+        "--config",
+        microkit_config,
+        "-o",
+        final_image_dest,
+        "-r",
+        b.getInstallPath(.prefix, "./report.txt"),
+    });
+    inline for (objcopys) |objcopy| {
+        microkit_tool_cmd.step.dependOn(&objcopy.step);
+    }
+    microkit_tool_cmd.step.dependOn(&meta_output_install.step);
+    microkit_tool_cmd.step.dependOn(b.getInstallStep());
+    microkit_tool_cmd.setEnvironmentVariable("MICROKIT_SDK", microkit_sdk.getPath3(b, null).toString(b.allocator) catch @panic("OOM"));
+    const microkit_step = b.step("microkit", "Compile and build the final bootable image");
+    microkit_step.dependOn(&microkit_tool_cmd.step);
+    b.default_step = microkit_step;
+}
---
+++ examples/i2c_bus_scan/meta.py
@@ -0,0 +1,102 @@
+# Copyright 2025, UNSW
+# SPDX-License-Identifier: BSD-2-Clause
+import os, sys
+import argparse
+from typing import List
+from dataclasses import dataclass
+from sdfgen import SystemDescription, Sddf, DeviceTree
+from importlib.metadata import version
+
+sys.path.append(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../tools/meta")
+)
+from board import BOARDS
+
+assert version("sdfgen").split(".")[1] == "27", "Unexpected sdfgen version"
+
+ProtectionDomain = SystemDescription.ProtectionDomain
+MemoryRegion = SystemDescription.MemoryRegion
+Map = SystemDescription.Map
+
+
+def generate(sdf_file: str, output_dir: str, dtb: DeviceTree):
+    serial_driver = ProtectionDomain("serial_driver", "serial_driver.elf", priority=200)
+    # Increase the stack size as running with UBSAN uses more stack space than normal.
+    serial_virt_tx = ProtectionDomain(
+        "serial_virt_tx", "serial_virt_tx.elf", priority=199, stack_size=0x2000
+    )
+
+    timer_driver = ProtectionDomain("timer_driver", "timer_driver.elf", priority=4)
+    i2c_driver = ProtectionDomain("i2c_driver", "i2c_driver.elf", priority=3)
+    i2c_virt = ProtectionDomain("i2c_virt", "i2c_virt.elf", priority=2)
+    client_scan = ProtectionDomain("client_scan", "client_scan.elf", priority=1)
+
+    # Right now we do not have separate clk and GPIO drivers and so our I2C driver does manual
+    # clk/GPIO setup for I2C.
+    clk_mr = MemoryRegion(sdf, "clk", 0x1000, paddr=0xFF63C000)
+    gpio_mr = MemoryRegion(sdf, "gpio", 0x1000, paddr=0xFF634000)
+    sdf.add_mr(clk_mr)
+    sdf.add_mr(gpio_mr)
+
+    i2c_driver.add_map(Map(clk_mr, 0x30_000_000, "rw", cached=False))
+    i2c_driver.add_map(Map(gpio_mr, 0x30_100_000, "rw", cached=False))
+
+    i2c_node = dtb.node(board.i2c)
+    assert i2c_node is not None
+    timer_node = dtb.node(board.timer)
+    assert timer_node is not None
+    serial_node = dtb.node(board.serial)
+    assert serial_node is not None
+
+    i2c_system = Sddf.I2c(sdf, i2c_node, i2c_driver, i2c_virt)
+    i2c_system.add_client(client_scan)
+
+    timer_system = Sddf.Timer(sdf, timer_node, timer_driver)
+    timer_system.add_client(client_scan)
+
+    serial_system = Sddf.Serial(
+        sdf, serial_node, serial_driver, serial_virt_tx, enable_color=False
+    )
+    serial_system.add_client(client_scan)
+
+    pds = [
+        serial_driver,
+        serial_virt_tx,
+        timer_driver,
+        i2c_driver,
+        i2c_virt,
+        client_scan,
+    ]
+    for pd in pds:
+        sdf.add_pd(pd)
+
+    assert i2c_system.connect()
+    assert i2c_system.serialise_config(output_dir)
+    assert serial_system.connect()
+    assert serial_system.serialise_config(output_dir)
+    assert timer_system.connect()
+    assert timer_system.serialise_config(output_dir)
+
+    with open(f"{output_dir}/{sdf_file}", "w+") as f:
+        f.write(sdf.render())
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dtb", required=True)
+    parser.add_argument("--sddf", required=True)
+    parser.add_argument("--board", required=True, choices=[b.name for b in BOARDS])
+    parser.add_argument("--output", required=True)
+    parser.add_argument("--sdf", required=True)
+
+    args = parser.parse_args()
+
+    board = next(filter(lambda b: b.name == args.board, BOARDS))
+
+    sdf = SystemDescription(board.arch, board.paddr_top)
+    sddf = Sddf(args.sddf)
+
+    with open(args.dtb, "rb") as f:
+        dtb = DeviceTree(f.read())
+
+    generate(args.sdf, args.output, dtb)
