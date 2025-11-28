e150bf80df3a46d4e31c90d7f6f25be20022fce9
Ivan Velickovic <i.velickovic@unsw.edu.au>
2025-11-28 15:34:25 +1100

    tools/make: board fixes
    Could probably just get these to include each other...
    
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- tools/make/board/rpi4b_4gb.mk
+++ tools/make/board/rpi4b_4gb.mk
@@ -3,11 +3,9 @@
 #
 # SPDX-License-Identifier: BSD-2-Clause
 #
-# Set up variables for Raspberry Pi 4b ith 1Gb RAM
+# Set up variables for Raspberry Pi 4B ith 4GB RAM
 # Should be included _before_ toolchain makefile.
-# NET_DRIV_DIR :=
-# ETH_DRIV := eth_driver_dwmac-5.10a.elf
+
 UART_DRIV_DIR := ns16550a
 TIMER_DRIV_DIR := bcm2835
-#I2C_DRIV_DIR := ${PLATFORM}
 CPU := cortex-a72
--- tools/make/board/rpi4b_8gb.mk
+++ tools/make/board/rpi4b_8gb.mk
@@ -3,11 +3,9 @@
 #
 # SPDX-License-Identifier: BSD-2-Clause
 #
-# Set up variables for Raspberry Pi 4b ith 1Gb RAM
+# Set up variables for Raspberry Pi 4B ith 8GB RAM
 # Should be included _before_ toolchain makefile.
-# NET_DRIV_DIR := genet-v5
-# ETH_DRIV :=
+
 UART_DRIV_DIR := ns16550a
 TIMER_DRIV_DIR := bcm2835
-#I2C_DRIV_DIR := ${PLATFORM}
 CPU := cortex-a72
