594ab76e258c1d7eaee87dd63ccffa668e14f802
Ivan Velickovic <10481259+Ivan-Velickovic@users.noreply.github.com>
2025-12-05 10:05:10 +1000

    Update to sdfgen 0.28.1 (#582)
    Necessary for x86-64 related changes and custom serial
    baud rate.
    
    0.28.0 had a strange error when not building from source
    on Linux x86-64: 'Illegal hardware instruction'. It is not
    clear why this happens and the only difference between 0.28.1
    which seems to work and 0.28.0 is that the PyPI package has
    been rebuilt.
    
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     zig-overlay.url = "github:mitchellh/zig-overlay";
     zig-overlay.inputs.nixpkgs.follows = "nixpkgs";
-    sdfgen.url = "github:au-ts/microkit_sdf_gen/0.27.0";
+    sdfgen.url = "github:au-ts/microkit_sdf_gen/0.28.1";
     sdfgen.inputs.nixpkgs.follows = "nixpkgs";
   };
 
