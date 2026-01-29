d1b4000e81cfafe9ea7b5d99bed070c5f9285a65
Lesley Rossouw <lesley.rossouw@unsw.edu.au>
2026-01-29 14:05:37 +1100

    Fix GPIO example (WIP)
    Signed-off-by: Lesley Rossouw <lesley.rossouw@unsw.edu.au>

--- examples/gpio/gpio.mk
+++ examples/gpio/gpio.mk
@@ -3,55 +3,32 @@
 #
 # SPDX-License-Identifier: BSD-2-Clause
 #
+# This Makefile is copied into the build directory
+# and operated on from there.
+#
 
 ifeq ($(strip $(MICROKIT_SDK)),)
 $(error MICROKIT_SDK must be specified)
 endif
 
-ifeq ($(strip $(SDDF)),)
-$(error SDDF must be specified)
-endif
-
-BUILD_DIR ?= build
-MICROKIT_CONFIG ?= debug
-IMAGE_FILE := loader.img
-REPORT_FILE := report.txt
-
 ifeq ($(strip $(TOOLCHAIN)),)
 	TOOLCHAIN := clang
 endif
-ifeq ($(strip $(TOOLCHAIN)), clang)
-	CC := clang
-	LD := ld.lld
-	AR := llvm-ar
-	RANLIB := llvm-ranlib
-	OBJCOPY := llvm-objcopy
-else
-	CC := $(TOOLCHAIN)-gcc
-	LD := $(TOOLCHAIN)-ld
-	AS := $(TOOLCHAIN)-as
-	AR := $(TOOLCHAIN)-ar
-	RANLIB := $(TOOLCHAIN)-ranlib
-	OBJCOPY := $(TOOLCHAIN)-objcopy
-endif
-DTC := dtc
-PYTHON ?= python3
 
-MICROKIT_TOOL ?= $(MICROKIT_SDK)/bin/microkit
+PYTHONPATH := ${SDDF}/tools/meta:${PYTHONPATH}
+export PYTHONPATH
 
-ifeq ($(strip $(MICROKIT_BOARD)), odroidc4)
-	export GPIO_DRIVER_DIR := meson
-	export CPU := cortex-a55
-else
-$(error Unsupported MICROKIT_BOARD given)
-endif
+SUPPORTED_BOARDS := \
+		odroidc4
 
+include ${SDDF}/tools/make/board/common.mk
+
+SDDF_CUSTOM_LIBC := 1
 TOP:= ${SDDF}/examples/gpio
 METAPROGRAM := $(TOP)/meta.py
 UTIL := $(SDDF)/util
 LIBCO := $(SDDF)/libco
-GPIO_DRIVER := $(SDDF)/drivers/gpio/$(GPIO_DRIVER_DIR)
-BOARD_DIR := $(MICROKIT_SDK)/board/$(MICROKIT_BOARD)/$(MICROKIT_CONFIG)
+GPIO_DRIVER := $(SDDF)/drivers/gpio/$(GPIO_DRIV_DIR)
 SYSTEM_FILE := gpio.system
 DTS := $(SDDF)/dts/$(MICROKIT_BOARD).dts
 DTB := $(MICROKIT_BOARD).dtb
@@ -59,14 +36,13 @@ CONFIGS_DIR   := $(TOP)/include
 CONFIG_HEADER := $(CONFIGS_DIR)/gpio_config.h
 
 IMAGES := gpio_driver.elf client.elf
-CFLAGS_ARCH := -mcpu=$(CPU) -mstrict-align -target aarch64-none-elf
 
-# @ Tristan : may be able to not compile with the sib i got rid of this (-nostdlib \)
-CFLAGS := -nostdlib \
-		  -ffreestanding \
-		  -g3 \
-		  -O3 \
-		  -Wall -Wno-unused-function -Werror -Wno-unused-command-line-argument \
+
+IMAGE_FILE = loader.img
+REPORT_FILE = report.txt
+SYSTEM_FILE = gpio.system
+
+CFLAGS += -Wall -Wno-unused-function -Werror -Wno-unused-command-line-argument \
 		  -I$(BOARD_DIR)/include \
 		  -I$(SDDF)/include \
 		  -I$(SDDF)/include/microkit \
@@ -75,15 +51,9 @@ CFLAGS := -nostdlib \
 		  $(CFLAGS_ARCH)
 LDFLAGS := -L$(BOARD_DIR)/lib
 LIBS := --start-group -lmicrokit -Tmicrokit.ld libsddf_util_debug.a --end-group
-
-
 all: $(IMAGE_FILE)
-# @Trsitan: i dont think this is neccesary
-CHECK_FLAGS_BOARD_MD5:=.board_cflags-$(shell echo -- ${CFLAGS} ${MICROKIT_SDK} ${MICROKIT_BOARD} ${MICROKIT_CONFIG} | shasum | sed 's/ *-//')
 
-${CHECK_FLAGS_BOARD_MD5}:
-	-rm -f .board_cflags-*
-	touch $@
+CHECK_FLAGS_BOARD_MD5:=.board_cflags-$(shell echo -- ${CFLAGS} ${MICROKIT_SDK} ${MICROKIT_BOARD} ${MICROKIT_CONFIG} | shasum | sed 's/ *-//')
 
 include ${GPIO_DRIVER}/gpio_driver.mk
 include ${SDDF}/util/util.mk
@@ -99,18 +69,20 @@ client.o: ${TOP}/client.c $(CONFIG_HEADER)
 client.elf: client.o libco.a
 	$(LD) $(LDFLAGS) $^ $(LIBS) -o client.elf
 
-$(DTB): $(DTS)
-	dtc -q -I dts -O dtb $(DTS) > $(DTB)
-
 $(SYSTEM_FILE): $(METAPROGRAM) $(IMAGES) $(DTB)
 	$(PYTHON) $(METAPROGRAM) --sddf $(SDDF) --board $(MICROKIT_BOARD) --dtb $(DTB) --output . --sdf $(SYSTEM_FILE)
 	$(OBJCOPY) --update-section .device_resources=gpio_driver_device_resources.data gpio_driver.elf
 	$(OBJCOPY) --update-section .gpio_client_config=gpio_client_client.data client.elf
 
-$(IMAGE_FILE) $(REPORT_FILE): $(SYSTEM_FILE)
+$(IMAGE_FILE) $(REPORT_FILE): $(IMAGES) $(SYSTEM_FILE)
 	$(MICROKIT_TOOL) $(SYSTEM_FILE) --search-path $(BUILD_DIR) --board $(MICROKIT_BOARD) --config $(MICROKIT_CONFIG) -o $(IMAGE_FILE) -r $(REPORT_FILE)
+
+${IMAGES}: libsddf_util_debug.a
+.PHONY: all compile clean
 
 clean::
 	rm -f client.o
+	find . -name '*.[do]' |xargs --no-run-if-empty rm
+
 clobber:: clean
 	rm -f client.elf ${IMAGE_FILE} ${REPORT_FILE}
