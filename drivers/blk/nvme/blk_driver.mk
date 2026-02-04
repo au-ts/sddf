e260a61601b5ebffe35e48e3a55d2ccb095c7931
dumsum <dumsum@pm.me>
2026-02-04 13:24:44 +1100

    Initial NVMe driver for x86
    Co-authored-by: julia <git.ts@trainwit.ch>
    Signed-off-by: dumsum <dumsum@pm.me>

---
+++ drivers/blk/nvme/blk_driver.mk
@@ -0,0 +1,27 @@
+#
+# Copyright 2026, UNSW
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Include this snippet in your project Makefile to build the NVMe driver.
+# Assumes libsddf_util_debug.a is in ${LIBS}.
+
+BLK_DRIVER_DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
+BLK_NEED_TIMER := 1
+
+CHECK_NVME_FLAGS_MD5:=.nvme_cflags-$(shell echo -- ${CFLAGS} | shasum | sed 's/ *-//')
+
+${CHECK_NVME_FLAGS_MD5}:
+	-rm -f .nvme_cflags-*
+	touch $@
+
+blk_driver.elf: blk_driver.o
+	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
+
+blk_driver.o: ${BLK_DRIVER_DIR}/nvme.c ${CHECK_NVME_FLAGS_MD5}
+	$(CC) -c $(CFLAGS) -I${BLK_DRIVER_DIR} -o $@ $<
+
+clean::
+	rm -f blk_driver.o
+
+clobber::
+	rm -f blk_driver.elf
