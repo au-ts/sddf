a09f80e666f6114be81ed12393e6cbce22001ed9
Samuel Tyler <samuel@samuelt.me>
2026-02-06 15:34:32 +1100

    Use new transpiler features

--- drivers/network/imx/transpile.sh
+++ drivers/network/imx/transpile.sh
@@ -4,5 +4,4 @@ cat \
 	ethernet_header.pnk \
 	../../../include/sddf/network/queue.pnk \
 	ethernet.pnk | cpp -P > ethernet_full.pnk
-pancake2viper -I device.vpr -I virtualiser.vpr -I ethernet_hard_import.vpr --allow-undefined-shared --ignore-warnings transpile ethernet_full.pnk ethernet.vpr
-sed -i -e 's/:= bv64_to_int(bv64_or(bv64_from_int(x_126), bv64_from_int(8192)))/:= _f_or_WRAP(x_126)/' ethernet.vpr
+$HOME/Documents/pancake-transpiler-private/target/debug/pancake2viper -I device.vpr -I virtualiser.vpr --allow-undefined-shared --ignore-warnings --incremental --encoding-mode mapped --encoding-mode-map mapping transpile ethernet_full.pnk ethernet.vpr
