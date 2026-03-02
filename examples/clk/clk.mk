55cdd49c266fde18f9545c530d5b38021d18e27a
Lesley Rossouw <lesley.rossouw@unsw.edu.au>
2026-03-02 17:15:48 +1100

    Update clock driver to current sddf build style, added meta.py
    Signed-off-by: Lesley Rossouw <lesley.rossouw@unsw.edu.au>

--- examples/clk/clk.mk
+++ examples/clk/clk.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2024, UNSW
+# Copyright 2026, UNSW
 #
 # SPDX-License-Identifier: BSD-2-Clause
 #
@@ -8,71 +8,58 @@ ifeq ($(strip $(MICROKIT_SDK)),)
 $(error MICROKIT_SDK must be specified)
 endif
 
-
-ifeq ($(strip $(MICROKIT_BOARD)), odroidc4)
-  CPU := cortex-a55
-  DTS_FILE := $(TOP)/dts/odroidc4.dts
-	SYSTEM_FILE := ${TOP}/board/odroidc4/clk.system
-	ARCH := aarch64
-	DRIVER_DIR := meson
-  DRIVER_CLASS := meson
-	CPU := cortex-a55
-else ifeq ($(strip $(MICROKIT_BOARD)), maaxboard)
-	ARCH := aarch64
-  DTS_FILE := $(TOP)/dts/maaxboard.dts
-	SYSTEM_FILE := ${TOP}/board/maaxboard/clk.system
-	DRIVER_DIR := imx
-  DRIVER_CLASS := imx
-	CPU := cortex-a53
-else
-$(error Unsupported MICROKIT_BOARD given)
+ifeq ($(strip $(TOOLCHAIN)),)
+	TOOLCHAIN := clang
 endif
-
-BUILD_DIR ?= $(TOP)/build
-# By default we make a debug build so that the client debug prints can be seen.
-MICROKIT_CONFIG ?= debug
-
-CC := aarch64-none-elf-gcc
-LD := aarch64-none-elf-ld
-AR := aarch64-none-elf-ar
-AS := aarch64-none-elf-as
-RANLIB := aarch64-none-elf-ranlib
-PYTHON := python3
-
-MICROKIT_TOOL ?= $(MICROKIT_SDK)/bin/microkit
 
 BOARD_DIR := $(MICROKIT_SDK)/board/$(MICROKIT_BOARD)/$(MICROKIT_CONFIG)
 UTIL := $(SDDF)/util
 
-IMAGES := timer_driver.elf clk_driver.elf client.elf
-CFLAGS := -mcpu=$(CPU) \
-		  -mstrict-align \
-		  -nostdlib \
-		  -ffreestanding \
-		  -g3 \
-		  -Wall -Wno-unused-function -Werror -Wno-unused-command-line-argument \
-		  -I$(BOARD_DIR)/include \
-		  -I$(SDDF)/include \
-		  -I$(LIBMICROKITCO_PATH) \
-		  -I$(TOP) \
-      -DBOARD_CLASS_$(DRIVER_CLASS)
+SUPPORTED_BOARDS := \
+		odroidc4 \
+		maaxboard
 
-LDFLAGS := -L$(BOARD_DIR)/lib -L.
+include ${SDDF}/tools/make/board/common.mk
+
+SDDF_CUSTOM_LIBC := 1
+UTIL := $(SDDF)/util
+LIBCO := $(SDDF)/libco
+TOP := ${SDDF}/examples/clk
+SERIAL := $(SDDF)/serial
+I2C_DRIVER := $(SDDF)/drivers/i2c/${I2C_DRIV_DIR}
+SERIAL_DRIVER := $(SDDF)/drivers/serial/${UART_DRIV_DIR}
+CLK_DRIVER := $(SDDF)/drivers/clk/${CLK_DRIV_DIR}
+TIMER_DRIVER := $(SDDF)/drivers/timer/${TIMER_DRIV_DIR}
+
+IMAGES := timer_driver.elf \
+		  clk_driver.elf \
+		  client.elf \
+		  serial_driver.elf \
+		  serial_virt_tx.elf
+LDFLAGS := -L$(BOARD_DIR)/lib
 LIBS := --start-group -lmicrokit -Tmicrokit.ld libsddf_util_debug.a --end-group
 
 IMAGE_FILE := loader.img
 REPORT_FILE := report.txt
+SYSTEM_FILE = clk.system
 CLIENT_OBJS := client.o
-CLK_DRIVER := $(SDDF)/drivers/clk/$(DRIVER_DIR)
-TIMER_DRIVER := $(SDDF)/drivers/timer/$(DRIVER_DIR)
 
+DTS := $(SDDF)/dts/$(MICROKIT_BOARD).dts
+DTB := $(MICROKIT_BOARD).dtb
+METAPROGRAM := $(TOP)/meta.py
+
+CFLAGS += -I$(BOARD_DIR)/include \
+	-I$(SDDF)/include \
+	-I$(SDDF)/include/microkit \
+	-I$(LIBCO) \
+	-MD \
+	-MP
+
+VPATH := ${TOP}
 all: $(IMAGE_FILE)
 
 ${IMAGES}: libsddf_util_debug.a
 
-include ${SDDF}/util/util.mk
-include ${TIMER_DRIVER}/timer_driver.mk
-include ${CLK_DRIVER}/clk_driver.mk
 
 client.o: ${TOP}/client.c
 	$(CC) -c $(CFLAGS) $(CHIP_HEADER_INC) -DTEST_BOARD_${MICROKIT_BOARD} $< -o client.o
@@ -80,10 +67,32 @@ client.o: ${TOP}/client.c
 client.elf: client.o
 	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
 
+$(SYSTEM_FILE): $(METAPROGRAM) $(IMAGES) $(DTB)
+	$(PYTHON) $(METAPROGRAM) --sddf $(SDDF) --board $(MICROKIT_BOARD) --dtb $(DTB) --output . --sdf $(SYSTEM_FILE)
+	$(OBJCOPY) --update-section .device_resources=timer_driver_device_resources.data timer_driver.elf
+	$(OBJCOPY) --update-section .timer_client_config=timer_client_client.data client.elf
+	$(OBJCOPY) --update-section .device_resources=serial_driver_device_resources.data serial_driver.elf
+	$(OBJCOPY) --update-section .serial_driver_config=serial_driver_config.data serial_driver.elf
+	$(OBJCOPY) --update-section .serial_virt_tx_config=serial_virt_tx.data serial_virt_tx.elf
+	$(OBJCOPY) --update-section .serial_client_config=serial_client_client.data client.elf
+	touch $@
+
 $(IMAGE_FILE) $(REPORT_FILE): $(IMAGES) $(SYSTEM_FILE)
-	$(MICROKIT_TOOL) $(SYSTEM_FILE) --search-path $(TOP)/build --board $(MICROKIT_BOARD) --config $(MICROKIT_CONFIG) -o $(IMAGE_FILE) -r $(REPORT_FILE)
+	$(MICROKIT_TOOL) $(SYSTEM_FILE) --search-path $(BUILD_DIR) --board $(MICROKIT_BOARD) --config $(MICROKIT_CONFIG) -o $(IMAGE_FILE) -r $(REPORT_FILE)
+
+${IMAGES}: libsddf_util_debug.a
+.PHONY: all compile clean
 
 clean::
-	rm -f client.o
+	rm -f *.elf
+	find . -name '*.[do]' |xargs --no-run-if-empty rm
+
 clobber:: clean
-	rm -f client.elf ${IMAGE_FILE} ${REPORT_FILE}
+	rm -f ${REPORT_FILE} ${IMAGE_FILE} *.a .*cflags*
+
+include ${SDDF}/util/util.mk
+include ${SERIAL}/components/serial_components.mk
+include ${SERIAL_DRIVER}/serial_driver.mk
+include ${TIMER_DRIVER}/timer_driver.mk
+include ${LIBCO}/libco.mk
+include ${CLK_DRIVER}/clk_driver.mk
