3c90a9649a09b86a9c7b3278cef61fa55def7ac4
Kurt Wu <rihui.wu@unsw.edu.au>
2026-03-04 12:28:25 +1100

    fence: fix included header for Djawula compatibility
    Signed-off-by: Kurt Wu <rihui.wu@unsw.edu.au>

--- ci/genmc/os/sddf.h
+++ ci/genmc/os/sddf.h
@@ -5,3 +5,9 @@
  */
 
 /* This is a fake header file for GenMC model checking. */
+
+/* The following macro is to bypass the error branch in include/sddf/util/fence.h
+ * for DMA and I/O barriers. These barriers are not handled by GenMC and
+ * do not participate in CI.
+ */
+#define CONFIG_ARCH_X86_64 1
