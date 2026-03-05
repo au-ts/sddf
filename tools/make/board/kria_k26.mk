51241376916a5b8ea670a1b7b980d20122e3fa10
SeedRizvi <syedasadrazarizvi1@gmail.com>
2026-03-05 16:03:36 +1100

    Kria K26 platform: dts file, mk snippet and board definition
    Signed-off-by: SeedRizvi <syedasadrazarizvi1@gmail.com>

---
+++ tools/make/board/kria_k26.mk
@@ -0,0 +1,12 @@
+#
+# Copyright 2026, Skykraft
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Set up variables for kria_k26
+# Should be included _before_ toolchain makefile.
+PLATFORM := zynqmp
+TIMER_DRIV_DIR := cdns
+UART_DRIV_DIR := ${PLATFORM}
+
+CPU := cortex-a53
