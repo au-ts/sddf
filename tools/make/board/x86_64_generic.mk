46a55d9dfe5a03c150195596cd055b1e055dbcbe
Jingyao Zhou <jingyao.zhou@unsw.edu.au>
2026-03-24 11:49:07 +1100

    drivers/network: add ixgbe driver
    This driver is for Intel X540/X550 NICs and works
    with IO-APIC and MSI/MSI-X interrupts.
    
    Signed-off-by: Terry Bai <tianyi.bai@unsw.edu.au>

--- tools/make/board/x86_64_generic.mk
+++ tools/make/board/x86_64_generic.mk
@@ -12,21 +12,44 @@ ETH_DRIV ?= eth_driver_virtio.elf
 TIMER_DRIV_DIR ?= tsc_hpet
 UART_DRIV_DIR ?= pc99
 
-CPU := generic
+ifeq (${X86_BOARD},)
+    X86_BOARD := qemu_virt_x86
+endif
 
-SEL4_64B := $(MICROKIT_SDK)/board/$(MICROKIT_BOARD)/$(MICROKIT_CONFIG)/elf/sel4.elf
-SEL4_32B := $(MICROKIT_SDK)/board/$(MICROKIT_BOARD)/$(MICROKIT_CONFIG)/elf/sel4_32.elf
+ifeq (${X86_BOARD},qemu_virt_x86)
+    BLK_DRIV_DIR ?= virtio/pci
+    NET_DRIV_DIR ?= virtio/pci
+    ETH_DRIV ?= eth_driver_virtio.elf
+    UART_DRIV_DIR ?= pc99
 
-QEMU := qemu-system-x86_64
-QEMU_ARCH_ARGS := -machine q35 \
-		-kernel $(SEL4_32B) \
-		-m size=2G \
-		-serial mon:stdio \
-		-cpu qemu64,+fsgsbase,+pdpe1gb,+pcid,+invpcid,+xsave,+xsaves,+xsaveopt \
-		-initrd $(IMAGE_FILE)
+    CPU := generic
 
-# The PCI slot is hard-coded in the virtIO drivers for now, so we have to
-# specify the slot with QEMU as well.
-# See https://github.com/au-ts/sddf/issues/607 for details.
-QEMU_NET_ARGS ?= -device virtio-net-pci,netdev=netdev0,addr=0x2.0
-QEMU_BLK_ARGS ?= -device virtio-blk-pci,drive=hd,addr=0x3.0
+    SEL4_64B := $(MICROKIT_SDK)/board/$(MICROKIT_BOARD)/$(MICROKIT_CONFIG)/elf/sel4.elf
+    SEL4_32B := $(MICROKIT_SDK)/board/$(MICROKIT_BOARD)/$(MICROKIT_CONFIG)/elf/sel4_32.elf
+
+    QEMU := qemu-system-x86_64
+    QEMU_ARCH_ARGS := -machine q35 \
+            -kernel $(SEL4_32B) \
+            -m size=2G \
+            -serial mon:stdio \
+            -cpu qemu64,+fsgsbase,+pdpe1gb,+pcid,+invpcid,+xsave,+xsaves,+xsaveopt \
+            -initrd $(IMAGE_FILE)
+
+    # The PCI slot is hard-coded in the virtIO drivers for now, so we have to
+    # specify the slot with QEMU as well.
+    # See https://github.com/au-ts/sddf/issues/607 for details.
+    QEMU_NET_ARGS ?= -device virtio-net-pci,netdev=netdev0,addr=0x2.0
+    QEMU_BLK_ARGS ?= -device virtio-blk-pci,drive=hd,addr=0x3.0
+
+else ifeq ($(X86_BOARD), $(filter ${X86_BOARD},makatea vb_105))
+    NET_DRIV_DIR := ixgbe
+    ETH_DRIV := eth_driver_ixgbe.elf
+    UART_DRIV_DIR := pc99
+    CPU := generic
+
+    DTS :=
+    SEL4_64B = $(MICROKIT_SDK)/board/$(MICROKIT_BOARD)/$(MICROKIT_CONFIG)/elf/sel4.elf
+    SEL4_32B := $(MICROKIT_SDK)/board/$(MICROKIT_BOARD)/$(MICROKIT_CONFIG)/elf/sel4_32.elf
+else
+$(error Unsupported X86_BOARD given)
+endif
