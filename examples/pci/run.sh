ef77f3037545ea8fea8b9c300085dc83c9ca08f6
Terry Bai <tianyi.bai@unsw.edu.au>
2026-03-30 10:10:26 +1100

    WIP: ACPI AML parser
    Signed-off-by: Terry Bai <tianyi.bai@unsw.edu.au>

--- examples/pci/run.sh
+++ examples/pci/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
 cd /Users/terrybai/ts/sddf/examples/pci && \
+rm -rf build && \
 make BUILD_DIR=build MICROKIT_BOARD=x86_64_generic X86_BOARD=qemu_virt_x86 MICROKIT_CONFIG=debug MICROKIT_SDK=/Users/terrybai/ts/microkit/release/microkit-sdk-2.1.0-dev qemu
