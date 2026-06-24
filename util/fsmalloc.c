9bb75809dc4185713bb4d83c2cc0979b65b81155
Courtney Darville <courtneydarville94@outlook.com>
2026-06-24 17:12:13 +1000

    Use booleans for bit values
    Signed-off-by: Courtney Darville <courtneydarville94@outlook.com>

--- util/fsmalloc.c
+++ util/fsmalloc.c
@@ -40,10 +40,10 @@ void fsmalloc_free(fsmalloc_t *fsmalloc, uintptr_t addr, uint64_t count)
     unsigned int start_bitpos = addr_to_bitpos(fsmalloc, addr);
 
     // Assert that these bits were allocated
-    assert(bitarray_count_bits(fsmalloc->avail_bitarr, start_bitpos, 0) >= count);
+    assert(bitarray_count_bits(fsmalloc->avail_bitarr, start_bitpos, false) >= count);
 
     // Set the next count many bits as available
-    bitarray_set_region(fsmalloc->avail_bitarr, start_bitpos, count, 1);
+    bitarray_set_region(fsmalloc->avail_bitarr, start_bitpos, count, true);
 }
 
 int fsmalloc_alloc(fsmalloc_t *fsmalloc, uintptr_t *addr, uint64_t count)
@@ -62,13 +62,13 @@ int fsmalloc_alloc(fsmalloc_t *fsmalloc, uintptr_t *addr, uint64_t count)
             searched += fsmalloc->num_cells - curr_bitpos;
         }
 
-        uint64_t free_blocks = bitarray_count_bits(fsmalloc->avail_bitarr, curr_bitpos, 1);
+        uint64_t free_blocks = bitarray_count_bits(fsmalloc->avail_bitarr, curr_bitpos, true);
         // Allocate blocks, update head
         if (free_blocks >= count) {
             *addr = bitpos_to_addr(fsmalloc, curr_bitpos);
 
             // Set the next count many bits as unavailable
-            bitarray_set_region(fsmalloc->avail_bitarr, curr_bitpos, count, 0);
+            bitarray_set_region(fsmalloc->avail_bitarr, curr_bitpos, count, false);
 
             // Update the bitpos
             fsmalloc->avail_bitpos = (curr_bitpos + count) % fsmalloc->num_cells;
@@ -77,7 +77,7 @@ int fsmalloc_alloc(fsmalloc_t *fsmalloc, uintptr_t *addr, uint64_t count)
 
         // Skip past the allocated blocks
         curr_bitpos += free_blocks;
-        uint64_t alloced_blocks = bitarray_count_bits(fsmalloc->avail_bitarr, curr_bitpos, 0);
+        uint64_t alloced_blocks = bitarray_count_bits(fsmalloc->avail_bitarr, curr_bitpos, false);
 
         curr_bitpos = (curr_bitpos + alloced_blocks) % fsmalloc->num_cells;
         searched += free_blocks + alloced_blocks;
@@ -100,5 +100,5 @@ void fsmalloc_init(fsmalloc_t *fsmalloc, uintptr_t base_addr, uint64_t cell_size
     fsmalloc->num_cells = num_cells;
 
     /* Set all available bits to 1 to indicate all cells are available */
-    bitarray_set_region(bitarr, 0, num_cells, 1);
+    bitarray_set_region(bitarr, 0, num_cells, true);
 }
