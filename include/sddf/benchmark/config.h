36e069dc79e65b15fb75ad4acee64a2f084bc6c1
Terry Bai <tianyi.bai@unsw.edu.au>
2026-07-22 22:32:29 +1000

    wip: add cycles reading for x86
    Signed-off-by: Terry Bai <tianyi.bai@unsw.edu.au>

--- include/sddf/benchmark/config.h
+++ include/sddf/benchmark/config.h
@@ -15,7 +15,7 @@
  * on non-benchmarking configurations.
  * This defines whether we actually try to benchmark, setup the PMU etc.
  */
-#if defined(CONFIG_ENABLE_BENCHMARKS) && (defined(CONFIG_ARCH_ARM) || defined(CONFIG_ARCH_RISCV))
+#if defined(CONFIG_ENABLE_BENCHMARKS) && (defined(CONFIG_ARCH_ARM) || defined(CONFIG_ARCH_RISCV) || defined(CONFIG_ARCH_X86_64))
 #define ENABLE_BENCHMARKING 1
 #else
 #define ENABLE_BENCHMARKING 0
