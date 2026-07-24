5f48c16960cfba0cd1c55976744a564fc3679721
dumsum <dumsum@pm.me>
2026-07-24 10:50:56 +1000

    timer: use user space arch timer API for x86
    Signed-off-by: dumsum <dumsum@pm.me>

--- include/sddf/timer/client.h
+++ include/sddf/timer/client.h
@@ -8,6 +8,9 @@
 #include <os/sddf.h>
 #include <stdint.h>
 #include <sddf/timer/protocol.h>
+#include <sddf/timer/timer_common.h>
+#include <sddf/util/util.h>
+#include <sddf/util/arch_timestamp_counter.h>
 
 /**
  * Request a timeout via PPC into the passive timer driver.
@@ -22,14 +25,23 @@ static inline void sddf_timer_set_timeout(unsigned int channel, uint64_t timeout
 }
 
 /**
- * Request the time since start up via PPC into the passive timer driver.
- * Use the label to indicate this request.
+ * Request the time since start up.
+ * If architecture counter is usable, fast-paths reading the hardware counter
+ * locally via sddf_read_counter().
+ * If unusable, silently falls back to a PPC into the passive timer driver.
  * @param channel ID of the timer driver.
  * @return the time in nanoseconds since start up.
  */
 static inline uint64_t sddf_timer_time_now(unsigned int channel)
 {
+    // TODO: remove this guard once remaining architectures supported
+#if defined(CONFIG_ARCH_X86)
+    uint64_t freq = sddf_read_freq();
+    if (likely(freq != 0)) {
+        return ticks_to_ns(sddf_read_counter(), freq);
+    }
+#endif // CONFIG_ARCH_X86
+
     sddf_ppcall(channel, seL4_MessageInfo_new(SDDF_TIMER_GET_TIME, 0, 0, 0));
-    uint64_t time_now = sddf_get_mr(0);
-    return time_now;
+    return sddf_get_mr(0);
 }
