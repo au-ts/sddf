3f82d43b7aff417b49a6c9048ceab8e97610af25
Kurt Wu <rihui.wu@unsw.edu.au>
2026-08-18 18:53:40 +1000

    drivers/pc99: add pc99 driver in Pancake
    Signed-off-by: Kurt Wu <rihui.wu@unsw.edu.au>

--- util/pancake_ffi.c
+++ util/pancake_ffi.c
@@ -1,3 +1,4 @@
+#include <microkit.h>
 #include <os/sddf.h>
 #include <sddf/util/cache.h>
 #include <sddf/util/fence.h>
@@ -19,6 +20,17 @@ void ffisddf_deferred_irq_ack(unsigned char *c, long clen, unsigned char *a, lon
 void ffisddf_deferred_notify(unsigned char *c, long clen, unsigned char *a, long alen) {
     sddf_deferred_notify(clen);
 }
+
+#if defined(CONFIG_ARCH_X86_64)
+void ffimicrokit_x86_ioport_write_8(unsigned char *c, long clen, unsigned char *a, long alen) {
+    microkit_x86_ioport_write_8(clen, (seL4_Word) c, (seL4_Word) a);
+}
+
+void ffimicrokit_x86_ioport_read_8(unsigned char *c, long clen, unsigned char *a, long alen) {
+    seL4_Uint8 ret = microkit_x86_ioport_read_8(clen, (seL4_Word) a);
+    *(seL4_Uint8 *)c = ret; 
+}
+#endif
 
 void fficache_clean(unsigned char *c, long clen, unsigned char *a, long alen) {
     cache_clean((unsigned long) c, (unsigned long) a);
