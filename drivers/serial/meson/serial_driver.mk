a4e977663dbcea39563399b8da7a31194aba1919
Kurt Wu <rihui.wu@unsw.edu.au>
2026-08-18 13:12:48 +1000

    drivers/serial: add meson derivers in Pancake
    Signed-off-by: Kurt Wu <rihui.wu@unsw.edu.au>

--- drivers/serial/meson/serial_driver.mk
+++ drivers/serial/meson/serial_driver.mk
@@ -8,11 +8,30 @@
 
 SERIAL_DRIVER_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
+ifeq ($(PANCAKE_SERIAL_DRIVER),1)
+DRIVER_PNK = \
+	${UTIL}/util.pnk \
+	${SDDF}/include/sddf/serial/queue.pnk \
+	${SERIAL_DRIVER_DIR}/uart.pnk
+
+serial_driver.elf: serial/meson/serial_driver_pnk.o serial/meson/serial_driver.o util/pancake_ffi.o libsddf_util_debug.a
+	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
+
+serial/meson/serial_driver_pnk.o: serial/meson/serial_driver_pnk.S |serial/meson
+	$(CC) -c $(CFLAGS) -o $@ $<
+
+serial/meson/serial_driver_pnk.S: $(DRIVER_PNK) |serial/meson
+	cat $(DRIVER_PNK) | cpp -P | $(PANCAKE_COMPILER) $(PANCAKE_FLAGS) > $@
+
+serial/meson/serial_driver.o: ${SERIAL_DRIVER_DIR}/uart.c |serial/meson $(SDDF_LIBC_INCLUDE)
+	$(CC) -c $(CFLAGS) -DPANCAKE_SERIAL_DRIVER -I${SERIAL_DRIVER_DIR}/include -o $@ $<
+else
 serial_driver.elf: serial/meson/serial_driver.o libsddf_util_debug.a
 	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
 
 serial/meson/serial_driver.o: ${SERIAL_DRIVER_DIR}/uart.c |serial/meson $(SDDF_LIBC_INCLUDE)
 	$(CC) -c $(CFLAGS) -I${SERIAL_DRIVER_DIR}/include -o $@ $<
+endif
 
 serial/meson:
 	mkdir -p $@
@@ -20,7 +39,7 @@ serial/meson:
 -include serial/meson/serial_driver.d
 
 clean::
-	rm -f serial/meson/serial_driver.[do]
+	rm -f serial/meson/serial_driver.[do] serial/meson/serial_driver_pnk.[oS]
 
 clobber::
 	rm -rf serial
