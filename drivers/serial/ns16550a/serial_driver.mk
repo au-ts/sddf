147b158e745c8de9babbe4540790008272c15388
Kurt Wu <rihui.wu@unsw.edu.au>
2026-08-18 16:44:28 +1000

    drivers/serial: add ns16550a drivers in Pancake
    Signed-off-by: Kurt Wu <rihui.wu@unsw.edu.au>

--- drivers/serial/ns16550a/serial_driver.mk
+++ drivers/serial/ns16550a/serial_driver.mk
@@ -8,11 +8,31 @@
 
 SERIAL_DRIVER_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
+ifeq ($(PANCAKE_SERIAL_DRIVER),1)
+DRIVER_PNK = \
+	${UTIL}/util.pnk \
+	${SDDF}/include/sddf/serial/queue.pnk \
+	${SERIAL_DRIVER_DIR}/uart.pnk
+
+serial_driver.elf: serial/ns16550a/serial_driver_pnk.o serial/ns16550a/serial_driver.o util/pancake_ffi.o libsddf_util_debug.a
+	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
+
+serial/ns16550a/serial_driver_pnk.o: serial/ns16550a/serial_driver_pnk.S |serial/ns16550a
+	$(CC) -c $(CFLAGS) -o $@ $<
+
+serial/ns16550a/serial_driver_pnk.S: $(DRIVER_PNK) |serial/ns16550a
+	cat $(DRIVER_PNK) | cpp -P > /workspaces/sddf/1.pnk
+	cat $(DRIVER_PNK) | cpp -P | $(PANCAKE_COMPILER) $(PANCAKE_FLAGS) > $@
+
+serial/ns16550a/serial_driver.o: ${SERIAL_DRIVER_DIR}/uart.c |serial/ns16550a $(SDDF_LIBC_INCLUDE)
+	$(CC) -c $(CFLAGS) -DPANCAKE_SERIAL_DRIVER -I${SERIAL_DRIVER_DIR}/include -o $@ $<
+else
 serial_driver.elf: serial/ns16550a/serial_driver.o
 	$(LD) $(LDFLAGS) $< $(LIBS) -o $@
 
 serial/ns16550a/serial_driver.o: ${SERIAL_DRIVER_DIR}/uart.c |serial/ns16550a $(SDDF_LIBC_INCLUDE)
 	$(CC) -c $(CFLAGS) -I${SERIAL_DRIVER_DIR}/include -o $@ $<
+endif
 
 serial/ns16550a:
 	mkdir -p $@
@@ -20,6 +40,6 @@ serial/ns16550a:
 -include serial/ns16550a/serial_driver.d
 
 clean::
-	rm -f serial/ns16550a/serial_driver.[do]
+	rm -f serial/ns16550a/serial_driver.[do] serial/ns16550a/serial_driver_pnk.[oS]
 clobber:: clean
 	rm -rf serial_driver.elf serial
