6e37f969bb55fbb4962e5467a3aaa6355a48c979
Kurt Wu <rihui.wu@unsw.edu.au>
2026-08-18 18:53:40 +1000

    drivers/pc99: add pc99 driver in Pancake
    Signed-off-by: Kurt Wu <rihui.wu@unsw.edu.au>

--- drivers/serial/pc99/serial_driver.mk
+++ drivers/serial/pc99/serial_driver.mk
@@ -11,11 +11,30 @@
 
 SERIAL_DRIVER_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
+ifeq ($(PANCAKE_SERIAL_DRIVER),1)
+DRIVER_PNK = \
+	${UTIL}/util.pnk \
+	${SDDF}/include/sddf/serial/queue.pnk \
+	${SERIAL_DRIVER_DIR}/uart.pnk
+
+serial_driver.elf: serial/pc99/serial_driver_pnk.o serial/pc99/serial_driver.o util/pancake_ffi.o libsddf_util_debug.a
+	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
+
+serial/pc99/serial_driver_pnk.o: serial/pc99/serial_driver_pnk.S |serial/pc99
+	$(CC) -c $(CFLAGS) -o $@ $<
+
+serial/pc99/serial_driver_pnk.S: $(DRIVER_PNK) |serial/pc99
+	cat $(DRIVER_PNK) | cpp -P | $(PANCAKE_COMPILER) $(PANCAKE_FLAGS) > $@
+
+serial/pc99/serial_driver.o: ${SERIAL_DRIVER_DIR}/uart.c |serial/pc99 $(SDDF_LIBC_INCLUDE)
+	$(CC) -c $(CFLAGS) -DPANCAKE_SERIAL_DRIVER -I${SERIAL_DRIVER_DIR}/include -o $@ $<
+else
 serial_driver.elf: serial/pc99/serial_driver.o libsddf_util_debug.a
 	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
 
 serial/pc99/serial_driver.o: ${SERIAL_DRIVER_DIR}/uart.c |serial/pc99 $(SDDF_LIBC_INCLUDE)
 	$(CC) -c $(CFLAGS) -I${SERIAL_DRIVER_DIR}/include -o $@ $<
+endif
 
 serial/pc99:
 	mkdir -p $@
