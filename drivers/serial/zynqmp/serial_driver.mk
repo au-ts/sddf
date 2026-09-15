08918a68be8298d96666e47ba561a30425979001
Kurt Wu <rihui.wu@unsw.edu.au>
2026-09-15 17:36:49 +1000

    drivers/serial/zynqmp: separate Pancake code
    Signed-off-by: Kurt Wu <rihui.wu@unsw.edu.au>

--- drivers/serial/zynqmp/serial_driver.mk
+++ drivers/serial/zynqmp/serial_driver.mk
@@ -9,33 +9,14 @@
 
 SERIAL_DRIVER_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-ifeq ($(PANCAKE_SERIAL_DRIVER),1)
-DRIVER_PNK = \
-	${UTIL}/util.pnk \
-	${SDDF}/include/sddf/serial/queue.pnk \
-	${SERIAL_DRIVER_DIR}/uart.pnk
-
-serial_driver.elf: serial/zynqmp/serial_driver_pnk.o serial/zynqmp/serial_driver.o util/pancake_ffi.o libsddf_util_debug.a util/pancake_common.o
+serial_driver.elf: serial/zynqmp/uart.o serial/zynqmp/uart_common.o
 	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
 
-serial/zynqmp/serial_driver_pnk.o: serial/zynqmp/serial_driver_pnk.S |serial/zynqmp
-	$(CC) -c $(CFLAGS) -o $@ $<
-
-serial/zynqmp/serial_driver_pnk.S: serial/zynqmp/serial_driver_pnk.pnk |serial/zynqmp
-	$(PANCAKE_COMPILER) $(PANCAKE_FLAGS) < $< > $@
-
-serial/zynqmp/serial_driver_pnk.pnk: $(DRIVER_PNK) |serial/zynqmp
-	cat $^ | cpp -P -nostdinc > $@
-
-serial/zynqmp/serial_driver.o: ${SERIAL_DRIVER_DIR}/uart.c |serial/zynqmp $(SDDF_LIBC_INCLUDE)
-	$(CC) -c $(CFLAGS) -DPANCAKE_SERIAL_DRIVER -I${SERIAL_DRIVER_DIR}/include -o $@ $<
-else
-serial_driver.elf: serial/zynqmp/serial_driver.o
-	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
-
-serial/zynqmp/serial_driver.o: ${SERIAL_DRIVER_DIR}/uart.c |serial/zynqmp $(SDDF_LIBC_INCLUDE)
+serial/zynqmp/uart.o: ${SERIAL_DRIVER_DIR}/uart.c |serial/zynqmp $(SDDF_LIBC_INCLUDE)
 	$(CC) -c $(CFLAGS) -I${SERIAL_DRIVER_DIR}/include -o $@ $<
-endif
+
+serial/zynqmp/uart_common.o: ${SERIAL_DRIVER_DIR}/uart_common.c |serial/zynqmp $(SDDF_LIBC_INCLUDE)
+	$(CC) -c $(CFLAGS) -I${SERIAL_DRIVER_DIR}/include -o $@ $<
 
 -include serial_driver.d
 
