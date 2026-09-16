a3a49ee13294474c6fd4c6c4ba265f7379d94de1
Kurt Wu <rihui.wu@unsw.edu.au>
2026-09-16 11:58:57 +1000

    drivers/serial/meson: rename `notified()` symbol
    Signed-off-by: Kurt Wu <rihui.wu@unsw.edu.au>

--- drivers/serial/meson/serial_driver.mk
+++ drivers/serial/meson/serial_driver.mk
@@ -8,33 +8,11 @@
 
 SERIAL_DRIVER_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-ifeq ($(PANCAKE_SERIAL_DRIVER),1)
-DRIVER_PNK = \
-	${UTIL}/util.pnk \
-	${SDDF}/include/sddf/serial/queue.pnk \
-	${SERIAL_DRIVER_DIR}/uart.pnk
-
-serial_driver.elf: serial/meson/serial_driver_pnk.o serial/meson/serial_driver.o util/pancake_ffi.o libsddf_util_debug.a util/pancake_common.o
-	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
-
-serial/meson/serial_driver_pnk.o: serial/meson/serial_driver_pnk.S |serial/meson
-	$(CC) -c $(CFLAGS) -o $@ $<
-
-serial/meson/serial_driver_pnk.S: serial/meson/serial_driver_pnk.pnk |serial/meson
-	$(PANCAKE_COMPILER) $(PANCAKE_FLAGS) < $< > $@
-
-serial/meson/serial_driver_pnk.pnk: $(DRIVER_PNK) |serial/meson
-	cat $^ | cpp -P > $@
-
-serial/meson/serial_driver.o: ${SERIAL_DRIVER_DIR}/uart.c |serial/meson $(SDDF_LIBC_INCLUDE)
-	$(CC) -c $(CFLAGS) -DPANCAKE_SERIAL_DRIVER -I${SERIAL_DRIVER_DIR}/include -o $@ $<
-else
 serial_driver.elf: serial/meson/serial_driver.o libsddf_util_debug.a
 	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
 
 serial/meson/serial_driver.o: ${SERIAL_DRIVER_DIR}/uart.c |serial/meson $(SDDF_LIBC_INCLUDE)
 	$(CC) -c $(CFLAGS) -I${SERIAL_DRIVER_DIR}/include -o $@ $<
-endif
 
 serial/meson:
 	mkdir -p $@
@@ -42,7 +20,7 @@ serial/meson:
 -include serial/meson/serial_driver.d
 
 clean::
-	rm -f serial/meson/serial_driver.[do] serial/meson/serial_driver_pnk.[oS]
+	rm -f serial/meson/serial_driver.[do]
 
 clobber::
 	rm -rf serial
--- drivers/serial/meson/uart.c
+++ drivers/serial/meson/uart.c
@@ -11,10 +11,6 @@
 #include <sddf/serial/config.h>
 #include <uart.h>
 
-#ifdef PANCAKE_SERIAL_DRIVER
-#include <sddf/util/pancake_common.h>
-#endif /* PANCAKE_SERIAL_DRIVER */
-
 __attribute__((__section__(".device_resources"))) device_resources_t device_resources;
 
 __attribute__((__section__(".serial_driver_config"))) serial_driver_config_t config;
@@ -75,7 +71,6 @@ static void set_baud(unsigned long baud)
     uart_regs->reg5 = baud_register;
 }
 
-#ifndef PANCAKE_SERIAL_DRIVER
 static void tx_provide(void)
 {
     bool transferred = false;
@@ -148,7 +143,6 @@ static void handle_irq(void)
         uart_cr = uart_regs->cr;
     }
 }
-#endif /* PANCAKE_SERIAL_DRIVER */
 
 static void uart_setup(void)
 {
@@ -209,27 +203,8 @@ void init(void)
         serial_queue_init(&rx_queue_handle, config.rx.queue.vaddr, config.rx.data.size, config.rx.data.vaddr);
     }
     serial_queue_init(&tx_queue_handle, config.tx.queue.vaddr, config.tx.data.size, config.tx.data.vaddr);
-
-#ifdef PANCAKE_SERIAL_DRIVER
-    init_pancake_mem();
-
-    uintptr_t *pnk_mem = (uintptr_t *)cml_heap;
-
-    pnk_mem[0] = (uintptr_t)uart_regs;
-    pnk_mem[1] = device_resources.irqs[0].id;
-    pnk_mem[2] = config.rx.id;
-    pnk_mem[3] = config.tx.id;
-    pnk_mem[4] = (uintptr_t)&rx_queue_handle;
-    pnk_mem[5] = (uintptr_t)&tx_queue_handle;
-    pnk_mem[1024] = config.rx_enabled;
-
-    cml_main();
-#endif /* PANCAKE_SERIAL_DRIVER */
 }
 
-#ifdef PANCAKE_SERIAL_DRIVER
-extern void notified(sddf_channel ch);
-#else
 void notified(sddf_channel ch)
 {
     if (ch == device_resources.irqs[0].id) {
@@ -244,4 +219,3 @@ void notified(sddf_channel ch)
         sddf_dprintf("UART|LOG: received notification on unexpected channel: %u\n", ch);
     }
 }
-#endif /* PANCAKE_SERIAL_DRIVER */
