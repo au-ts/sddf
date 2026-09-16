4880f9f1f014423715c4816cddf356a35c1e379d
Lesley Rossouw <lesley.rossouw@unsw.edu.au>
2026-09-16 11:48:37 +1000

    Minor fixes to match ConfigStructs updates
    Signed-off-by: Lesley Rossouw <lesley.rossouw@unsw.edu.au>

--- acacia_sddf/timer.py
+++ acacia_sddf/timer.py
@@ -98,10 +98,9 @@ class sDDFTimer(sDDFDriverClass):
         )
         self.driver.add_irq(hpet_irq)
         # paddr=0xFED00000 is a x86 convention for HPET, though it may be different on some machines depending on their BIOS.
-        hpet_regs = MemoryRegion("hpet_regs", 0x1000, paddr=0xFED00000)
+        hpet_regs = MemoryRegion(self.sdf, "hpet_regs", 0x1000, paddr=0xFED00000)
         hpet_regs_map = Map(hpet_regs, 0x5000_0000, "rw")
         self.driver.add_map(hpet_regs_map)
-        sdf.add_memory_region(hpet_regs)
 
 
 # Driver configs
