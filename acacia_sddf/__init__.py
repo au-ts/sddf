ea9d3e441fa19788bf2a7f059095adf80e7201a6
Lesley Rossouw <lesley.rossouw@unsw.edu.au>
2026-09-16 12:40:32 +1000

    Cleanup and PR comments
    Signed-off-by: Lesley Rossouw <lesley.rossouw@unsw.edu.au>

--- acacia_sddf/__init__.py
+++ acacia_sddf/__init__.py
@@ -1,10 +1,10 @@
 # Copyright 2026, UNSW
 # SPDX-License-Identifier: BSD-2-Clause
 
+from .blk import sDDFBlk
 from .board import BOARDS, Board
 from .driver_manifest import sDDFDriverConfig, sDDFDriverManifest
 from .i2c import sDDFI2C
 from .sddf import sDDFDriverClass
 from .serial import sDDFSerial
 from .timer import sDDFTimer
-from .blk import sDDFBlk
